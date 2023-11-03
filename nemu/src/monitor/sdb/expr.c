f3b69e10e6ea5cf6a7744d4adfe93875e7561ea9
tracer-ysyx <tracer@ysyx.org>
2023-11-03 22:58:57 +0800

    > compile NEMU ysyx_22040000 张三 Linux corgi-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:58:57 up 10:24,  1 user,  load average: 0.14, 0.20, 0.23

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -248,8 +248,8 @@ int tokens_len = nr_token;
 	    tokens[i].type = TK_NOTYPE;
 	    int tmp = atoi(tokens[i+1].str);
 	    if(tmp == 0){
-		memset(tokens[i+1].str, 0 ,sizeof(tokens[i+1].str));
-		tokens[i+1].str[0] = '1';
+		memset(tokens[i+1].str, 1 ,sizeof(tokens[i+1].str));
+		//tokens[i+1].str[0] = '1';
 	    }
 	    else
 	    {
