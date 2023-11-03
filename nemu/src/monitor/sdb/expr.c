5a6bbbc8f98ef2b0dc0b6f6cc59282597d9e6a1c
tracer-ysyx <tracer@ysyx.org>
2023-11-03 09:05:41 +0800

    > compile NEMU ysyx_22040000 张三 Linux corgi-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:05:41 up 20 min,  1 user,  load average: 0.49, 0.24, 0.19

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -189,7 +189,8 @@ int tokens_len = nr_token;
 	    tmp = isa_reg_str2val(tokens[i].str, &flat);
 	    if(flat)
 	    {
-	        sprintf(tokens[i].str,"%x",tmp);
+	        sprintf(tokens[i].str,"0x%x",tmp);
+		tokens[i].type=HEX;
 	//	int_to_char(tmp, tokens[i].str); // transfrom the str --> $egx
 	    }
 	    else
