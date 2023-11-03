cdd90a6a178b117c0f75582e60819ea154c07fb1
tracer-ysyx <tracer@ysyx.org>
2023-11-04 07:54:31 +0800

    > compile NEMU ysyx_22040000 张三 Linux corgi-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  07:54:31 up 17:13,  1 user,  load average: 0.08, 0.13, 0.17

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -286,7 +286,7 @@ int tokens_len = nr_token;
 		{
             tokens[i].type = TK_NOTYPE;
             int tmp = char_to_int(tokens[i+1].str);
-            uintptr_t a = (uintptr_t)tmp;//int <-> *()
+            uintptr_t *a = (uintptr_t*)&tmp;//int <-> *()
             int value = *((int*)a);
             //int_to_char(value, tokens[i+1].str);	    
 	    sprintf(tokens[i+1].str,"%d",value);
