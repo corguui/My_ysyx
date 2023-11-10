5b3f87ebccbb0cf837bed519b19c63d007bdfeaa
tracer-ysyx <tracer@ysyx.org>
2023-11-10 21:45:49 +0800

    > compile NEMU ysyx_22040000 张三 Linux corgi-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:45:49 up 1 day, 12:02,  4 users,  load average: 0.16, 0.15, 0.13

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -52,6 +52,7 @@ static int decode_exec(Decode *s) {
   int rd = 0;
   word_t src1 = 0, src2 = 0, imm = 0;
   s->dnpc = s->snpc;
+  printf("the dnpc=snpc the snpc== %x\n",s->snpc);
 
 #define INSTPAT_INST(s) ((s)->isa.inst.val)
 #define INSTPAT_MATCH(s, name, type, ... /* execute body */ ) { \
