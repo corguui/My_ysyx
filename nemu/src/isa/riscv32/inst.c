42b00e3ba77c0506176844c693ffd097b5fd4f1d
tracer-ysyx <tracer@ysyx.org>
2023-11-11 10:44:37 +0800

    > compile NEMU ysyx_22040000 张三 Linux corgi-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:44:37 up 1 day, 15:55,  6 users,  load average: 0.10, 0.14, 0.15

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -24,7 +24,8 @@
 
 enum {
   TYPE_I, TYPE_U, TYPE_S,
-  TYPE_N, TYPE_J, TYPE_B,// none
+  TYPE_N, TYPE_J, TYPE_B,
+  TYPE_R,// none
 };
 
 #define src1R() do { *src1 = R(rs1); } while (0)
@@ -47,6 +48,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_S: src1R(); src2R(); immS(); break;
     case TYPE_J:                   immJ(); break;
     case TYPE_B: src1R(); src2R(); immB(); break;
+    case TYPE_R: src1R(); src2R();         break;
   }
 }
 
@@ -74,6 +76,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal     , J, R(rd) = s->snpc ; s->dnpc = s->pc + imm );
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
+  INSTPAT("0000000 ????? ????? 000 ????? 01100 11", add , R, R(rd)=src1+src2);
   INSTPAT_END();
 
 printf("src1 %d  |  src2 %d  | imm  %d\n",src1,src2,imm);
