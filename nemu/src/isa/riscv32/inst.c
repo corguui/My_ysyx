a78ab780e5e540e6d7772a6245026f4e4ee5a8f2
tracer-ysyx <tracer@ysyx.org>
2023-11-11 21:07:02 +0800

    > compile NEMU ysyx_22040000 张三 Linux corgi-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:07:02 up  2:28,  1 user,  load average: 0.25, 0.20, 0.17

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -104,6 +104,7 @@ static int decode_exec(Decode *s) {
 
 printf("src1 %d  |  src2 %d  | imm  %d\n",src1,src2,imm);
 printf("src1 %x  |  src2 %x  | imm  %x\n",src1,src2,imm);
+printf("pc   %x  \n",s->pc);
 
   R(0) = 0; // reset $zero to 0
 
