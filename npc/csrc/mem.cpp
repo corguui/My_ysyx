0eae17231b768ebef0a4e5c7de45330e43cadb7f
tracer-ysyx <tracer@ysyx.org>
2023-11-16 21:02:56 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:02:56 up  3:40,  1 user,  load average: 0.22, 0.17, 0.16

--- npc/csrc/mem.cpp
+++ npc/csrc/mem.cpp
@@ -20,9 +20,9 @@ void init_mem()
 
 uint32_t pmem_read(uint32_t &pc)
 {
-	uint32_t inst =*(pmem+pc-0x80000000);
+	uint32_t* inst =pmem+pc-0x80000000;
 	pc=pc+4; 
-	return inst;
+	return *inst;
 }
 
 
