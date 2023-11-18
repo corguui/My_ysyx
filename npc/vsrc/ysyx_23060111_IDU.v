704e835c7ba3acdfb771bc4dc3339423800f3b4b
tracer-ysyx <tracer@ysyx.org>
2023-11-18 11:16:23 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:16:23 up  2:13,  1 user,  load average: 0.07, 0.12, 0.13

--- npc/vsrc/ysyx_23060111_IDU.v
+++ npc/vsrc/ysyx_23060111_IDU.v
@@ -4,7 +4,7 @@ module ysyx_23060111_IDU(
   input [31:0] inst,
   output [31:20] imm,
   output [19:15] rs1,
-  output [11:7] rd,
+  output [11:7] rd
 );
   assign dnpc=snpc;
   assign imm=inst[31:20];
--- npc/vsrc/ysyx_23060111_reg.v
+++ npc/vsrc/ysyx_23060111_reg.v
@@ -2,11 +2,10 @@ module ysyx_23060111_reg #(ADDR_WIDTH = 1, DATA_WIDTH = 1) (
   input clk,
   input [DATA_WIDTH-1:0] wdata,
   input [ADDR_WIDTH-1:0] waddr,
-  input wen;
+  input wen,
   output [DATA_WIDTH-1:0] out 
 );
   reg [DATA_WIDTH-1:0] rf [ADDR_WIDTH-1:0];
-  assign rf[0] = 32'h0;
   assign out =wadta;
   always @(posedge clk) begin
     if (wen) rf[waddr] <= wdata;
