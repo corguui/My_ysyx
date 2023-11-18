ef02d8b35a382fd1d474b193013a42b11dbd71ba
tracer-ysyx <tracer@ysyx.org>
2023-11-18 09:18:18 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:18:18 up 15 min,  1 user,  load average: 0.33, 0.24, 0.19

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -14,30 +14,30 @@ int main(int argc ,char** argv, char** env)
 	int count=0;
 	VerilatedContext* contextp = new VerilatedContext;
 	contextp->commandArgs(argc,argv);
-	Vysyx_23060111_top *ysyx_23060111_top = new Vysyx_23060111_top{contextp};
+	Vysyx_23060111_top *top = new Vysyx_23060111_top{contextp};
 
 	VerilatedVcdC* tfp=new VerilatedVcdC;
 	contextp->traceEverOn(true);
-	ysyx_23060111_top->trace(tfp,0);
+	top->trace(tfp,0);
 	tfp->open("wave.vcd");
 
 	//init mem
 	init_mem();
         uint32_t a=0x80000000;
-	ysyx_23060111_top->pc=a;
+	top->pc=a;
 
 	while(count<=1&&!contextp->gotFinish())
 	{
-		ysyx_23060111_top->inst =pc_read(ysyx_23060111_top->pc);
-		printf("------%x\n",ysyx_23060111_top->pc);
-		printf("------top->b %x\n",ysyx_23060111_top->inst);
-		ysyx_23060111_top->eval();
+		top->inst =pc_read(top->pc);
+		printf("------%x\n",top->pc);
+		printf("------top->b %x\n",top->inst);
+		top->eval();
 
 		tfp->dump(contextp->time());
 		contextp->timeInc(1);
 		count++;
 	}
-	delete ysyx_23060111_top;
+	delete top;
 	tfp->close();
 	delete contextp;
 	return 0;
