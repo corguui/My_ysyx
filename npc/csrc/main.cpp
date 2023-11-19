4cc7c1210d059bc8e9ac0c27996e1a98ea060e8c
tracer-ysyx <tracer@ysyx.org>
2023-11-19 23:56:51 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:56:51 up  3:45,  1 user,  load average: 0.19, 0.18, 0.11

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -15,7 +15,7 @@ int main(int argc ,char** argv, char** env)
 	VerilatedContext* contextp = new VerilatedContext;
 	contextp->commandArgs(argc,argv);
 	Vysyx_23060111_top *top = new Vysyx_23060111_top{contextp};
-        Vysyx_23060111_top *trigger = new Vysyx_23060111_top{contextp};
+        Vysyx_23060111_top *EXU = new Vysyx_23060111_top{contextp};
 
 	VerilatedVcdC* tfp=new VerilatedVcdC;
 	contextp->traceEverOn(true);
@@ -32,11 +32,11 @@ int main(int argc ,char** argv, char** env)
 		top->inst =pc_read(top->pc);
 		if(count==2)
 		{
-		trigger->rst=1;
+		EXU->rst=1;
 		}
 		else
 		{
-		trigger->rst=0;
+		EXU->rst=0;
 		}
 		top->clk =0; top->eval();
 		top->clk =1; top->eval();
