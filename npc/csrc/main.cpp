2531aa3c32e4d3c6f8f19adb7deef74376e91dce
tracer-ysyx <tracer@ysyx.org>
2023-11-20 00:44:13 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:44:13 up  4:32,  1 user,  load average: 0.06, 0.11, 0.09

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -26,18 +26,16 @@ int main(int argc ,char** argv, char** env)
         uint32_t a=0x80000000;
 	top->pc=a;
 
-        Vysyx_23060111_top__Syms* syms= top->ysyx_23060111_top__DOT__ysyx_23060111_EXU->__VlSymsp;
-
 	while(count<=3&&!contextp->gotFinish())
 	{
 		top->inst =pc_read(top->pc);
 		if(count==2)
 		{
-		syms->ysyx_23060111_top__ysyx_23060111_top__DOT__ysyx_2306011_EXU__DOT__rst=1;
+		top->ysyx_23060111_top__DOT__ysyx_23060111_EXU->rst=1;
 		}
 		else
 		{
-		syms->ysyx_23060111_top__ysyx_23060111_top__DOT__ysyx_2306011_EXU__DOT__rst=0;
+		top->ysyx_23060111_top__DOT__ysyx_23060111_EXU->rst=0;
 		}
 		top->clk =0; top->eval();
 		top->clk =1; top->eval();
