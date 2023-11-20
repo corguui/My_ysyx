152212c8cf0667e5bf0faa94b98bfe21bd25b782
tracer-ysyx <tracer@ysyx.org>
2023-11-20 22:16:11 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:16:11 up  2:54,  1 user,  load average: 0.12, 0.13, 0.09

--- npc/vsrc/ysyx_23060111_top.v
+++ npc/vsrc/ysyx_23060111_top.v
@@ -1,4 +1,4 @@
-import "DPI-C" function void add(input int inst);
+import "DPI-C" function int add(input int inst);
 module ysyx_23060111_top(
   input       rst,
   input       clk,
@@ -21,6 +21,7 @@ module ysyx_23060111_top(
 
 assign val=inst;
 assign snpc=pc+32'h4;
+
 //break
 always @(*)
 begin 
