61b4f93470a2e9f7e7eb3384eac12cc14ef989d2
tracer-ysyx <tracer@ysyx.org>
2023-11-20 19:59:33 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:59:33 up 40 min,  1 user,  load average: 0.20, 0.16, 0.16

--- npc/vsrc/ysyx_23060111_EXU.v
+++ npc/vsrc/ysyx_23060111_EXU.v
@@ -7,13 +7,14 @@ module ysyx_23060111_EXU(
   input[14:12] funct3,
   input[11:7] rd,
   input[6:0] opcode,
+  output[31:0] pc,
   output[31:0] reg_out,
   output[31:0] reg_out1,
-  output[19:15] out
+  output[20:0] out
 );
   reg [31:0] imm_32;
   assign imm_32={20'h00000,imm};
-  assign out =rd+rs1+funct3+opcode;
+  assign out ={rd,rs1,funct3,opcode};
  ysyx_23060111_trigger #(32,32'h80000000) renew_pc(clk,rst,dnpc,pc,1'b1);
  ysyx_23060111_reg #(1,32) reg_src1(clk,0,rs1[15:15],1'b0,reg_out);
  ysyx_23060111_reg #(1,32) reg_rd(clk,reg_out+imm_32,rd[7:7],1'b1,reg_out1);
