bb45b5242761ceedf4ec8d5ec02baef98f397437
tracer-ysyx <tracer@ysyx.org>
2023-12-02 10:01:05 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:01:05 up 44 min,  1 user,  load average: 0.15, 0.16, 0.17

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -102,7 +102,7 @@ int strcmp(const char *s1, const char *s2) {
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-	while(n!=0||*s1!='\0'||*s2!='\0')
+	while(n!=0&&(*s1!='\0'||*s2!='\0'))
 	{
 		if(*s1>*s2)
 		{
