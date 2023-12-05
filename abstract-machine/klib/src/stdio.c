8e526a20b3afc346caf8622dbae17cf9bcbb71fb
tracer-ysyx <tracer@ysyx.org>
2023-12-05 08:56:31 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:56:31 up  9:41,  1 user,  load average: 0.17, 0.15, 0.12

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -15,11 +15,10 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 
 int sprintf(char *out, const char *fmt, ...) {
 	   va_list ap;
-	   size_t n=0;
+	   int n;
            va_start(ap, fmt);
-	   vsprintf(out,fmt,ap);
+	   n=vsprintf(out,fmt,ap);
            va_end(ap);
-	   n=strlen(out);
            return n;
 }
 
