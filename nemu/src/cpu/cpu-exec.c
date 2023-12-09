ca0ed746009473b81554fc0975104145896c879a
tracer-ysyx <tracer@ysyx.org>
2023-12-09 13:30:33 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:30:33 up  3:52,  1 user,  load average: 0.00, 0.01, 0.00

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -32,7 +32,7 @@
 #define BUF_LEN 10
 #define NEXT_POS(x) ((x+1)%BUF_LEN)
 char* ringbuf[BUF_LEN];
-int r=0,w=0;//ringbuf's read and write flag
+int w=0;//ringbuf's read and write flag
 void iringbuf_put_char(char *p);
 
 CPU_state cpu = {};
@@ -83,6 +83,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   memset(p, ' ', space_len);
   p += space_len;
   printf("----%s\n",s->logbuf);
+  if(s->pc!=0x80000000)
   iringbuf_put_char(s->logbuf);
 
 #ifndef CONFIG_ISA_loongarch32r
