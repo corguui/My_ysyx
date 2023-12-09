ce83df0fc2e1a6567644f7896e44ae734f4575aa
tracer-ysyx <tracer@ysyx.org>
2023-12-09 16:57:46 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:57:46 up  6:42,  1 user,  load average: 0.01, 0.02, 0.00

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -104,7 +104,7 @@ static void execute(uint64_t n) {
     exec_once(&s, cpu.pc);
     g_nr_guest_inst ++;
     trace_and_difftest(&s, cpu.pc);
-    if (nemu_state.state != NEMU_RUNNING) break;
+    if (nemu_state.state != NEMU_RUNNING){print_ringbuf(); break;}; 
     IFDEF(CONFIG_DEVICE, device_update());
   }
 }
@@ -149,7 +149,7 @@ void cpu_exec(uint64_t n) {
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
             ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
           nemu_state.halt_pc);
-	  //if(nemu_state.halt_ret !=0)
+	  if(nemu_state.halt_ret !=0)
 	  print_ringbuf();
       // fall through
     case NEMU_QUIT: statistic();
