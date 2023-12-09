872423ff93ea01aa86a117fc2c50981f594ac35e
tracer-ysyx <tracer@ysyx.org>
2023-12-09 13:21:35 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:21:35 up  3:43,  1 user,  load average: 0.00, 0.02, 0.00

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -165,7 +165,7 @@ void iringbuf_put_char(char *p)
 	if(!(r==NEXT_POS(w)))
 	{
 		//int n=sizeof(ringbuf[w]);
-		strcpy(ringbuf[w],p);
+		//strcpy(ringbuf[w],p);
 		ringbuf[w]=p;
 		w=NEXT_POS(w);
 	}
