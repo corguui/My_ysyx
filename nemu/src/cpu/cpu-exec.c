ccdc100933e15217ecd51071e288b96c03e445b8
tracer-ysyx <tracer@ysyx.org>
2023-12-09 15:53:04 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:53:04 up  5:37,  1 user,  load average: 0.01, 0.01, 0.00

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -28,7 +28,7 @@
 #define MAX_INST_TO_PRINT 10
 
 //ringbuf val
-#define BUF_LEN 10
+#define BUF_LEN 16
 #define NEXT_POS(x) ((x+1)%BUF_LEN)
 char ringbuf[BUF_LEN][128];
 int w=0;//ringbuf's read and write flag
