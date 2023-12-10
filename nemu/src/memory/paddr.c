0536a50db65aaca1e7f93a7a991214b5e0a28ac6
tracer-ysyx <tracer@ysyx.org>
2023-12-10 22:00:25 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:00:25 up  1:36,  1 user,  load average: 0.15, 0.15, 0.11

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -44,7 +44,7 @@ static void pmem_write(paddr_t addr, int len, word_t data) {
 static void out_of_bound(paddr_t addr) {
   for(int i=0;i<write_num;i++)
   {
-  	printf("----  %u\n",write_buf[i]);
+  	printf("----  %x\n",write_buf[i]);
   }
   panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
       addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
