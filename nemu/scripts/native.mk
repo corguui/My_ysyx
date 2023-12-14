dc9e64bac153fb572e66022ac7e544a1373bc9c0
tracer-ysyx <tracer@ysyx.org>
2023-12-14 20:50:07 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:50:07 up  6:54,  1 user,  load average: 0.18, 0.17, 0.13

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -42,6 +42,7 @@ NEMU_EXEC_RUN := $(BINARY) $(ARGS_RUN) $(IMG)
 run-env: $(BINARY) $(DIFF_REF_SO)
 
 run: run-env
+	echo "!!!$(ELF_FILE)"
 	echo "---$(ARGS_RUN)"
 	$(call git_commit, "run NEMU")
 	echo $(NEMU_EXEC_RUN)
