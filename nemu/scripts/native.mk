87dddb1b4510b8f033a19910ca0d045a19d4a9a1
tracer-ysyx <tracer@ysyx.org>
2023-12-14 20:49:11 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:49:11 up  6:53,  1 user,  load average: 0.08, 0.15, 0.12

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -24,11 +24,10 @@ $(BINARY): compile_git
 
 # Some convenient rules
 
+override ARGS_RUN ?= --batch=$(BUILD_DIR)/nemu-log.txt 
 ifdef CONFIG_FTRACE
 ELF_FILE=$(subst bin,elf,$(IMG))
-override ARGS_RUN ?= --batch=$(BUILD_DIR)/nemu-log.txt $(ELF_FILE) 
-else
-override ARGS_RUN ?= --batch=$(BUILD_DIR)/nemu-log.txt 
+override ARGS_RUN += $(ELF_FILE) 
 endif
 override ARGS_GDB ?= --log=$(BUILD_DIR)/nemu-log.txt
 override ARGS_RUN += $(ARGS_DIFF)
