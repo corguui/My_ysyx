9cbd4753a428e266720c2ebd36f76902216c0e2d
tracer-ysyx <tracer@ysyx.org>
2023-12-14 21:01:01 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:01:01 up  7:05,  1 user,  load average: 0.08, 0.13, 0.12

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -86,7 +86,7 @@ static int parse_args(int argc, char *argv[]) {
       		#ifdef CONFIG_FTRACE
 		elf_file = strtok(optarg," ");	
 		#endif
-		printf("---%s\n",elf_file);
+		printf("---%s\n",img_file);
 		printf("---%s\n",log_file);
 		//sdb_set_batch_mode();
 		break;
