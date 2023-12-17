e1a5d522284048f8204a6087790bf2f32c8baa12
tracer-ysyx <tracer@ysyx.org>
2023-12-17 19:05:35 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:05:35 up 30 min,  1 user,  load average: 0.40, 0.35, 0.30

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -105,7 +105,7 @@ static int parse_args(int argc, char *argv[]) {
     switch (o) {
       case 'b':
 		log_file = optarg; 
-      		//sdb_set_batch_mode();
+      		sdb_set_batch_mode();
 		break;
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
