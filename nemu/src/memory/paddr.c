1afa4d6d6fdc5a111dcdd223a1d9bd45f38b7add
tracer-ysyx <tracer@ysyx.org>
2023-12-17 19:11:23 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:11:23 up 36 min,  1 user,  load average: 0.57, 0.51, 0.39

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -20,8 +20,8 @@
 
 #ifdef CONFIG_MTRACE
 //memory tarce
-unsigned int write_buf[1024];
-unsigned int read_buf[1024];
+unsigned int write_buf[2048];
+unsigned int read_buf[2048];
 int write_num=0;
 int read_num=0;
 #endif
