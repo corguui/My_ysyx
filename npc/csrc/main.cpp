1b2bd7b8e2b040cfa3d211be85e5ebe820f92cf9
tracer-ysyx <tracer@ysyx.org>
2023-12-19 20:17:23 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:17:23 up 15:01,  1 user,  load average: 0.24, 0.19, 0.12

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -38,6 +38,7 @@ int main(int argc ,char** argv, char** env)
 
 	while(count<=3&&!contextp->gotFinish())
 	{
+		printf("------%x\n",top->pc);
 		top->inst =pc_read(top->pc);
 		if(count==2)
 		{
@@ -50,7 +51,6 @@ int main(int argc ,char** argv, char** env)
 		top->clk =0; top->eval();
 		top->clk =1; top->eval();
 
-		printf("------%x\n",top->pc);
 		printf("------top->reg_out1 %x\n",top->reg_out1);
 		printf("------top->imm %x\n",top->imm);
 		printf("------top->val %x\n",top->inst);
