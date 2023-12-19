78b3a94849a7f14da1b230e17664371f7aedd88a
tracer-ysyx <tracer@ysyx.org>
2023-12-19 09:12:59 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:12:59 up  6:27,  1 user,  load average: 0.40, 0.25, 0.20

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -97,7 +97,7 @@ else if(strncmp(s->funbuf+24,ar1,3)==0)
 		printf("2\n");
 		strcpy(fun_str,s->funbuf+34);
 		uint32_t call_pc;
-		sprintf(fun_str,"%08x",call_pc);
+		sscanf(fun_str,"%08x",&call_pc);
 		printf("call %d\n",call_pc);
  }
 
