17347d7a49d23769c5a45edab133b4dc0448dffe
tracer-ysyx <tracer@ysyx.org>
2023-12-22 17:03:01 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:03:01 up  7:38,  1 user,  load average: 0.12, 0.16, 0.12

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -214,6 +214,7 @@ void elf_read(char *elf_file)
 {
 	//elf_read_strtab(elf_file);
 	elf_read_fun(elf_file);
+   Log("read elf file: %s",elf_file ?elf_file:"stdout");
 }
 void elf_read_strtab(char *elf_file) {
 	FILE* fp;
