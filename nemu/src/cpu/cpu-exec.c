96ac96a6a3c028d095a3cddfe993072c0b62f34a
tracer-ysyx <tracer@ysyx.org>
2023-12-26 20:46:30 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:46:30 up  4:19,  1 user,  load average: 0.11, 0.14, 0.09

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -247,7 +247,8 @@ void cpu_exec(uint64_t n) {
 
 	#ifdef CONFIG_ITRACE
 	  //print the ringbuf
-	  if(nemu_state.halt_ret !=0||nemu_state.state==NEMU_ABORT);
+	  if(nemu_state.halt_ret ==0);
+	  else
 	  print_ringbuf();
 	#endif
 
