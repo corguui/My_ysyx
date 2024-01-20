958f003a2cd2f4fef131d872974940661f35e6f7
tracer-ysyx <tracer@ysyx.org>
2024-01-20 11:05:53 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:05:53 up 50 min,  1 user,  load average: 0.27, 0.20, 0.19

--- npc/csrc/mem.cpp
+++ npc/csrc/mem.cpp
@@ -61,7 +61,7 @@ static int parse_args(int argc ,char *argv[])
       {0          , 0                , NULL,  0 },
     };
     int o;                 
-    while ( (o = getopt_long(argc, argv, "h", table, NULL)    ) != -1) {
+    while ( (o = getopt_long(argc, argv, "-h", table, NULL)    ) != -1) {
       switch (o) {         
         case 1:   img_file =optarg; return 0;         
 	default :
