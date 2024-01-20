c0d1c4e34eb01a8f0da9a9df60cbc91fe16cdce0
tracer-ysyx <tracer@ysyx.org>
2024-01-20 15:08:34 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:08:34 up  3:51,  1 user,  load average: 0.37, 0.20, 0.18

--- npc/obj_dir/Vysyx_23060111_top.mk
+++ npc/obj_dir/Vysyx_23060111_top.mk
@@ -35,7 +35,7 @@ VM_PREFIX = Vysyx_23060111_top
 VM_MODPREFIX = Vysyx_23060111_top
 # User CFLAGS (from -CFLAGS on Verilator command line)
 VM_USER_CFLAGS = \
-	"word" \
+	word \
 
 # User LDLIBS (from -LDFLAGS on Verilator command line)
 VM_USER_LDLIBS = \
