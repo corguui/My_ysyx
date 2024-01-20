1b67040e5cd10ca85716981474218becadb89c62
tracer-ysyx <tracer@ysyx.org>
2024-01-20 14:22:39 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:22:39 up  3:05,  1 user,  load average: 0.17, 0.20, 0.18

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -21,7 +21,7 @@ void ebreak (int inst)
 
 int main(int argc ,char** argv, char** env)
 {
-	printf("%s\n",argv[1]);
+	printf("%s\n",argv[0]);
 	for (int i=0;i<argc;i++)
 	{
 		printf("---%s\n",argv[i]);
