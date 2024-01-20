500c2334e810adf8d891e2a8ad0952b63046e5e1
tracer-ysyx <tracer@ysyx.org>
2024-01-20 17:25:53 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:25:53 up  6:09,  1 user,  load average: 0.13, 0.15, 0.14

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -22,7 +22,7 @@ void ebreak (int inst)
 int main(int argc ,char** argv, char** env)
 {
 	//init
-	printf("%s\n",IMG);
+	printf("%s\n",(char *)IMG);
 	int count=0;
 	VerilatedContext* contextp = new VerilatedContext;
 	contextp->commandArgs(argc,argv);
