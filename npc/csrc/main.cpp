ed40d90126ce3914c88aac3e662a65b84e7a7090
tracer-ysyx <tracer@ysyx.org>
2024-01-22 08:42:32 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:42:32 up 26 min,  1 user,  load average: 0.19, 0.33, 0.37

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -14,6 +14,7 @@ int time=0;
 void cpu_exce_once(VerilatedVcdC* tfp)
 
 VerilatedContext* contextp = new VerilatedContext;
+contextp->commandArgs(argc,argv);
 Vysyx_23060111_top *top = new Vysyx_23060111_top{contextp};
 VerilatedVcdC* tfp=new VerilatedVcdC;
 
@@ -32,7 +33,6 @@ int main(int argc ,char** argv, char** env)
 	//init
 	printf("%s\n",IMG);
 	int count=0;
-	contextp->commandArgs(argc,argv);
 	contextp->traceEverOn(true);
 	top->trace(tfp,0);
 	tfp->open("wave.vcd");
