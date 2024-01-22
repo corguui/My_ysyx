ffffbe832c382a596d01592b4a8e78db6122e027
tracer-ysyx <tracer@ysyx.org>
2024-01-22 20:05:43 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:05:43 up  4:15,  1 user,  load average: 0.22, 0.19, 0.17

--- npc/vsrc/ysyx_23060111_EXU.v
+++ npc/vsrc/ysyx_23060111_EXU.v
@@ -1,32 +1,64 @@
 module ysyx_23060111_EXU(
   input       clk,
   input       rst,
-  output[31:0] dnpc,
-  input[31:20] imm,
+  output reg[31:0] dnpc,
+  input reg[31:0] pc,
+  input reg[31:0] snpc,
+  input reg[31:20] imm,
   input[19:15] rs1,
   input[14:12] funct3,
   input[11:7] rd,
   input[6:0] opcode,
   output[31:0] pc,
   output[20:1] rbb,
-  input[31:0] rout,
-  output[31:0] wdata,
-  output[4:0] waddr,
-  output[4:0] raddr,
-  output wen
+  input  reg[31:0] rout,
+  output reg[31:0] wdata,
+  output reg[4:0] waddr,
+  output reg[4:0] raddr,
+  output reg wen
 );
   reg [31:0] imm_32;
   assign rbb ={rd,rs1,funct3,opcode};
   ysyx_23060111_trigger #(32,32'h80000000) renew_pc(clk,rst,dnpc,pc,1'b1);
+	
+  always(posedge clk)
+  {
+  	casex({imm,rs1,funct3,rd,opcode})
+	//addi
+	32'b?????????????????000?????0010011:
+	imm_32={20'h00000,imm};
+	raddr=rs1[19:15];
+  	wdata=rout+imm_32;
+  	waddr=rd[11:7];
+  	wen=1'b1;
+	//auipc
+	32'b?????????????????????????0010111:
+	waddr=rd[11:7]+{imm,rs1,funct3};	
+	wen=1'b1;//write
+	32'b?????????????????????????0110111:
+	waddr=rd[11:7]+{imm,rs1,funct3};
+	wen=1'b1;
+	32'b?????????????????000?????1100111:
+	waddr=rd[11:7];
+	wdata=snpc;
+	dnpc=imm+rs1;
+	wen=1'b1;
+	32'b?????????????????????????1101111:
+	waddr=rd[11:7];
+	wdata=snpc;
+	dnpc={imm,rs1,funct3}+pc;
+	wen=1'b1;
+	endcase
 
+
+  }
   //addi
-  assign imm_32={20'h00000,imm};
-  assign dnpc=32'h80000004;
-  assign raddr=rs1[19:15];
-  assign wdata=rout+imm_32;
-  assign waddr=rd[11:7];
-  assign wen=1'b1;
-
+  //assign imm_32={20'h00000,imm};
+  //assign raddr=rs1[19:15];
+  //assign wdata=rout+imm_32;
+  //assign waddr=rd[11:7];
+  //assign wen=1'b1;
+  //assign dnpc=32'h80000004;
 
 endmodule
   
--- npc/vsrc/ysyx_23060111_top.v
+++ npc/vsrc/ysyx_23060111_top.v
@@ -30,83 +30,50 @@ assign wdata=0;
 assign waddr=5'd0;
 assign raddr=5'd0;
 assign wen=1'b1;
-/*
-ysyx_23060111_reg #(5,32) reg_$1(clk,0,5'd1,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$2(clk,0,5'd2,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$3(clk,0,5'd3,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$4(clk,0,5'd4,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$5(clk,0,5'd5,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$6(clk,0,5'd6,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$7(clk,0,5'd7,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$8(clk,0,5'd8,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$9(clk,0,5'd9,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$10(clk,0,5'd10,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$11(clk,0,5'd11,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$12(clk,0,5'd12,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$13(clk,0,5'd13,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$14(clk,0,5'd14,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$15(clk,0,5'd15,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$16(clk,0,5'd16,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$17(clk,0,5'd17,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$18(clk,0,5'd18,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$19(clk,0,5'd19,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$20(clk,0,5'd20,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$21(clk,0,5'd21,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$22(clk,0,5'd22,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$23(clk,0,5'd23,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$24(clk,0,5'd24,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$25(clk,0,5'd25,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$26(clk,0,5'd26,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$27(clk,0,5'd27,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$28(clk,0,5'd28,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$29(clk,0,5'd29,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$30(clk,0,5'd30,1'b1,out);
-ysyx_23060111_reg #(5,32) reg_$31(clk,0,5'd31,1'b1,out);
-*/
 
 
 
- //init IDU
- ysyx_23060111_IDU init_IDU (snpc,inst,dnpc,imm,funct3,rs1,rd,opcode);
+ //init idu
+ ysyx_23060111_idu init_idu (snpc,inst,dnpc,imm,funct3,rs1,rd,opcode);
 
- //init EXU
- ysyx_23060111_EXU init_EXU (clk,rst,dnpc,imm,rs1,funct3,rd,opcode,pc,rbb,rout,wdata,waddr,raddr,wen);
+ //init exu
+ ysyx_23060111_exu init_exu (clk,rst,dnpc,pc,snpc,imm,rs1,funct3,rd,opcode,pc,rbb,rout,wdata,waddr,raddr,wen);
 endmodule
 
 
 
 /*
-module ysyx_23060111_MuxKeyInternal #(NR_KEY = 2, KEY_LEN = 1, DATA_LEN = 1, HAS_DEFAULT = 0) (
-  output reg [DATA_LEN-1:0] out,
-  input [KEY_LEN-1:0] key,
-  input [DATA_LEN-1:0] default_out,
-  input [NR_KEY*(KEY_LEN + DATA_LEN)-1:0] lut
+module ysyx_23060111_muxkeyinternal #(nr_key = 2, key_len = 1, data_len = 1, has_default = 0) (
+  output reg [data_len-1:0] out,
+  input [key_len-1:0] key,
+  input [data_len-1:0] default_out,
+  input [nr_key*(key_len + data_len)-1:0] lut
 );
 
-  localparam PAIR_LEN = KEY_LEN + DATA_LEN;
-  wire [PAIR_LEN-1:0] pair_list [NR_KEY-1:0];
-  wire [KEY_LEN-1:0] key_list [NR_KEY-1:0];
-  wire [DATA_LEN-1:0] data_list [NR_KEY-1:0];
+  localparam pair_len = key_len + data_len;
+  wire [pair_len-1:0] pair_list [nr_key-1:0];
+  wire [key_len-1:0] key_list [nr_key-1:0];
+  wire [data_len-1:0] data_list [nr_key-1:0];
 
   generate
-    for (genvar n = 0; n < NR_KEY; n = n + 1) begin
-      assign pair_list[n] = lut[PAIR_LEN*(n+1)-1 : PAIR_LEN*n];
-      assign data_list[n] = pair_list[n][DATA_LEN-1:0];
-      assign key_list[n]  = pair_list[n][PAIR_LEN-1:DATA_LEN];
+    for (genvar n = 0; n < nr_key; n = n + 1) begin
+      assign pair_list[n] = lut[pair_len*(n+1)-1 : pair_len*n];
+      assign data_list[n] = pair_list[n][data_len-1:0];
+      assign key_list[n]  = pair_list[n][pair_len-1:data_len];
     end
   endgenerate
 
-  reg [DATA_LEN-1 : 0] lut_out;
+  reg [data_len-1 : 0] lut_out;
   reg hit;
   integer i;
   always @(*) begin
     lut_out = 0;
     hit = 0;
-    for (i = 0; i < NR_KEY; i = i + 1) begin
-      lut_out = lut_out | ({DATA_LEN{key == key_list[i]}} & data_list[i]);
+    for (i = 0; i < nr_key; i = i + 1) begin
+      lut_out = lut_out | ({data_len{key == key_list[i]}} & data_list[i]);
       hit = hit | (key == key_list[i]);
     end
-    if (!HAS_DEFAULT) out = lut_out;
+    if (!has_default) out = lut_out;
     else out = (hit ? lut_out : default_out);
   end
 endmodule
