310e2515aa003e5f2f7d4cf2a75d1377e7c1ec60
tracer-ysyx <tracer@ysyx.org>
2024-01-22 10:29:25 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:29:25 up  2:11,  1 user,  load average: 0.13, 0.17, 0.17

--- npc/obj_dir/Vysyx_23060111_top__Trace__0__Slow.cpp
+++ npc/obj_dir/Vysyx_23060111_top__Trace__0__Slow.cpp
@@ -92,7 +92,6 @@ VL_ATTR_COLD void Vysyx_23060111_top___024root__trace_init_sub__TOP__0(Vysyx_230
     tracep->declBit(c+25,"wen", false,-1);
     tracep->popNamePrefix(2);
     tracep->pushNamePrefix("init_IDU ");
-    tracep->declBus(c+5,"snpc", false,-1, 31,0);
     tracep->declBus(c+3,"inst", false,-1, 31,0);
     tracep->declBus(c+6,"dnpc", false,-1, 31,0);
     tracep->declBus(c+8,"imm", false,-1, 31,20);
