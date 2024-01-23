794663f68438006ecfd211c85c205b4c1a79385c
tracer-ysyx <tracer@ysyx.org>
2024-01-23 17:26:40 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:26:40 up  7:50,  1 user,  load average: 0.13, 0.19, 0.18

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -85,14 +85,14 @@ void cpu_init()
 void cpu_exce_once(VerilatedVcdC* tfp)
 {
 		//top->snpc=top->pc;
-		top->inst =pc_read(top->snpc);
 		//top->dnpc=top->snpc;
 
-		top->clk =1; top->eval();
+		top->clk =0; top->eval();
+		top->inst =pc_read(top->snpc);
 		tfp->dump(main_time);
 		main_time++;
 		top->eval();
-		top->clk =0; top->eval();
+		top->clk =1; top->eval();
 		tfp->dump(main_time);
 		main_time++;
 		top->eval();
