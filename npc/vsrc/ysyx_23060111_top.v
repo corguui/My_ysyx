6565b63e52f3abb4716646176846c486511d3cc6
tracer-ysyx <tracer@ysyx.org>
2024-01-23 16:13:07 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:13:07 up  6:36,  1 user,  load average: 0.13, 0.17, 0.12

--- npc/vsrc/ysyx_23060111_top.v
+++ npc/vsrc/ysyx_23060111_top.v
@@ -4,11 +4,11 @@ module ysyx_23060111_top(
   input       clk,
   input[31:0] inst,
   output[31:0] snpc,
-  output[31:0] dnpc,
-  output[9:0] rbb,
-  output[31:0] pc
+  output[9:0] rbb
   );
 
+  wire[31:0] pc;
+  wire[31:0] dnpc;
   wire[31:20] imm;
   wire[19:15] rs1;
   wire[14:12] funct3;
