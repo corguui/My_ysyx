01cb67b1c25a2d3384053479ec50a85f3223ce5c
tracer-ysyx <tracer@ysyx.org>
2024-01-23 19:52:14 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:52:14 up  8:54,  1 user,  load average: 0.18, 0.21, 0.18

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -69,12 +69,12 @@ int main(int argc ,char** argv, char** env)
 void cpu_init()
 {
 	top->pc=0x80000000;
-	top->rst=0;
+	top->rst=1;
 	top->clk =1; top->eval();
 	tfp->dump(main_time);
 	main_time++;
 	top->eval();
-	top->clk =1; top->eval();
+	top->clk =0; top->eval();
 	top->rst=0;
 	tfp->dump(main_time);
 	main_time++;
