54df11050ade4aab39f1d5f21f492a4c641f794c
tracer-ysyx <tracer@ysyx.org>
2024-01-24 10:46:15 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:46:15 up 13:24,  1 user,  load average: 0.14, 0.14, 0.15

--- npc/vsrc/ysyx_23060111_EXU.v
+++ npc/vsrc/ysyx_23060111_EXU.v
@@ -1,28 +1,47 @@
 module ysyx_23060111_EXU(
   output[31:0] dnpc,
+  input [31:0] pc,
   input [31:0] snpc,
-  input[31:20] imm,
-  input[19:15] rs1,
-  input[14:12] funct3,
-  input[11:7] rd,
   input[6:0] opcode,
-  input[31:0] rout,
+  input[11:7] rd,
+  input[14:12] funct3,
+  input[19:15] rs1,
+  input[24:20] rs2,
+  input[31:25] funct7,
+  input[3:0] type,
+  input[31:0] imm,
   output[31:0] wdata,
   output[4:0] waddr,
   output[4:0] raddr,
-  output[9:0] rbb,
-  output wen
+  output[31:0] rout,
+  output wen,
+  output[31:0] rbb
 );
-  wire [31:0] imm_32;
-  assign rbb={funct3,opcode};
+	
+  assign waddr=rd[11:7]; //R(rd)
+  assign raddr=rs1[19:15]; //src1
+  assign rbb={opcode,rd,rs1,funct3,rs1,rs2,funct7};//rbb
 
-  //addi
-  assign imm_32={20'h00000,imm};
-  assign raddr=rs1[19:15];
-  assign wdata=rout+imm_32;
-  assign waddr=rd[11:7];
-  assign wen=1'b1;
-  assign dnpc=snpc;
+
+  always @(type)
+	begin
+	case(type)
+	//auipc  UPC
+	4'd1: wdata=pc+imm;    wen=1'b1;         dnpc=snpc;	
+	//lui    U
+	4'd2: wdata=imm;       wen=1'b1;         dnpc=snpc;
+	//jal    J
+	4'd3: wdata=snpc;      wen=1'b1;         dnpc=pc+imm;
+	//addi   I
+	4'd4: wdata=rout+imm;  wen=1'b1;         dnpc=snpc;
+	//jalr   JR
+	4'd5: wdata=snpc;      wen=1'b1;         dnpc=imm+rout;
+	default: dnpc=32'h00000000;
+	       
+
+	endcase
+	
+	end
 
 endmodule
   
