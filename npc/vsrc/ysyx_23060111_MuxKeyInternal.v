19a74e77b938df92d119153aa56853898cf2254a
tracer-ysyx <tracer@ysyx.org>
2024-01-24 10:54:45 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:54:45 up 13:33,  1 user,  load average: 0.25, 0.20, 0.18

--- npc/vsrc/ysyx_23060111_MuxKeyInternal.v
+++ npc/vsrc/ysyx_23060111_MuxKeyInternal.v
@@ -40,7 +40,7 @@ module MuxKey #(NR_KEY = 2, KEY_LEN = 1, DATA_LEN = 1) (
   input [KEY_LEN-1:0] key,
   input [NR_KEY*(KEY_LEN + DATA_LEN)-1:0] lut
 );
-  MuxKeyInternal #(NR_KEY, KEY_LEN, DATA_LEN, 0) i0 (out, key, {DATA_LEN{1'b0}}, lut);
+  ysyx_23060111_MuxKeyInternal #(NR_KEY, KEY_LEN, DATA_LEN, 0) i0 (out, key, {DATA_LEN{1'b0}}, lut);
 endmodule
 
 // 带默认值的选择器模板
@@ -50,5 +50,5 @@ module MuxKeyWithDefault #(NR_KEY = 2, KEY_LEN = 1, DATA_LEN = 1) (
   input [DATA_LEN-1:0] default_out,
   input [NR_KEY*(KEY_LEN + DATA_LEN)-1:0] lut
 );
-  MuxKeyInternal #(NR_KEY, KEY_LEN, DATA_LEN, 1) i0 (out, key, default_out, lut);
+  ysyx_23060111_MuxKeyInternal #(NR_KEY, KEY_LEN, DATA_LEN, 1) i0 (out, key, default_out, lut);
 endmodule
