037cf0182ba256d7bab211c6c04f4b5bb46048e9
tracer-ysyx <tracer@ysyx.org>
2024-01-24 21:09:12 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:09:12 up 20:45,  1 user,  load average: 0.17, 0.21, 0.22

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -8,7 +8,7 @@
 #include"Vysyx_23060111_top__Dpi.h"
 #include"svdpi.h"
 
-#include "../include/mem.h"
+#include <mem.h>
 #include <cpu/cpu.h>
 
 
--- npc/obj_dir/Vysyx_23060111_top.mk
+++ npc/obj_dir/Vysyx_23060111_top.mk
@@ -36,6 +36,7 @@ VM_MODPREFIX = Vysyx_23060111_top
 # User CFLAGS (from -CFLAGS on Verilator command line)
 VM_USER_CFLAGS = \
 	-I/home/corgi/ysyx-workbench/npc/include \
+	-DIMG=\"/home/corgi/ysyx-workbench/am-kernels/tests/cpu-tests/build/dummy-riscv32e-npc.bin\" \
 
 # User LDLIBS (from -LDFLAGS on Verilator command line)
 VM_USER_LDLIBS = \
