7e45f77e94de05096dd416a9e8714a4548b1fedd
tracer-ysyx <tracer@ysyx.org>
2024-01-24 11:10:53 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:10:53 up 13:49,  1 user,  load average: 0.19, 0.16, 0.17

--- npc/vsrc/ysyx_23060111_IDU.v
+++ npc/vsrc/ysyx_23060111_IDU.v
@@ -25,7 +25,7 @@ end
   assign rs2=inst[24:20];
   assign funct7=inst[31:25];
 
-ysyx_23060111_MuxKeyWithDefault #(5, 7, 36) i0 ({tpye_i,imm}, opcode ,36'd0 , {
+ysyx_23060111_MuxKeyWithDefault #(5, 7, 33) i0 ({tpye_i,imm}, opcode ,33'b0 , {
   		      //4'd0    type:Can't find the type
     7'b0010111, {4'b0001,inst[31:12],12'b0}, //auipc   type:UPC   4'd1
     7'b0110111, {4'b0010,inst[31:12],12'b0}, //lui     type:U   4'd2
