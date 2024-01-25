287edd6e03ba1d81972cdabee08e207bb2cbce00
tracer-ysyx <tracer@ysyx.org>
2024-01-25 08:53:03 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:53:03 up 22:47,  1 user,  load average: 0.12, 0.17, 0.17

--- npc/csrc/cpu/cpu.c
+++ npc/csrc/cpu/cpu.c
@@ -1,4 +1,13 @@
 #include <cstdint>
+#include "Vysyx_23060111_top.h"
+#include"verilated.h"
+#include"verilated_vcd_c.h"
+#include"Vysyx_23060111_top__Dpi.h"
+
+extern int main_time;
+extern VerilatedContext* contextp;
+extern Vysyx_23060111_top *top;
+extern VerilatedVcdC* tfp;
 
 
 void cpu_init()
