f83c9211813aba155a9c4280dbefe9758432c94b
tracer-ysyx <tracer@ysyx.org>
2024-01-25 16:03:42 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:03:42 up 1 day,  3:35,  1 user,  load average: 0.22, 0.23, 0.19

--- npc/csrc/utils/state.c
+++ npc/csrc/utils/state.c
@@ -18,11 +18,11 @@ void set_nemu_state(int state,int halt_ret)
 
 void invalid_inst()
 {	
-        printf("invalid opcode (PC = %s)\n",top->pc);
+        printf("invalid opcode (PC = 0x%x)\n",top->pc);
   printf("There are two cases which will trigger this unexpected exception:\n"
-      "1. The instruction at PC = " %s " is not implemented.\n"
+      "1. The instruction at PC = " 0x%x " is not implemented.\n"
       "2. Something is implemented incorrectly.\n", top->pc);
-  printf("Find this PC(" %s ") in the disassembling result to distinguish which case it is.\n\n", top->pc);
+  printf("Find this PC(" 0x%x ") in the disassembling result to distinguish which case it is.\n\n", top->pc);
   printf(ANSI_FMT("If it is the first case, see\n%s\nfor more details.\n\n"
         "If it is the second case, remember:\n"
         "* The machine is always right!\n"
