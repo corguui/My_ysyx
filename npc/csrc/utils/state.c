33a2f701e93756945f05098f3011a821633b30b5
tracer-ysyx <tracer@ysyx.org>
2024-01-25 16:08:01 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:08:01 up 1 day,  3:39,  1 user,  load average: 0.24, 0.19, 0.18

--- npc/csrc/utils/state.c
+++ npc/csrc/utils/state.c
@@ -20,9 +20,9 @@ void invalid_inst()
 {	
         printf("invalid opcode (PC = 0x%x)\n",top->pc);
   printf("There are two cases which will trigger this unexpected exception:\n"
-      "1. The instruction at PC = " 0x%x " is not implemented.\n"
+      "1. The instruction at PC = \" 0x%x \" is not implemented.\n"
       "2. Something is implemented incorrectly.\n", top->pc);
-  printf("Find this PC(" 0x%x ") in the disassembling result to distinguish which case it is.\n\n", top->pc);
+  printf("Find this PC(\" 0x%x \") in the disassembling result to distinguish which case it is.\n\n", top->pc);
   printf(ANSI_FMT("If it is the first case, see\n%s\nfor more details.\n\n"
         "If it is the second case, remember:\n"
         "* The machine is always right!\n"
