fc0fbb0b0a2598659c37876da5a13beb3d993811
tracer-ysyx <tracer@ysyx.org>
2024-01-26 09:33:28 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:33:28 up 47 min,  1 user,  load average: 0.24, 0.20, 0.19

--- npc/csrc/monitor/monitor.c
+++ npc/csrc/monitor/monitor.c
@@ -1,12 +1,11 @@
 #include <common.h>
 #include <stdio.h>
 #include <string.h>
-#include <mem.h>
 
 void init_mem();
 void init_log();
 
-void init_monitor(int argc, char *argv[]) {
+void init_monitor() {
 
         init_log();
 
