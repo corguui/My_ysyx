14dc484ed8d9c66da34ebba39f0414f3ad075846
tracer-ysyx <tracer@ysyx.org>
2024-01-26 16:19:08 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:19:08 up  6:12,  1 user,  load average: 0.45, 0.25, 0.20

--- npc/obj_dir/Vysyx_23060111_top.mk
+++ npc/obj_dir/Vysyx_23060111_top.mk
@@ -40,10 +40,6 @@ VM_USER_CFLAGS = \
 	-I/usr/lib/llvm-14/include \
 	-std=c++14 \
 	-fno-exceptions \
-	-D_GNU_SOURCE \
-	-D__STDC_CONSTANT_MACROS \
-	-D__STDC_FORMAT_MACROS \
-	-D__STDC_LIMIT_MACROS \
 	-fPIE \
 
 # User LDLIBS (from -LDFLAGS on Verilator command line)
