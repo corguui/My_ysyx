9f729befb9c84daf7fddd4f3af32728851e764a6
tracer-ysyx <tracer@ysyx.org>
2024-01-26 15:49:11 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:49:11 up  5:42,  1 user,  load average: 0.16, 0.17, 0.17

--- npc/csrc/cpu/cpu.c
+++ npc/csrc/cpu/cpu.c
@@ -71,11 +71,9 @@ void cpu_exec_once(VerilatedVcdC* tfp,Decode *s)
   space_len = space_len * 3 + 1;
   memset(p, ' ', space_len);
   p += space_len;
-  p[0] = '\0'; // the upstream llvm does not support loongarch32r
-  /*
+  //p[0] = '\0'; // the upstream llvm does not support loongarch32r
  void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);                                                                            
  disassemble(p, s->logbuf + sizeof(s->logbuf) - p,s->pc, (uint8_t *)&s->inst, space_len);
-*/
 
 #endif
 
