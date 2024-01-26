c0cf8746ab35e664d3925a88fb7f305a7611b8d9
tracer-ysyx <tracer@ysyx.org>
2024-01-26 09:31:33 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:31:33 up 45 min,  1 user,  load average: 0.20, 0.20, 0.20

--- npc/include/utils.h
+++ npc/include/utils.h
@@ -46,16 +46,15 @@ extern NPCState npc_state;
 
 #define CONFIG_TARGET_NATIVE_ELF 1
 #define log_write(...) IFDEF(CONFIG_TARGET_NATIVE_ELF, \
-   do { \         
-     extern FILE* log_fp; \
-     extern bool log_enable(); \                                             
-     if (log_enable()) { \
-       fprintf(log_fp, __VA_ARGS__); \
-       fflush(log_fp); \
-     } \          
-   } while (0) \  
- )               
-
+  do { \
+    extern FILE* log_fp; \
+    extern bool log_enable(); \
+    if (log_enable()) { \
+      fprintf(log_fp, __VA_ARGS__); \
+      fflush(log_fp); \
+    } \
+  } while (0) \
+)
 #define _Log(...) \
   do { \
     printf(__VA_ARGS__); \
