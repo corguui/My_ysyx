d68b1132b1c4406f1ecf28c664dcd6a874a45b87
tracer-ysyx <tracer@ysyx.org>
2024-01-26 16:45:42 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:45:42 up  6:38,  1 user,  load average: 0.30, 0.18, 0.18

--- npc/csrc/cpu/cpu.c
+++ npc/csrc/cpu/cpu.c
@@ -4,6 +4,7 @@
 #include<common.h>
 #include<disasm.h>
 
+void disassemble(char *str, int size, uint64_t pc, uint8_t *code,int nbyte);
 static bool g_print_step = false;  
 
 static void trace_and_difftest(Decode *_this) {
@@ -73,7 +74,6 @@ void cpu_exec_once(VerilatedVcdC* tfp,Decode *s)
   memset(p, ' ', space_len);
   p += space_len;
   //p[0] = '\0'; // the upstream llvm does not support loongarch32r
-void disassemble(char *str, int size, uint64_t pc, uint8_t *code,int nbyte);
 disassemble(p, s->logbuf + sizeof(s->logbuf) - p,s->pc, (uint8_t *)&s->inst, ilen);
 
 #endif
