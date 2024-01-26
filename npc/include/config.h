45703cceb0740f0a1621041b004a926c96a4a09b
tracer-ysyx <tracer@ysyx.org>
2024-01-26 10:55:37 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:55:37 up  2:09,  1 user,  load average: 0.18, 0.16, 0.15

---
+++ npc/include/config.h
@@ -0,0 +1,9 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#define CONFIG_ITRACE 1
+#define CONFIG_TARGET_NATIVE_ELF 1
+#define CONFIG_ITRACE_COND "true"
+
+#endif
+
--- npc/include/utils.h
+++ npc/include/utils.h
@@ -44,7 +44,6 @@ extern NPCState npc_state;
 
 #define ANSI_FMT(str, fmt) fmt str ANSI_NONE
 
-#define CONFIG_TARGET_NATIVE_ELF 1
 #define log_write(...) IFDEF(CONFIG_TARGET_NATIVE_ELF, \
   do { \
     extern FILE* log_fp; \
@@ -59,9 +58,13 @@ extern NPCState npc_state;
   do { \
     printf(__VA_ARGS__); \
   } while (0)
-
-
-
+/*
+#define _Log(...) \
+  do { \
+    printf(__VA_ARGS__); \
+    log_write(__VA_ARGS__); \
+  } while (0)
+*/
 
 #endif
 
