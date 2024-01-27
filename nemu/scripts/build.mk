be2e5d2c9f5c179fd56ae750dd813278ff536f54
tracer-ysyx <tracer@ysyx.org>
2024-01-27 11:52:23 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:52:23 up 14:01,  1 user,  load average: 0.48, 0.35, 0.26

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -32,13 +32,13 @@ $(OBJ_DIR)/%.o: %.c
 	@echo + CC $<
 	@mkdir -p $(dir $@)
 	@$(CC) $(CFLAGS) -LD_PRELOAD -c -o $@ $<
-	@$(CC) $(CFLAGS) -E -MF  /dev/null $< | clang-format > $@.i
+	@$(CC) $(CFLAGS) -LD_PRELOAD -E -MF  /dev/null $< | clang-format > $@.i
 	$(call call_fixdep, $(@:.o=.d), $@)
 
 $(OBJ_DIR)/%.o: %.cc
 	@echo + CXX $<
 	@mkdir -p $(dir $@)
-	@$(CXX) $(CFLAGS) $(CXXFLAGS) -c -o $@ $<
+	@$(CXX) $(CFLAGS) $(CXXFLAGS) -LD_PRELOAD -c -o $@ $<
 	$(call call_fixdep, $(@:.o=.d), $@)
 
 # Depencies
