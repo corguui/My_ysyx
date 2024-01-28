4151127ff1385fb196104526b67fe7d4fb649fb5
tracer-ysyx <tracer@ysyx.org>
2024-01-28 21:45:44 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:45:44 up  6:14,  1 user,  load average: 0.20, 0.18, 0.18

--- npc/csrc/monitor/monitor.c
+++ npc/csrc/monitor/monitor.c
@@ -189,7 +189,7 @@ void elf_read_fun(char *elf_file) {
 	}
 	*/
 	
-    free (sym_entries);
+    //free (sym_entries);
 	free (strtab_string_table);
     } else {
         printf("No symbol table!\n");
