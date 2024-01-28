b325c35fd9afb1a6d0447d98c8b172db1350a696
tracer-ysyx <tracer@ysyx.org>
2024-01-28 21:56:28 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:56:28 up  6:25,  1 user,  load average: 0.48, 0.23, 0.20

--- npc/csrc/monitor/monitor.c
+++ npc/csrc/monitor/monitor.c
@@ -101,15 +101,15 @@ void elf_read_fun(char *elf_file) {
 
 	fseek(fp, sec_headers[symtab_ind].sh_offset, SEEK_SET);//将指针移动到符号表对应的偏移地址
 
-        Elf32_Sym* sym_entries = (Elf32_Sym*)malloc(sizeof(Elf32_Sym)*entry_num);//开辟堆内存用来存储符号表中所有entry
-        int ret3=fread(sym_entries, sizeof(Elf64_Sym)*entry_num,1, fp);//读符号表
+      //  Elf32_Sym* sym_entries = (Elf32_Sym*)malloc(sizeof(Elf32_Sym)*entry_num);//开辟堆内存用来存储符号表中所有entry
+      //  int ret3=fread(sym_entries, sizeof(Elf64_Sym)*entry_num,1, fp);//读符号表
 	assert(ret3==1);
 
 
 	//printf("  NUM:\tValue\t\tSize\tType\tName\n");
 
 	for(int i=0;i<entry_num;i++)
-	{
+	{/*
 	   if((sym_entries[i].st_info & 0x0000000f)==STT_FUNC)
 	   {
  	    printf("  %3d:\t", i);
@@ -136,7 +136,7 @@ void elf_read_fun(char *elf_file) {
 	}
 	*/
 	
-    free (sym_entries);
+    //free (sym_entries);
 	free (strtab_string_table);
     } else {
         printf("No symbol table!\n");
