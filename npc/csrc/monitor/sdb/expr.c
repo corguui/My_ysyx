d2c13236114d6562e7408bb9f6889158324d48f3
tracer-ysyx <tracer@ysyx.org>
2024-01-28 11:52:46 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:52:46 up  2:56,  1 user,  load average: 0.15, 0.16, 0.17

--- npc/csrc/monitor/sdb/expr.c
+++ npc/csrc/monitor/sdb/expr.c
@@ -95,8 +95,6 @@ static bool make_token(char *e) {
   nr_token = 0;
 
   while (e[position] != '\0') {
-
-        printf("------");
     /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
