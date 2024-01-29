c9ee45a7f480a03e720e978c19f0076ed54ab9db
tracer-ysyx <tracer@ysyx.org>
2024-01-29 15:26:36 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:26:36 up 11:35,  1 user,  load average: 0.17, 0.19, 0.18

--- npc/csrc/monitor/monitor.c
+++ npc/csrc/monitor/monitor.c
@@ -16,7 +16,7 @@ char *elf_file =(char*)ELF;
 int times=0;
 int fun_num=0;
 void elf_read(char *elf_file);
-void show_symbol_table(int symtab_ind,uint32_t entry_num,char *string_table,char* elf_file,Elf32_Shdr* sec_headers);
+void show_symbol_table(int symtab_ind,uint32_t entry_num,char *string_table,FILE* fp,Elf32_Shdr* sec_headers);
 void elf_read_fun(char* elf_file);
 /*
 transfer to momitor.h
@@ -104,8 +104,7 @@ void elf_read_fun(char *elf_file) {
 		assert(ret5==1);
 
         uint32_t entry_num = sec_headers[symtab_ind].sh_size / sec_headers[symtab_ind].sh_entsize;
-		fclose(fp);
-		show_symbol_table(symtab_ind,entry_num,strtab_string_table,elf_file,sec_headers);
+		show_symbol_table(symtab_ind,entry_num,strtab_string_table,fp,sec_headers);
 
 		//printf("%d\n",sec_headers[symtab_ind].sh_size);
 		//printf("%d\n",sec_headers[symtab_ind].sh_entsize);
@@ -154,40 +153,36 @@ void elf_read_fun(char *elf_file) {
     //释放堆内存
     free (string_table);
     free (sec_headers);
-    //fclose (fp);	
+    fclose (fp);	
 
 
 
 }
-void show_symbol_table(int symtab_ind,uint32_t entry_num,char *strtab_string_table,char* elf_file,Elf32_Shdr* sec_headers)
+void show_symbol_table(int symtab_ind,uint32_t entry_num,char *strtab_string_table,FILE* fp,Elf32_Shdr* sec_headers)
 {
-	FILE* fp;
-	fp=fopen(elf_file,"r");
 	fseek(fp, sec_headers[symtab_ind].sh_offset, SEEK_SET);//将指针移动到符号表对应的偏移地址
-    //Elf32_Sym* sym_entries = (Elf32_Sym*)malloc(sizeof(Elf32_Sym)*entry_num);//开辟堆内存用来存储符号表中所有entry
-	Elf32_Sym* sym_entries[entry_num];
-    int ret3=fread(sym_entries, sizeof(Elf32_Sym),entry_num, fp);//读符号表
+    Elf32_Sym* sym_entries = (Elf32_Sym*)malloc(sizeof(Elf32_Sym)*entry_num);//开辟堆内存用来存储符号表中所有entry
+    int ret3=fread(sym_entries, sizeof(sym_entries),1, fp);//读符号表
 	//assert(ret3==1);
 
 	for(int i=0;i<entry_num;i++)
 	{
-	   if((sym_entries[i]->st_info & 0x0000000f)==STT_FUNC)
+	   if((sym_entries[i].st_info & 0x0000000f)==STT_FUNC)
 	   {
  	    printf("  %3d:\t", i);
-        printf("0x%08x:\t", sym_entries[i]->st_value);
-        printf("%4d\t", sym_entries[i]->st_size);
+        printf("0x%08x:\t", sym_entries[i].st_value);
+        printf("%4d\t", sym_entries[i].st_size);
 	    printf("FUN\t");
-        printf("%s", &strtab_string_table[sym_entries[i]->st_name]);
+        printf("%s", &strtab_string_table[sym_entries[i].st_name]);
         printf("\n");
-	    fun_buff[fun_num].value=sym_entries[i]->st_value;
-	    fun_buff[fun_num].size=sym_entries[i]->st_size;
-	    strcpy(fun_buff[fun_num].name,&strtab_string_table[sym_entries[i]->st_name]);
+	    fun_buff[fun_num].value=sym_entries[i].st_value;
+	    fun_buff[fun_num].size=sym_entries[i].st_size;
+	    strcpy(fun_buff[fun_num].name,&strtab_string_table[sym_entries[i].st_name]);
 
 	    fun_num++;
 	    }
 	}
-		//free (sym_entries);
-		//sym_entries=NULL;
-	fclose(fp);
+		free (sym_entries);
+		sym_entries=NULL;
 }
 #endif
