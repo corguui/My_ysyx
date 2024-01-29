08d4d9350d5bfee4659ed313104410807b606b30
tracer-ysyx <tracer@ysyx.org>
2024-01-29 15:33:51 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:33:51 up 11:42,  1 user,  load average: 0.12, 0.16, 0.17

--- npc/csrc/monitor/monitor.c
+++ npc/csrc/monitor/monitor.c
@@ -164,6 +164,8 @@ void show_symbol_table(int symtab_ind,uint32_t entry_num,char *strtab_string_tab
     //Elf32_Sym* sym_entries = (Elf32_Sym*)malloc(sizeof(Elf32_Sym)*entry_num);//开辟堆内存用来存储符号表中所有entry
 	Elf32_Sym sym_entries[entry_num];
     int ret3=fread(sym_entries, sizeof(sym_entries),1, fp);//读符号表
+	printf("%x\n",sec_headers[symtab_ind].sh_offset);
+	printf("%lx\n",sizeof(sym_entries));
 	assert(ret3==1);
 
 	for(int i=0;i<entry_num;i++)
