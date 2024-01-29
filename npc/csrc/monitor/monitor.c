2a79b5db842d65df29760a9d7a64380fd5e7f62a
tracer-ysyx <tracer@ysyx.org>
2024-01-29 11:18:08 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:18:08 up 10:03,  1 user,  load average: 0.11, 0.20, 0.18

--- npc/csrc/monitor/monitor.c
+++ npc/csrc/monitor/monitor.c
@@ -106,17 +106,16 @@ void elf_read_fun(char *elf_file) {
 		//printf("%d\n",entry_num);
 		//printf("%ld\n",sizeof(Elf32_Sym));
         fseek(fp, sec_headers[symtab_ind].sh_offset,SEEK_SET );//将指针移动到符号表对应的偏移地址
-		Elf32_Sym* sym_entries=new Elf32_Sym[entry_num];
 		//printf("%ld\n",ftell(fp));
         //Elf32_Sym* sym_entries = (Elf32_Sym*)malloc(sizeof(Elf32_Sym)*entry_num);//开辟堆内存用来存储符号表中所有entry
         //Elf32_Sym* sym_entries = (Elf32_Sym*)malloc(entry_num*sizeof(Elf32_Sym));
 		//Elf32_Sym sym_entries[entry_num];
-		fread(sym_entries,sizeof(Elf32_Sym),entry_num,fp );
         //int ret3=fread(sym_entries, sizeof(Elf32_Sym)*entry_num,1, fp);//读符号表
 		//assert(ret3==1);
   		//free (sym_entries);
 		//sym_entries=NULL;
 	//printf("  NUM:\tValue\t\tSize\tType\tName\n");
+	/*
 	for(int i=0;i<entry_num;i++)
 	{
 	   if((sym_entries[i].st_info & 0x0000000f)==STT_FUNC)
@@ -135,7 +134,6 @@ void elf_read_fun(char *elf_file) {
 	    }
 	}
 
-	delete [] sym_entries;
 	/*
 	//   print the fun_buff
 	for(int i=0;i<fun_num;i++)
