9b5c591e7a1ca095755c16c0d5d40be18a30a140
tracer-ysyx <tracer@ysyx.org>
2024-01-29 10:49:12 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:49:12 up  9:34,  1 user,  load average: 0.11, 0.15, 0.17

--- npc/csrc/monitor/monitor.c
+++ npc/csrc/monitor/monitor.c
@@ -112,8 +112,8 @@ void elf_read_fun(char *elf_file) {
         Elf32_Sym* sym_entries = (Elf32_Sym*)malloc(entry_num*sizeof(Elf32_Sym));
 		printf("%ld\n",entry_num*sizeof(Elf32_Sym));
 		//Elf32_Sym sym_entries[entry_num];
-        int ret3=fread(sym_entries, sizeof(Elf32_Sym)*entry_num,1, fp);//读符号表
-	assert(ret3==1);
+        //int ret3=fread(sym_entries, sizeof(Elf32_Sym)*entry_num,1, fp);//读符号表
+		//assert(ret3==1);
 
 	/*
 	//printf("  NUM:\tValue\t\tSize\tType\tName\n");
