6f7ded572724fad01890ee1ea281ffc64692611e
tracer-ysyx <tracer@ysyx.org>
2024-01-29 10:53:16 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:53:16 up  9:38,  1 user,  load average: 0.33, 0.19, 0.18

--- npc/csrc/monitor/monitor.c
+++ npc/csrc/monitor/monitor.c
@@ -103,7 +103,7 @@ void elf_read_fun(char *elf_file) {
         uint32_t entry_num = sec_headers[symtab_ind].sh_size / sec_headers[symtab_ind].sh_entsize;
 		printf("%d\n",sec_headers[symtab_ind].sh_size);
 		printf("%d\n",sec_headers[symtab_ind].sh_entsize);
-		printf("%d\n",symtab_ind);
+		printf("%x\n",sec_headers[symtab_ind].sh_offset);
 		printf("%ld\n",sizeof(Elf32_Sym));
 
         fseek(fp, sec_headers[symtab_ind].sh_offset, SEEK_SET);//将指针移动到符号表对应的偏移地址
