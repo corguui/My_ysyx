f30d7ebfe0ae9690660d7112bbdd4d7ac957ed44
tracer-ysyx <tracer@ysyx.org>
2024-01-29 15:06:29 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:06:29 up 11:15,  1 user,  load average: 0.28, 0.21, 0.19

--- npc/csrc/monitor/monitor.c
+++ npc/csrc/monitor/monitor.c
@@ -173,7 +173,7 @@ void show_symbol_table(int symtab_ind,uint32_t entry_num,char *strtab_string_tab
         printf("0x%08x:\t", sym_entries[i].st_value);
         printf("%4d\t", sym_entries[i].st_size);
 	    printf("FUN\t");
-        printf("%s", &strtab_string_table[sym_entries[i].st_name]);
+        //printf("%s", &strtab_string_table[sym_entries[i].st_name]);
         printf("\n");
 	    fun_buff[fun_num].value=sym_entries[i].st_value;
 	    fun_buff[fun_num].size=sym_entries[i].st_size;
