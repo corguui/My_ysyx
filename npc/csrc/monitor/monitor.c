162672ac6f12c4a6efda47de039d3c4a3a06d6fd
tracer-ysyx <tracer@ysyx.org>
2024-01-29 10:48:17 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:48:17 up  9:33,  1 user,  load average: 0.16, 0.17, 0.18

--- npc/csrc/monitor/monitor.c
+++ npc/csrc/monitor/monitor.c
@@ -110,6 +110,7 @@ void elf_read_fun(char *elf_file) {
 		printf("%d\n",sec_headers[symtab_ind].sh_offset);
         //Elf32_Sym* sym_entries = (Elf32_Sym*)malloc(sizeof(Elf32_Sym)*entry_num);//开辟堆内存用来存储符号表中所有entry
         Elf32_Sym* sym_entries = (Elf32_Sym*)malloc(entry_num*sizeof(Elf32_Sym));
+		printf("%ld\n",entry_num*sizeof(Elf32_Sym));
 		//Elf32_Sym sym_entries[entry_num];
         int ret3=fread(sym_entries, sizeof(Elf32_Sym)*entry_num,1, fp);//读符号表
 	assert(ret3==1);
