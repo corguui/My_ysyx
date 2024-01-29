52cfe573c8f865ce9a2ee7ccb619a7a8a7773117
tracer-ysyx <tracer@ysyx.org>
2024-01-29 14:25:14 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:25:14 up 10:34,  1 user,  load average: 0.45, 0.27, 0.21

--- npc/csrc/monitor/monitor.c
+++ npc/csrc/monitor/monitor.c
@@ -94,8 +94,8 @@ void elf_read_fun(char *elf_file) {
 
 	if ((symtab_ind != -1) && (strtab_ind != -1)) {
         fseek(fp, sec_headers[strtab_ind].sh_offset, SEEK_SET);
-        //char* strtab_string_table = (char*)malloc(sec_headers[str_tab_ind].sh_size * sizeof(char));
-		char strtab_string_table [sec_headers[str_tab_ind].sh_size];
+        char* strtab_string_table = (char*)malloc(sec_headers[str_tab_ind].sh_size * sizeof(char));
+		//char strtab_string_table [sec_headers[str_tab_ind].sh_size];
         //int ret5=fread(strtab_string_table, sec_headers[strtab_ind].sh_size,1, fp);
 		//assert(ret5==1);
 
