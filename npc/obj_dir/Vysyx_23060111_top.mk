a4d54874d57c7cac480001f5b5c490eb26aed269
tracer-ysyx <tracer@ysyx.org>
2024-01-30 20:53:09 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:53:09 up 1 day,  1:43,  1 user,  load average: 0.27, 0.26, 0.23

--- npc/obj_dir/Vysyx_23060111_top.mk
+++ npc/obj_dir/Vysyx_23060111_top.mk
@@ -36,10 +36,17 @@ VM_MODPREFIX = Vysyx_23060111_top
 # User CFLAGS (from -CFLAGS on Verilator command line)
 VM_USER_CFLAGS = \
 	-I/home/corgi/ysyx-workbench/npc/include \
-	-DIMG=\"/home/corgi/ysyx-workbench/am-kernels/tests/cpu-tests/build/dummy-riscv32e-npc.bin\"-CFLAGS \
+	-DIMG=\"/home/corgi/ysyx-workbench/am-kernels/tests/cpu-tests/build/dummy-riscv32e-npc.bin\" \
+	-DELF=\"/home/corgi/ysyx-workbench/am-kernels/tests/cpu-tests/build/dummy-riscv32e-npc.elf\" \
+	-I/usr/lib/llvm-14/include \
+	-std=c++17 \
+	-fno-exceptions \
+	-fPIE \
 
 # User LDLIBS (from -LDFLAGS on Verilator command line)
 VM_USER_LDLIBS = \
+	-lreadline \
+	-lLLVM-14 \
 
 # User .cpp files (from .cpp's on Verilator command line)
 VM_USER_CLASSES = \
