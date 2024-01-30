c674ffee8f64ff3ecc4d0618e4a696f1a311c268
tracer-ysyx <tracer@ysyx.org>
2024-01-30 14:59:21 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:59:21 up 21:08,  1 user,  load average: 0.12, 0.16, 0.17

--- npc/obj_dir/Vysyx_23060111_top.mk
+++ npc/obj_dir/Vysyx_23060111_top.mk
@@ -47,7 +47,7 @@ VM_USER_CFLAGS = \
 VM_USER_LDLIBS = \
 	-lreadline \
 	-lLLVM-14 \
-	-L//home/corgi/ysyx-workbench/nemu/build/ \
+	-L/home/corgi/ysyx-workbench/nemu/build/ \
 	-lriscv32-nemu-interpreter-so \
 
 # User .cpp files (from .cpp's on Verilator command line)
