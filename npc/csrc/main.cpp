c6dba42c9e2744427698b8ec3a0744a41023f8ac
tracer-ysyx <tracer@ysyx.org>
2024-01-31 09:50:56 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:50:56 up 1 day,  4:56,  1 user,  load average: 0.14, 0.20, 0.18

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -26,11 +26,11 @@ int main(int argc ,char** argv, char** env)
 	tfp->open("wave.vcd");
 
 
-	//init_monitor
-	init_monitor();
-
 	//init cpu
 	cpu_init();
+
+	//init_monitor
+	init_monitor();
 
 	sdb_mainloop();
 	delete top;
