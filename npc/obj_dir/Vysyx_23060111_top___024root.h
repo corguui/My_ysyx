9369e567f4c2aa30f31df49b32f206b57d812769
tracer-ysyx <tracer@ysyx.org>
2024-01-31 09:48:15 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:48:15 up 1 day,  4:53,  1 user,  load average: 0.14, 0.20, 0.18

---
+++ npc/obj_dir/Vysyx_23060111_top___024root.h
@@ -0,0 +1,66 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design internal header
+// See Vysyx_23060111_top.h for the primary calling header
+
+#ifndef VERILATED_VYSYX_23060111_TOP___024ROOT_H_
+#define VERILATED_VYSYX_23060111_TOP___024ROOT_H_  // guard
+
+#include "verilated.h"
+
+class Vysyx_23060111_top__Syms;
+class Vysyx_23060111_top___024unit;
+
+
+class Vysyx_23060111_top___024root final : public VerilatedModule {
+  public:
+    // CELLS
+    Vysyx_23060111_top___024unit* __PVT____024unit;
+
+    // DESIGN SPECIFIC STATE
+    VL_IN8(clk,0,0);
+    VL_IN8(rst,0,0);
+    VL_OUT8(inv_flag,0,0);
+    CData/*3:0*/ ysyx_23060111_top__DOT__type_i;
+    CData/*0:0*/ ysyx_23060111_top__DOT__init_IDU__DOT__typeMux__DOT__i0__DOT__hit;
+    CData/*4:0*/ __Vdlyvdim0__ysyx_23060111_top__DOT__reg___0240__DOT__rf__v0;
+    CData/*0:0*/ __Vdlyvset__ysyx_23060111_top__DOT__reg___0240__DOT__rf__v0;
+    CData/*0:0*/ __Vtrigrprev__TOP__clk;
+    CData/*3:0*/ __Vtrigrprev__TOP__ysyx_23060111_top__DOT__type_i;
+    CData/*0:0*/ __VactDidInit;
+    CData/*0:0*/ __VactContinue;
+    VL_IN(inst,31,0);
+    VL_OUT(pc,31,0);
+    IData/*31:0*/ ysyx_23060111_top__DOT__dnpc;
+    IData/*31:0*/ ysyx_23060111_top__DOT__wdata;
+    VlWide<10>/*307:0*/ ysyx_23060111_top__DOT__init_IDU__DOT____Vcellinp__typeMux____pinNumber4;
+    IData/*31:0*/ __Vdlyvval__ysyx_23060111_top__DOT__reg___0240__DOT__rf__v0;
+    IData/*31:0*/ __VstlIterCount;
+    IData/*31:0*/ __VicoIterCount;
+    IData/*31:0*/ __Vtrigrprev__TOP__inst;
+    IData/*31:0*/ __VactIterCount;
+    QData/*36:0*/ ysyx_23060111_top__DOT__init_IDU__DOT____Vcellout__typeMux____pinNumber1;
+    QData/*36:0*/ ysyx_23060111_top__DOT__init_IDU__DOT__typeMux__DOT__i0__DOT__lut_out;
+    VlUnpacked<IData/*31:0*/, 32> ysyx_23060111_top__DOT__reg___0240__DOT__rf;
+    VlUnpacked<QData/*43:0*/, 7> ysyx_23060111_top__DOT__init_IDU__DOT__typeMux__DOT__i0__DOT__pair_list;
+    VlUnpacked<CData/*6:0*/, 7> ysyx_23060111_top__DOT__init_IDU__DOT__typeMux__DOT__i0__DOT__key_list;
+    VlUnpacked<QData/*36:0*/, 7> ysyx_23060111_top__DOT__init_IDU__DOT__typeMux__DOT__i0__DOT__data_list;
+    VlUnpacked<CData/*0:0*/, 3> __Vm_traceActivity;
+    VlTriggerVec<1> __VstlTriggered;
+    VlTriggerVec<1> __VicoTriggered;
+    VlTriggerVec<3> __VactTriggered;
+    VlTriggerVec<3> __VnbaTriggered;
+
+    // INTERNAL VARIABLES
+    Vysyx_23060111_top__Syms* const vlSymsp;
+
+    // CONSTRUCTORS
+    Vysyx_23060111_top___024root(Vysyx_23060111_top__Syms* symsp, const char* v__name);
+    ~Vysyx_23060111_top___024root();
+    VL_UNCOPYABLE(Vysyx_23060111_top___024root);
+
+    // INTERNAL METHODS
+    void __Vconfigure(bool first);
+} VL_ATTR_ALIGNED(VL_CACHE_LINE_BYTES);
+
+
+#endif  // guard
