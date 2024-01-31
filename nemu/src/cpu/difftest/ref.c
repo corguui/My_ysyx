4a9ab71fd6323e765cb2e9e672e7c586258a212c
tracer-ysyx <tracer@ysyx.org>
2024-01-31 09:11:30 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:11:30 up 1 day,  4:16,  1 user,  load average: 0.20, 0.19, 0.18

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -33,6 +33,7 @@ __EXPORT void difftest_memcpy(uint32_t addr, void *buf, size_t n, bool direction
   else
   printf("direction error\n");
   printf("please type DIFFTEST_TO_REF or DIFFTEST_TO_DUT");
+  assert(0);
 }
 
 __EXPORT void difftest_regcpy(void *dut,uint32_t *pc, bool direction) {
@@ -71,5 +72,5 @@ __EXPORT void difftest_init(int port) {
   void init_mem();
   init_mem();
   /* Perform ISA dependent initialization. */
-  init_isa();
+  //init_isa();
 }
