eee5cc7f856b85756609c386485c8ddbf27ca647
tracer-ysyx <tracer@ysyx.org>
2024-01-31 10:56:59 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:56:59 up 1 day,  6:02,  1 user,  load average: 0.19, 0.20, 0.18

--- npc/csrc/cpu/cpu.c
+++ npc/csrc/cpu/cpu.c
@@ -58,6 +58,7 @@ static void trace_and_difftest(Decode *_this) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   #ifdef CONFIG_DIFFTEST 
   difftest_step(_this->pc, top->rootp->ysyx_23060111_top__DOT__dnpc);
+  printf("----\n");
   #endif
 
 //watchpoint
