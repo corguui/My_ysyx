bac78d98d2ae0a56fd6b54eaf3d17e2a3ae0dcba
tracer-ysyx <tracer@ysyx.org>
2024-01-31 14:11:53 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:11:53 up 1 day,  7:09,  1 user,  load average: 0.44, 0.33, 0.22

--- npc/csrc/cpu/difftest/dut.c
+++ npc/csrc/cpu/difftest/dut.c
@@ -53,9 +53,11 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
 
 void difftest_step(uint32_t pc, uint32_t npc) {
   NPC_CPU_state ref_r;
+  printf("%x\n%x\n",pc,npc);
 
   if (skip_dut_nr_inst > 0) {
     ref_difftest_regcpy(ref_r.gpr,&ref_r.pc, DIFFTEST_TO_DUT);
+    printf("%x\n",ref_r.pc);
     if (ref_r.pc == npc) {
       skip_dut_nr_inst = 0;
       checkregs(&ref_r, npc);
@@ -86,12 +88,14 @@ bool isa_difftest_checkregs(NPC_CPU_state *ref_r, uint32_t pc) {
   {
   	if(ref_r->gpr[i]!=cpu.gpr[i])
 	{
+    printf("npc:%x\nnmeu:%x\n",cpu.gpr[i],ref_r->gpr[i]);
     pc=ref_r->pc;
 		return false;
 	}
   }
   if(ref_r->pc!=cpu.pc)
   {
+    printf("npc:%x\nnemu%x\n",cpu.pc,ref_r->pc);
     pc=ref_r->pc;
     return false;
   }
