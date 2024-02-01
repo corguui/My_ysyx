fdb28aa8ebdbadb892e068b362fdb1cadef478fb
tracer-ysyx <tracer@ysyx.org>
2024-02-01 17:46:02 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:46:02 up 1 day, 19:04,  1 user,  load average: 0.85, 0.52, 0.35

--- npc/include/mem.h
+++ npc/include/mem.h
@@ -2,7 +2,7 @@
 #define __MEM_H__
 
 #include <common.h>
-extern "C" void pmem_read(int ad,int data,int len);
+extern "C" int pmem_read(int ad,int data,int len);
 //uint32_t pc_read(uint32_t &pc);
 extern "C" void pmem_write(int &ad,int len ,int data);
 void init_mem();
--- npc/vsrc/ysyx_23060111_IFU.v
+++ npc/vsrc/ysyx_23060111_IFU.v
@@ -1,11 +1,11 @@
 module ysyx_23060111_IFU (
     input [31:0] pc, 
-    output [31:0]inst
+    output reg [31:0]inst
 );
     wire [31:0] len;
     assign len=32'd4;
     always @(pc) begin
-         pmem_read (pc,inst,len);
+        inst = pmem_read (pc,len);
     end
 
 endmodule
