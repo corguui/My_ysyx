b41818a264e77d0b3036bfe2be472064864666d3
tracer-ysyx <tracer@ysyx.org>
2024-02-01 10:34:47 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:34:47 up 1 day, 14:30,  1 user,  load average: 0.17, 0.21, 0.22

--- npc/obj_dir/Vysyx_23060111_top.mk
+++ npc/obj_dir/Vysyx_23060111_top.mk
@@ -35,10 +35,7 @@ VM_PREFIX = Vysyx_23060111_top
 VM_MODPREFIX = Vysyx_23060111_top
 # User CFLAGS (from -CFLAGS on Verilator command line)
 VM_USER_CFLAGS = \
-	-DNPC_IMG=\"/home/corgi/ysyx-workbench/am-kernels/tests/cpu-tests/build/add-longlong-riscv32e-npc.bin\" \
 	-I/home/corgi/ysyx-workbench/npc/include \
-	-DNPC_LOG=\"./npc-log.txt\" \
-	-DNPC_ELF=\"/home/corgi/ysyx-workbench/am-kernels/tests/cpu-tests/build/add-longlong-riscv32e-npc.elf\" \
 	-I/usr/lib/llvm-14/include \
 	-std=c++17 \
 	-fno-exceptions \
