8dbaeb2223c4c93cd0c31bb742492b16afaf92f0
tracer-ysyx <tracer@ysyx.org>
2024-02-01 17:37:50 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:37:50 up 1 day, 18:55,  1 user,  load average: 0.75, 0.36, 0.23

--- npc/include/mem.h
+++ npc/include/mem.h
@@ -2,9 +2,9 @@
 #define __MEM_H__
 
 #include <common.h>
-uint32_t pmem_read(uint32_t &ad,int len);
+int pmem_read(int &ad,int len);
 uint32_t pc_read(uint32_t &pc);
-void pmem_write(uint32_t &ad,int len ,uint32_t data);
+void pmem_write(int &ad,int len ,int data);
 void init_mem();
 uint8_t* NPC_guest_to_host(uint32_t paddr);
 
