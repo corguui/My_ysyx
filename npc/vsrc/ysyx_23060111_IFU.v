5c637911a326653a38357296f5f283bc759c8b5a
tracer-ysyx <tracer@ysyx.org>
2024-02-01 20:24:00 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:24:00 up 1 day, 20:22,  1 user,  load average: 0.15, 0.18, 0.18

--- npc/vsrc/ysyx_23060111_IFU.v
+++ npc/vsrc/ysyx_23060111_IFU.v
@@ -4,7 +4,7 @@ module ysyx_23060111_IFU(
     input [31:0] pc,
     output reg [31:0] inst
 );
-    always@(negedge clk) begin
+    always@(posedge clk) begin
             inst=vlg_pmem_read(pc);
     end
 
