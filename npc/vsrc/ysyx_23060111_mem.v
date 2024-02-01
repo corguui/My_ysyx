831800807766afcf385d928c0145ccaefe904877
tracer-ysyx <tracer@ysyx.org>
2024-02-01 17:35:04 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:35:04 up 1 day, 18:53,  1 user,  load average: 0.26, 0.22, 0.19

--- npc/vsrc/ysyx_23060111_mem.v
+++ npc/vsrc/ysyx_23060111_mem.v
@@ -1,4 +1,4 @@
-import "DPI-C" function uint32_t pmem_read(input int raddr, int len);
+import "DPI-C" function int pmem_read(input int raddr, int len);
 import "DPI-C" function void pmem_write(
   input int waddr, input int wdata, input  int wmask);
 module ysyx_23060111_mem (
