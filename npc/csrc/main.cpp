f4f0870ccfa3449fb798a0745aebbf8c6a36173d
tracer-ysyx <tracer@ysyx.org>
2024-02-21 10:06:04 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:06:04 up  1:16,  1 user,  load average: 0.43, 0.30, 0.22

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -39,9 +39,7 @@ int main(int argc ,char** argv, char** env)
 	//(npc)   command
 	sdb_mainloop();
 
-	printf("%x\n",top->rootp->ysyx_23060111_top__DOT__m_rdata);
-	uint32_t ad=0x80000040;
-	printf("%x\n",pmem_read(ad, 4));
+
 	delete top;
 	tfp->close();
 	delete contextp;
