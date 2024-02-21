15b205d500a5905d35c9c245a7c08bf6262bab41
tracer-ysyx <tracer@ysyx.org>
2024-02-21 19:28:59 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:28:59 up  7:36,  1 user,  load average: 0.25, 0.17, 0.15

--- npc/obj_dir/Vysyx_23060111_top___024root__DepSet_hfb4f0fd6__0__Slow.cpp
+++ npc/obj_dir/Vysyx_23060111_top___024root__DepSet_hfb4f0fd6__0__Slow.cpp
@@ -37,8 +37,8 @@ VL_ATTR_COLD void Vysyx_23060111_top___024root___stl_sequent__TOP__0(Vysyx_23060
     // Body
     Vysyx_23060111_top___024unit____Vdpiimwrap_vlg_pmem_read_TOP____024unit(0x80000000U, 1U, __Vfunc_vlg_pmem_read__2__Vfuncout);
     vlSelf->ysyx_23060111_top__DOT__m_rdata = ((IData)(vlSelf->ysyx_23060111_top__DOT__m_ren)
-                                                ? __Vfunc_vlg_pmem_read__2__Vfuncout
-                                                : 0U);
+                                                ? 0U
+                                                : __Vfunc_vlg_pmem_read__2__Vfuncout);
     vlSelf->ysyx_23060111_top__DOT__init_IDU__DOT__typeMux__DOT__i0__DOT__pair_list[5U] 
         = (0x6e00000000ULL | ((QData)((IData)((vlSelf->ysyx_23060111_top__DOT__inst 
                                                >> 0xcU))) 
