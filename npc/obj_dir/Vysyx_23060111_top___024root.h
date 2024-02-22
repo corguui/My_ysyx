e7ff66d6cf5bcc2e66a3706aa8e677ba4a7b592d
tracer-ysyx <tracer@ysyx.org>
2024-02-22 17:46:46 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:46:46 up 16:12,  1 user,  load average: 0.15, 0.18, 0.20

--- npc/obj_dir/Vysyx_23060111_top___024root.h
+++ npc/obj_dir/Vysyx_23060111_top___024root.h
@@ -50,6 +50,7 @@ class Vysyx_23060111_top___024root final : public VerilatedModule {
     IData/*31:0*/ ysyx_23060111_top__DOT__m_rdata;
     IData/*31:0*/ ysyx_23060111_top__DOT__m_raddr;
     VlWide<13>/*399:0*/ ysyx_23060111_top__DOT__init_IDU__DOT____Vcellinp__typeMux____pinNumber4;
+    IData/*31:0*/ __Vfunc_vlg_pmem_read__2__Vfuncout;
     IData/*31:0*/ __VstlIterCount;
     IData/*31:0*/ __Vtrigrprev__TOP__pc;
     IData/*31:0*/ __Vtrigrprev__TOP__ysyx_23060111_top__DOT__imm;
@@ -57,7 +58,6 @@ class Vysyx_23060111_top___024root final : public VerilatedModule {
     IData/*31:0*/ __Vtrigrprev__TOP__ysyx_23060111_top__DOT__rout1;
     IData/*31:0*/ __Vtrigrprev__TOP__ysyx_23060111_top__DOT__rout2;
     IData/*31:0*/ __Vtrigrprev__TOP__ysyx_23060111_top__DOT__snpc;
-    IData/*31:0*/ __Vtrigrprev__TOP__ysyx_23060111_top__DOT__m_raddr;
     IData/*31:0*/ __Vtrigrprev__TOP__ysyx_23060111_top__DOT__inst;
     IData/*31:0*/ __VactIterCount;
     QData/*32:0*/ ysyx_23060111_top__DOT__init_IDU__DOT____Vcellout__typeMux____pinNumber1;
@@ -72,8 +72,8 @@ class Vysyx_23060111_top___024root final : public VerilatedModule {
     VlUnpacked<QData/*32:0*/, 10> ysyx_23060111_top__DOT__init_IDU__DOT__typeMux__DOT__i0__DOT__data_list;
     VlUnpacked<CData/*0:0*/, 4> __Vm_traceActivity;
     VlTriggerVec<1> __VstlTriggered;
-    VlTriggerVec<5> __VactTriggered;
-    VlTriggerVec<5> __VnbaTriggered;
+    VlTriggerVec<4> __VactTriggered;
+    VlTriggerVec<4> __VnbaTriggered;
 
     // INTERNAL VARIABLES
     Vysyx_23060111_top__Syms* const vlSymsp;
