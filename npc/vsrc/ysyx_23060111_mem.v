2161e8966d75f989bb99fbda6188a5db75d90775
tracer-ysyx <tracer@ysyx.org>
2024-02-22 14:52:44 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:52:44 up 13:18,  1 user,  load average: 0.21, 0.19, 0.18

--- npc/vsrc/ysyx_23060111_mem.v
+++ npc/vsrc/ysyx_23060111_mem.v
@@ -14,7 +14,10 @@ module ysyx_23060111_mem(
 ); 
 
     always @(m_raddr) begin
+        if(m_ren) begin
              m_rdata=vlg_pmem_read(m_raddr);
+             m_ren=1'b0;
+        end
     end
 
     always@(posedge clk)
