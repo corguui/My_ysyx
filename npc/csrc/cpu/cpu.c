3ac141049b350fac8b765398aa83aca855f4b000
tracer-ysyx <tracer@ysyx.org>
2024-02-22 10:48:51 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:48:51 up 11:29,  1 user,  load average: 0.27, 0.20, 0.18

--- npc/csrc/cpu/cpu.c
+++ npc/csrc/cpu/cpu.c
@@ -1,3 +1,4 @@
+#include "Vysyx_23060111_top__Dpi.h"
 #include "config.h"
 #include <cpu/cpu.h>
 #include <cpu/decode.h>
@@ -218,6 +219,7 @@ static void execute(uint64_t n)
 		if(top->pc==0x80000054)
 		{
 			printf("------%x\n",top->rootp->ysyx_23060111_top__DOT__m_rdata);
+			printf("------%x\n",vlg_pmem_read(0x80008fdc));
 		}
 		cpu_exec_once(tfp,&s);
 		trace_and_difftest(&s); 
