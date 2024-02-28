6739c6a0f487ceec0d50e99399042ccd45a52ba5
tracer-ysyx <tracer@ysyx.org>
2024-02-28 21:52:14 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:52:14 up  1:14,  1 user,  load average: 0.06, 0.10, 0.08

--- npc/csrc/mem.cpp
+++ npc/csrc/mem.cpp
@@ -170,7 +170,7 @@ static long load_img(){
    fseek(fp, 0, SEEK_END);
    long size = ftell(fp);
    Log("The image is %s, size = %ld", img_file, size); 
-   Log_color("hello",ANSI_BG_RED );
+   Log_color("hello %s",img_file,ANSI_BG_RED );
    fseek(fp, 0, SEEK_SET);
    int ret = fread(NPC_guest_to_host(0x80000000), size, 1, fp);
    assert(ret == 1);
