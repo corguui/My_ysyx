8473bcbcfcf8814374b66b831c760631a0a63ca9
tracer-ysyx <tracer@ysyx.org>
2024-03-11 21:13:39 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:13:39 up  3:04,  3 users,  load average: 0.02, 0.02, 0.00

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -33,7 +33,7 @@ int atoi(const char* nptr) {
 extern char _heap_start;
 char* addr=&_heap_start;
 */
-void* addr=(void*)ROUNDUP(heap.start,0);
+static char *hbrk;
 
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
@@ -41,9 +41,14 @@ void *malloc(size_t size) {
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
 #endif
-  void* buf=addr;
-  addr=(void*)ROUNDUP(buf,size );
-  return (void*)buf;
+  size  = (size_t)ROUNDUP(size, 8);
+  char *old = hbrk;
+  hbrk += size;
+  assert((uintptr_t)heap.start <= (uintptr_t)hbrk && (uintptr_t)hbrk < (uintptr_t)heap.end);
+  for (uint64_t *p = (uint64_t *)old; p != (uint64_t *)hbrk; p ++) {
+    *p = 0;
+  }
+  return (void*)old;
 }
 
 void free(void *ptr) {
