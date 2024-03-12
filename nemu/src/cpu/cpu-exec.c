012ad690382d6775db35719f43c7849487b7d1fd
tracer-ysyx <tracer@ysyx.org>
2024-03-12 15:51:40 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:51:40 up 33 min,  1 user,  load average: 0.35, 0.22, 0.19

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -33,6 +33,7 @@ extern FUN fun_buff[128];
 extern int fun_num;
 int space_num=-1;
 int space_flat=0;
+int print_flat=0;
 #endif
 
 //ringbuf val
@@ -53,6 +54,9 @@ void device_update();
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
   if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
+#endif
+#ifdef CONFIG_FTRACE
+  if (CONFIG_FTRACE&&print_flat==1) { print_flat=0; log_write("%s\n", _this->fun_printf_buf); }
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
@@ -79,6 +83,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   char ar2[]="00 00 80 67";//funbuf 0x8--------: 00 00 80 67 jalr  ..... the ret is 80 67
   //00 07 80 67 jr mean call to but no printf the ret//in f1 have jr call to f0 the f1 no ret
   char *q = s->funbuf;
+  char *pr= s->fun_printf_buf;
   q += snprintf(q, sizeof(s->funbuf), FMT_WORD ":", s->pc);
   int funlen = s->snpc - s->pc;
   int j;
@@ -135,9 +140,10 @@ if(pc!=0x80000000)
 		     {
 		     	space_num--;
 		     }
-		     printf("0x%x:",s->pc);
-		     printf("---num: %d   ret [fun:%s  @%x]\n",space_num,fun_buff[f].name,fun_buff[f].value); 
+		     pr+=sprintf(pr,"0x%x:",s->pc);
+		     pr+=sprintf(pr,"---num: %d   ret [fun:%s  @%x]\n",space_num,fun_buff[f].name,fun_buff[f].value); 
 		     space_flat=1;
+         print_flat=1;
 		     break;
 		   }
 		   else if(flat_ret==0)//call
@@ -146,9 +152,10 @@ if(pc!=0x80000000)
 		     {
 		     	space_num++;
 		     }
-		     printf("0x%x:",s->pc);
-		     printf("---num: %d  call [fun:%s  @%x]\n",space_num,fun_buff[g].name,fun_buff[g].value);
+		     pr+=sprintf(pr,"0x%x:",s->pc);
+		     pr+=sprintf(pr,"---num: %d  call [fun:%s  @%x]\n",space_num,fun_buff[g].name,fun_buff[g].value);
 			space_flat=0;
+      print_flat=1;
 			break;
 		   }
 		}
--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -52,6 +52,18 @@ void init_map() {
   p_space = io_space;
 }
 
+
+void Dtrace_read()
+{
+
+}
+
+void Dtrace_write()
+{
+
+}
+
+
 word_t map_read(paddr_t addr, int len, IOMap *map) {
   assert(len >= 1 && len <= 8);
   check_bound(map, addr);
