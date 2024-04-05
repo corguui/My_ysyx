30b6c141e14db9801a81a2b784efbb433be350f5
tracer-ysyx <tracer@ysyx.org>
2024-04-05 16:09:32 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:09:32 up  2:57,  1 user,  load average: 0.07, 0.03, 0.00

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,17 +1,21 @@
 #include <am.h>
 #include <nemu.h>
+#include <sys/types.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 # define W    inl(VGACTL_ADDR)>>16 
 # define H    (inl(VGACTL_ADDR)<<16)>>16
 
+
 void __am_gpu_init() {
+  /*
   int i;
   uint32_t w = W;  // TODO: get the correct width
   uint32_t h = H;  // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
+  */
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -23,6 +27,21 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  int x=ctl->x;
+  int y=ctl->y;
+  int i=0;
+  int k=0;
+  int count=0;
+  int w=ctl->w;
+  int h=ctl->h;
+  for(k=y;k<=h;k++)
+  {
+    for(i=x;i<=w;i++)
+    {
+      outl(FB_ADDR+(4*count),*(uint32_t*)ctl->pixels+(4*count));
+      count++;
+    }
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
