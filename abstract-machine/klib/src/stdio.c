dd51535763c0590ad90515931214beb2b75faa77
tracer-ysyx <tracer@ysyx.org>
2024-04-21 15:58:02 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:58:02 up 42 min,  1 user,  load average: 0.41, 0.65, 0.47

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,6 +6,10 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+#define NUM_LEN 32
+#define OSTRING_LEN 5120
+typedef uint32_t word_t;
+
 int printf(const char *fmt, ...) {
   	char buff[4096];
 	va_list arg;
@@ -20,191 +24,208 @@ int printf(const char *fmt, ...) {
 	return n;
 	//panic("Not implemented");	
 }
+
+static char *__itoa(int num, char *buff, uint16_t base)
+{
+  static const char sym[] = "0123456789abcdef";
+
+  char tmp[NUM_LEN];
+  bool is_neg = false;
+  if (num == 0)
+  {
+    strcpy(buff, "0");
+    return buff;
+  }
+  else if (num < 0)
+  {
+    is_neg = true;
+    strcpy(buff, "-");
+    buff++;
+    num = -num;
+  }
+
+  uint8_t i = 0;
+  while (num != 0)
+  {
+    tmp[i] = sym[num % base];
+    num /= base;
+    i++;
+  }
+
+  for (int j = i - 1; j >= 0; --j)
+    buff[i - 1 - j] = tmp[j];
+  buff[i] = '\0';
+
+  return is_neg ? (buff - 1) : buff;
+}
+
+static char *__ptoa(void *p, char *buff)
+{
+  static const char sym[] = "0123456789abcdef";
+
+  word_t num = (word_t)p;
+  char tmp[NUM_LEN];
+
+  if (num == 0)
+  {
+    buff[0] = '0';
+    buff[1] = '\0';
+    return (buff - 1);
+  }
+
+  uint8_t i = 0;
+  while (num != 0)
+  {
+    tmp[i] = sym[num % 16];
+    num /= 16;
+    i++;
+  }
+
+  for (int j = i - 1; j >= 0; --j)
+    buff[i - 1 - j] = tmp[j];
+  buff[i] = '\0';
+
+  return (buff - 1);
+}
+
+
 int vsprintf(char *out, const char *fmt, va_list ap) {
-	int len=0;
-	int Out_num=0;
-	while(*fmt!='\0')
-	{
-		switch(*fmt)
-		{
-			case '%':
-			fmt++;
-			if(*fmt>='0'&&*fmt<='9')
-			{
-			int a=1;
-			int count=0;
-			int k=0;
-			Out_num=0;
-			char out_buf[20];
-			if(*fmt=='0') fmt++;
-			while(*fmt<='9'&&*fmt>'0')
-			{
-					out_buf[count]=*fmt;
-					count++;
-					a=a*10;
-					fmt++;
-			}
-			while(count>0)
-			{
-				a=a/10;
-				Out_num=Out_num+(out_buf[k]-48)*a;
-				k++;
-				count--;
-			}
-			}
+  char *pout = out;
+  size_t i = 0;
+  while (fmt[i] != '\0')
+  {
+    if (fmt[i] != '%')
+    {
+      *(pout++) = fmt[i];
+    }
+    else
+    {
+      bool is_end = false;
+      char num_leftpad_sym = ' ';
+      bool left_align = false;
+      int width = 0;
+      while (!is_end)
+      {
+        i++;
+        switch (fmt[i])
+        {
+        case '0':
+        {
+          if (fmt[i - 1] == '%')
+          {
+            num_leftpad_sym = '0';
+            break;
+          }
+        }
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        {
+          width = width * 10 + (fmt[i] - '0');
+          break;
+        }
+        case '-':
+        {
+          if (fmt[i - 1] == '%')
+          {
+            left_align = true;
+          }
+          break;
+        }
+        case 's':
+        {
+          const char *str = va_arg(ap, char *);
+          if (!left_align)
+          {
+            for (int j = 0; j < width - (int)strlen(str); ++j)
+            {
+              *(pout++) = ' ';
+            }
+            strcpy(pout, str);
+            pout += strlen(str);
+          }
+          else
+          {
+            strcpy(pout, str);
+            pout += strlen(str);
+            for (int j = 0; j < width - (int)strlen(str); ++j)
+            {
+              *(pout++) = ' ';
+            }
+          }
+          is_end = true;
+          break;
+        }
+        case 'd':
+        case 'i':
+        case 'x':
+        {
+          int val = va_arg(ap, int);
 
-			switch(*fmt)
-			{
-			  	
-			  case 'd':
-			    int val=va_arg(ap,int); 
-			    if(val==0)
-			    {out[len]='0';
-			     len++;
-			     break;}
-			    else if(val<0)
-			    {out[len]='-';
-			     len++;
-			     val=0-val;}
-			     int r;
-			     char p[1024];
-			     int plen=0;
-			     while(val>0)
-			     {
-			     r=val%10;
-				 val=val/10;
-				 p[plen]=48+r;
-				 plen++;
-			     }
-				 if((plen-1)<Out_num)
-				 {
-					for(int y=0;y<(Out_num-plen);y++)
-					{
-					out[len]='0';
-					len++;
-					}
-				 }
-			     for(int i=plen-1;i>=0;i--)
-			     {
-			       out[len]=p[i];
-			       len++;
-			     }
-			     break;
-			     case 's':
-			     int i;
-			     char *sr=va_arg(ap,char*);
-			     for(i=0;sr[i]!='\0';i++)
-			     {
-			     	out[len]=sr[i];
-				len++;
-			     }
-			    break;
-				case 'c':
-				char ch= (char)va_arg(ap,int);
-				out[len]=ch;
-				len++;
-				break;
-				case 'X':
-				unsigned int hex;
-				int hval=va_arg(ap,int); 
-			    if(hval==0)
-			    {out[len]='0';
-			     len++;
-			     break;}
-			    else
-			    {
-				 hex=(unsigned int)hval;
-				}
-			     int u;
-			     char q[1024];
-			     int qlen=0;
-			     while(hex>0)
-			     {
-					u=hex%16;
-			     	if(u>=10)
-					{
-					q[qlen]=55+u;
-					}
-					else
-					{
-					q[qlen]=48+u;
-					}
-					hex=hex/16;
-					qlen++;
-			     }
-				 if((qlen-1)<Out_num)
-				 {
-					for(int y=0;y<(Out_num-qlen);y++)
-					{
-					out[len]='0';
-					len++;
-					}
-				 }
-			     for(int i=qlen-1;i>=0;i--)
-			     {
-			       out[len]=q[i];
-			       len++;
-			     }
-			     break;
-				case 'x':
-				unsigned int hexx;
-				int hxval=va_arg(ap,int); 
-			    if(hxval==0)
-			    {out[len]='0';
-			     len++;
-			     break;}
-			    else
-			    {
-				 hexx=(unsigned int)hxval;
-				}
-			     int f;
-			     char x[1024];
-			     int xlen=0;
-			     while(hexx>0)
-			     {
-					f=hexx%16;
-			     	if(f>=10)
-					{
-					x[xlen]=87+f;
-					}
-					else
-					{
-					x[xlen]=48+f;
-					}
-					hexx=hexx/16;
-					xlen++;
-			     }
-				 if((xlen-1)<Out_num)
-				 {
-					for(int y=0;y<(Out_num-xlen);y++)
-					{
-					out[len]='0';
-					len++;
-					}
-				 }				 
-			     for(int i=xlen-1;i>=0;i--)
-			     {
-			       out[len]=x[i];
-			       len++;
-			     }
-			     break;
+          char buff[NUM_LEN];
+          __itoa(val, buff, (fmt[i] == 'x') ? 16 : 10);
+          if (!left_align)
+          {
+            for (int j = 0; j < width - (int)strlen(buff); ++j)
+              *(pout++) = num_leftpad_sym;
+            strcpy(pout, buff);
+            pout += strlen(buff);
+          }
+          else
+          {
+            strcpy(pout, buff);
+            pout += strlen(buff);
+            for (int j = 0; j < width - (int)strlen(buff); ++j)
+              *(pout++) = num_leftpad_sym;
+          }
+          is_end = true;
+          break;
+        }
+        case 'p':
+        {
+          void *val = va_arg(ap, void *);
 
-			}
-			break;
+          char buff[NUM_LEN];
+          __ptoa(val, buff);
+          if (!left_align)
+          {
+            for (int j = 0; j < width - (int)strlen(buff); ++j)
+              *(pout++) = num_leftpad_sym;
+            strcpy(pout, buff);
+            pout += strlen(buff);
+          }
+          else
+          {
+            strcpy(pout, buff);
+            pout += strlen(buff);
+            for (int j = 0; j < width - (int)strlen(buff); ++j)
+              *(pout++) = num_leftpad_sym;
+          }
+          is_end = true;
+          break;
+        }
+        default:
+        {
+          printf("print format is wrong!\n");
+          // Wrong format!!!
+          assert(0);
+        }
+        }
+      }
+    }
+    ++i;
+  }
+  *pout = '\0';
 
-			case '\n':
-			out[len]='\n';
-			len++;
-			break;
-			default:
-			  panic("Not implemented"); 
-			  out[len]=*fmt;
-			  len++;
-		}
-		fmt++;
-	}
-	out[len]='\0';
-	return len;
+  va_end(ap);
+
+  return strlen(out);
 }
 
 int sprintf(char *out, const char *fmt, ...) {
