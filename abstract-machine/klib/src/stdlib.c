cffd69b7f6c9012388409d4dc34f0ed9a62af986
tracer-ysyx <tracer@ysyx.org>
2024-04-24 11:26:06 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:26:06 up  1:42,  1 user,  load average: 0.24, 0.32, 0.32

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -4,6 +4,8 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
+static char* start_addr;
+static bool init_flag = 0;
 
 int rand(void) {
   // RAND_MAX assumed to be 32767
@@ -29,18 +31,24 @@ int atoi(const char* nptr) {
   return x;
 }
 
-extern char _heap_start;
-char* addr=&_heap_start;
+
 
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+#if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+  // panic("Not implemented");
+  if(!init_flag) {
+  	start_addr = (void*)ROUNDUP(heap.start, 8);
+	init_flag = true;
+  }
+  size = (size_t)ROUNDUP(size, 8);
+  char* old = start_addr;
+  start_addr += size;
+
 #endif
-  char* buf=addr;
-  addr=buf+(char)size;
-  return (void*)buf;
+  return old;
 }
 
 void free(void *ptr) {
