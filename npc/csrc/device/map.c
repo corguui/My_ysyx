4153c6747a5bf14643f7ecc635f1a5f722e0f4e2
tracer-ysyx <tracer@ysyx.org>
2024-05-22 09:58:12 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:58:12 up 20 min,  1 user,  load average: 0.08, 0.21, 0.24

--- npc/csrc/device/map.c
+++ npc/csrc/device/map.c
@@ -1,5 +1,6 @@
 #include "utils.h"
 #include <cstdint>
+#include <cstdio>
 #include <device/map.h>
 #include <mem.h>
 
@@ -71,6 +72,7 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   assert(len >= 1 && len <= 8);
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
+  printf("0x%08x\n",addr);
   host_write((map->space + offset), len, data);
   invoke_callback(map->callback, offset, len, true);
   #ifdef CONFIG_DTRACE
