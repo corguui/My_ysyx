2d830907e326c020e5404235e8ca2f4c79ffb69e
tracer-ysyx <tracer@ysyx.org>
2024-05-22 10:29:57 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:29:57 up 51 min,  1 user,  load average: 0.23, 0.26, 0.25

--- npc/csrc/mem.cpp
+++ npc/csrc/mem.cpp
@@ -155,10 +155,6 @@ void host_write(void* addr, int len, uint32_t data)
 extern "C" void vlg_pmem_write(int ad,int wdata,int len)
 {
 	uint32_t addr=(uint32_t)ad;
-	if(ad>0xa0000000)
-	{
-		printf("0x%08X\n",ad);
-	}
 	if(likely(check_mem(addr)))
 	{
 	uint32_t data=(uint32_t)wdata;
