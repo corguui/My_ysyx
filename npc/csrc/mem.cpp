981aac0eded73304da9939ed42b5a51e13bbda4d
tracer-ysyx <tracer@ysyx.org>
2024-05-23 12:39:06 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:39:06 up  8:49,  1 user,  load average: 0.22, 0.25, 0.25

--- npc/csrc/mem.cpp
+++ npc/csrc/mem.cpp
@@ -104,6 +104,7 @@ uint32_t host_read(void* addr,int len)
 extern "C" int vlg_pmem_read(int ad,int len)
 {
 	uint32_t addr=(uint32_t)ad;
+	printf("%x %d",ad,len);
 	if(likely(check_mem(addr)))
 	{
 	uint32_t data=pmem_read(addr, len);
