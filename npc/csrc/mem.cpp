2e28e36851f13e97a1fdab7582668330d75c64e6
tracer-ysyx <tracer@ysyx.org>
2024-05-23 16:09:22 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:09:22 up 16 min,  1 user,  load average: 0.37, 0.24, 0.19

--- npc/csrc/mem.cpp
+++ npc/csrc/mem.cpp
@@ -115,10 +115,6 @@ extern "C" int vlg_pmem_read(int ad,int len)
 	#endif
 	return (int) data; 
 	}
-	else if (addr>0xb0000000) {
-		return 0;
-	
-	}
 	else {
 	#ifdef CONFIG_DEVICE
 		return mmio_read(addr,len);
