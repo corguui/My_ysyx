b4411ad2cc2c3020386268b78861f922686479f0
tracer-ysyx <tracer@ysyx.org>
2024-05-31 21:05:23 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:05:23 up  5:45,  1 user,  load average: 0.24, 0.19, 0.18

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -30,7 +30,7 @@ static void restart() {
   /* Set the initial program counter. */
   cpu.pc = RESET_VECTOR;
   cpu.csr.mcause=0xb;
-  cpu.csr.mstatus=0x1800;
+  cpu.gpr[17]=0x1800;
   /* The zero register is always 0. */
   cpu.gpr[0] = 0;
 }
