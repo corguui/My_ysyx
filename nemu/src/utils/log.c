327f360d725a132324ce75b52796b4de1d15a15f
tracer-ysyx <tracer@ysyx.org>
2024-06-04 00:20:29 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:20:29 up  5:00,  1 user,  load average: 0.23, 0.17, 0.12

--- nemu/src/utils/log.c
+++ nemu/src/utils/log.c
@@ -29,6 +29,7 @@ void init_log(const char *log_file) {
 }
 
 bool log_enable() {
+  printf("%lu\n",g_nr_guest_inst);
   return MUXDEF(CONFIG_TRACE, (g_nr_guest_inst >= CONFIG_TRACE_START) &&
          (g_nr_guest_inst <= CONFIG_TRACE_END), false);
 }
