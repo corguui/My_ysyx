14732f97141c2dfd2dd8dfb4acf66876b1b2872a
tracer-ysyx <tracer@ysyx.org>
2024-06-03 20:41:41 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:41:41 up  1:47,  1 user,  load average: 0.30, 0.22, 0.19

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -14,8 +14,6 @@
 ***************************************************************************************/
 
 #include <isa.h>
-void etrace();
-void isa_csr_display();
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
@@ -24,10 +22,6 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   cpu.csr.mcause=NO;
   #ifdef CONFIG_DIFFTEST
   printf("the isa intr.c isa_raise_intr error fix the mcause=NO or in isa riscv32 difftest dut.c let it pass the 0xb\n ");
-  #endif
-  #ifdef CONFIG_ETRACE 
-  etrace(); 
-  isa_csr_display();
   #endif
   return cpu.csr.mtvec;
 }
