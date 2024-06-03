cb481fa7547f3e0389efd07d3407b82adb4daae5
tracer-ysyx <tracer@ysyx.org>
2024-06-03 21:20:41 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:20:41 up  2:26,  1 user,  load average: 0.43, 0.22, 0.19

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -14,14 +14,13 @@
 ***************************************************************************************/
 
 #include <isa.h>
-#include "utils.h"
 void etrace()
 {
   printf("aasdfasdfdsaf  \n");
-  log_write("mepc      --> 0x%x\n",cpu.csr.mepc);
-  log_write("mcause    --> 0x%x\n",cpu.csr.mcause);
-  log_write("mstatus   --> 0x%x\n",cpu.csr.mstatus);
-  log_write("mtvec     --> 0x%x\n",cpu.csr.mtvec);
+  log_write("$mepc      --> 0x%x\n",cpu.csr.mepc);
+  log_write("$mcause    --> 0x%x\n",cpu.csr.mcause);
+  log_write("$mstatus   --> 0x%x\n",cpu.csr.mstatus);
+  log_write("$mtvec     --> 0x%x\n",cpu.csr.mtvec);
 }
 
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
