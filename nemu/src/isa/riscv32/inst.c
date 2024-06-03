5d00a4c472fb366a1de84f7632f825b1ce661fe6
tracer-ysyx <tracer@ysyx.org>
2024-06-03 20:45:40 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:45:40 up  1:51,  1 user,  load average: 0.31, 0.24, 0.19

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -36,6 +36,7 @@
 #ifdef CONFIG_ETRACE
 void etrace()
 {
+  log_write("dtrace read: device read 0xat 0x len is \n");
   log_write("$mepc      --> 0x%x\n",cpu.csr.mepc);
   log_write("$mcause    --> 0x%x\n",cpu.csr.mcause);
   log_write("$mstatus   --> 0x%x\n",cpu.csr.mstatus);
