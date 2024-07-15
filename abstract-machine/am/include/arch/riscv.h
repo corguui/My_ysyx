c51f03d46ae79f9ec641e5c038166a3036384c5f
tracer-ysyx <tracer@ysyx.org>
2024-07-15 14:53:30 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:53:30 up  9:10,  1 user,  load average: 0.06, 0.02, 0.00

--- abstract-machine/am/include/arch/riscv.h
+++ abstract-machine/am/include/arch/riscv.h
@@ -17,7 +17,7 @@ struct Context {
 //#define GPR2 gpr[0]  //a0   //[0]
 //#define GPR3 gpr[0]
 //#define GPR4 gpr[0]
-#define GPR2 gpr[12] // a2   //[0]
+#define GPR0 gpr[10] // a2   //[0]
 #define GPR3 gpr[13] // a3
 #define GPR4 gpr[14] // a4
 #define GPR5 gpr[15] // a5
--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -35,7 +35,7 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   Context *c = (Context*)kstack.end ;
-  c->GPR1 = (uintptr_t)arg;
+  c->GPR0 = (uintptr_t)arg;
   c->mepc = (uintptr_t)entry;  
   return c;
 }
