7c2e4b00e1b321a3277eeba1d864ad4c04c9be08
tracer-ysyx <tracer@ysyx.org>
2024-07-30 10:15:22 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:15:22 up  1:57,  1 user,  load average: 0.20, 0.24, 0.22

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -39,6 +39,10 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   Context *c = (Context*)kstack.end;
   c->mstatus = 0x1800;
   c->mepc = (uintptr_t)entry;  
+  if(c==NULL)
+  {
+    assert(0);
+  }
   return c;
 }
 
