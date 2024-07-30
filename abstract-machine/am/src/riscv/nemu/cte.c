bf7cd829ec00c4e8b9efceb286c8fe719e1f457a
tracer-ysyx <tracer@ysyx.org>
2024-07-30 13:17:05 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:17:05 up  4:18,  1 user,  load average: 0.24, 0.18, 0.18

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -11,7 +11,7 @@ Context* __am_irq_handle(Context *c) {
     switch (c->mcause) {
       case  0: ev.event = EVENT_YIELD; break;
       case  11:ev.event = EVENT_YIELD; break;
-      default: ev.event = EVENT_ERROR;assert(0); break;
+      default: ev.event = EVENT_ERROR; break;
     }
 
     c = user_handler(ev, c);
