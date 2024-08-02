890e6d1dddaa7ff6287383d9c68e4d8bb230fc28
tracer-ysyx <tracer@ysyx.org>
2024-08-02 09:57:38 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:57:38 up 1 day, 12:24,  1 user,  load average: 0.69, 0.38, 0.31

--- npc/vsrc/ysyx_23060111_ALU.v
+++ npc/vsrc/ysyx_23060111_ALU.v
@@ -363,12 +363,12 @@ begin
                 //ecall
                 if(csr_flag==2'd1)
                 begin
-                $display("csr_a5 %x\n",csr_a5);
+                //$display("csr_a5 %x\n",csr_a5);
                 csr_mcause_wdata=csr_a5;
-                $display("csr_mcause %x\n",csr_mcause_wdata);
-                $display("pc %x\n",pc);
+                //$display("csr_mcause %x\n",csr_mcause_wdata);
+                //$display("pc %x\n",pc);
                 csr_mepc_wdata=pc;
-                $display("csr_mepc %x\n",csr_mepc_wdata);
+                //$display("csr_mepc %x\n",csr_mepc_wdata);
                 csr_mcause_wen=1'b1;
                 csr_mepc_wen=1'b1;
                 csr_mstatus_wen=1'b0;
