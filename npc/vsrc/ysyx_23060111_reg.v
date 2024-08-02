d1427e69f5d27189514d4d2179c6a5165ff3824f
tracer-ysyx <tracer@ysyx.org>
2024-08-02 15:13:01 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:13:01 up 1 day, 15:53,  1 user,  load average: 0.66, 0.40, 0.26

--- npc/vsrc/ysyx_23060111_reg.v
+++ npc/vsrc/ysyx_23060111_reg.v
@@ -44,7 +44,7 @@ module ysyx_23060111_reg #(ADDR_WIDTH = 1, DATA_WIDTH = 1) (
   always @(posedge clk) begin
     if (wen&&waddr!=0)begin
        rf[waddr] <= wdata;
-       wen<=1'b0;
+       wen=1'b0;
     end
     if (csr_wen!=0)begin
        csr[csr_waddr] <= csr_wdata;
