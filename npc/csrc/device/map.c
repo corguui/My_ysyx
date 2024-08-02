40b90707e18b5a59161863de59208ba12945e159
tracer-ysyx <tracer@ysyx.org>
2024-08-02 08:46:32 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:46:32 up 1 day, 11:13,  1 user,  load average: 0.17, 0.34, 0.30

--- npc/csrc/device/map.c
+++ npc/csrc/device/map.c
@@ -21,6 +21,7 @@ uint8_t* new_space(int size) {
 static void check_bound(IOMap *map, paddr_t addr) {
   if (map == NULL) {
     log_write("address (" "0x%08x"  ") is out of bound at pc = " "0x%08x", addr, top->pc);
+    printf( "address (" "0x%08x"  ") is out of bound at pc = " "0x%08x", addr, top->pc);
     assert(map != NULL);
       }
   else {
