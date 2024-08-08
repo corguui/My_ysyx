3b94fa38eeb3477ebf1a62cedded0e3cc62ebfff
tracer-ysyx <tracer@ysyx.org>
2024-08-08 20:55:44 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:55:44 up  3:40,  1 user,  load average: 0.23, 0.18, 0.18

---
+++ npc/src/csrc/main.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+#include "top.h"  // create `multi_logic.v`,so use `Vmulti_logic.h`
+#include "verilated.h"
+ 
+VerilatedContext* contextp=NULL; 
+top *top=NULL; 
+VerilatedVcdC* tfp=NULL;
+
+
+int main_time=0; 
+
+
+int main(int argc, char** argv, char** env) {
+ 	contextp = new VerilatedContext;
+	contextp->commandArgs(argc,argv);
+	top = new top{contextp};
+
+    contextp->traceEverOn(true);
+	tfp=new VerilatedVcdC;
+	top->trace(tfp,0);
+	tfp->open("wave.vcd");
+
+  	top->clk =0; top->eval();
+    top->rst=1;
+	top->clk =1; top->eval();
+    tfp->dump(main_time);
+    main_time++;
+
+    for(int i=0;i<10;i++)
+    {
+    top->clk =0; top->eval();
+    top->rst=1;
+	top->clk =1; top->eval();
+    tfp->dump(main_time);
+    main_time++;
+    }
+
+    tfp->close();
+    delete contextp;
+    return 0;
+
+}
