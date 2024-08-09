56599182d196be45aa3eab398f12361383d779eb
tracer-ysyx <tracer@ysyx.org>
2024-08-09 16:26:13 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:26:13 up 11:34,  1 user,  load average: 0.18, 0.18, 0.18

--- npc/src/csrc/reg.cpp
+++ npc/src/csrc/reg.cpp
@@ -5,11 +5,9 @@
 const char *regs[] = {
   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
-  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
 };
 void isa_reg_display() {
-	int length=32;
+	int length=16;
 	int i;
   cpu_read_reg();
   printf("$ pc --> 0x%x \n",cpu.pc);
@@ -35,7 +33,7 @@ uint32_t isa_reg_str2val(const char *s, bool *success) {
     //return top->pc;
     return cpu.pc;
   }
-  for (int i = 1; i < 32; i++) {
+  for (int i = 1; i < 16; i++) {
     if (!strcmp(s+1, regs[i])) {
       //return top->rootp->ysyx_23060111_top__DOT__reg___0240__DOT__rf[i];
       return cpu.gpr[i];
