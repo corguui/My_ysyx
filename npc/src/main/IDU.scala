310f2149877b4d2aa1bc11d1c78e7d10815de98f
tracer-ysyx <tracer@ysyx.org>
2024-08-10 10:54:17 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:54:17 up 19:06,  1 user,  load average: 0.47, 0.32, 0.29

--- npc/src/main/IDU.scala
+++ npc/src/main/IDU.scala
@@ -63,9 +63,71 @@ class IDU extends Module {
 	{
 	//译码
 	switch(opcode){
+		/*
 		//R-Type
-		
+		is("b0110011".U){
+
+			switch(funct3){
+				//ADD or SUB
+				is("b000".U){
+					//ADD
+					when(funct7 === 0.U){
+
+					}
+					//SUB
+					.otherwise{
+
+					}
+
+				}
+
+				//XOR
+				is("b100".U){
+
+				}
+
+				//OR
+				is("b110".U){
+
+				}
+
+				//AND
+				is("b111".U){
+
+				}
+
+				//SLL
+				is("b001".U){
+
+				}
+
+				//SRL or SRA
+				is("b101".U){
+					//SRL
+					when(funct7 === 0.U){
+
+					}
+					//SRA
+					.otherwise{
+
+					}
+				}
+
+				//SLT
+				is("b010".U){
+
+				}
+
+				//SLTU
+				is("b011".U){
+
+				}
+			}	
+		}
+		*/
+
 		//I-Type
+		/*
 		is("b0010011".U){
 			imm := in_data.inst(31,20).asSInt.asUInt
 			switch(funct3){
@@ -120,8 +182,10 @@ class IDU extends Module {
 
 			}
 		}
+		*/
 
 		//IL-Type 		
+		/*
 		is("b0000011".U){
 			imm := in_data.inst(31,20).asSInt.asUInt
 			switch(funct3){
@@ -152,8 +216,10 @@ class IDU extends Module {
 			}
 
 		}
+		*/
 
 		//S-Type
+		/*
 		is("b0100011".U){
 			imm := Cat(in_data.inst(31,25),in_data.inst(11,7)).asSInt.asUInt
 			switch(funct3){
@@ -173,8 +239,10 @@ class IDU extends Module {
 				}
 			}
 		}
+		*/
 
 		//B-Type
+		/*
 		is("b1100011".U){
 			imm := Cat(in_data.inst(31),in_data.inst(7),in_data.inst(30,25),in_data.inst(11,8),0.U(1.W)).asSInt.asUInt
 			switch(funct3){
@@ -209,6 +277,7 @@ class IDU extends Module {
 				}
 			}
 		}
+		*/
 
 		//U-Type lui
 		is("b0110111".U){
