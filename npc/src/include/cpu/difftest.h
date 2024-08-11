c8fe3e2dd002d89c6cabee34836a6f2ca179f2ba
tracer-ysyx <tracer@ysyx.org>
2024-08-11 19:32:46 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:32:46 up  7:57,  1 user,  load average: 0.24, 0.21, 0.20

--- npc/src/include/cpu/difftest.h
+++ npc/src/include/cpu/difftest.h
@@ -7,20 +7,20 @@
 void difftest_skip_ref();
 void difftest_skip_dut(int nr_ref, int nr_dut);
 void difftest_set_patch(void (*fn)(void *arg), void *arg);
-void difftest_step(vaddr_t pc, vaddr_t npc);
+void difftest_step(uint32_t  pc, uint32_t npc);
 void difftest_detach();
 void difftest_attach();
 #else
 static inline void difftest_skip_ref() {}
 static inline void difftest_skip_dut(int nr_ref, int nr_dut) {}
 static inline void difftest_set_patch(void (*fn)(void *arg), void *arg) {}
-static inline void difftest_step(vaddr_t pc, vaddr_t npc) {}
+static inline void difftest_step(uint32_t pc, uint32_t npc) {}
 static inline void difftest_detach() {}
 static inline void difftest_attach() {}
 #endif
 
-extern void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction);
-extern void (*ref_difftest_regcpy)(void *dut, bool direction);
+extern void (*ref_difftest_memcpy)(uint32_t addr, void *buf, size_t n, bool direction);
+extern void (*ref_difftest_regcpy)(void *dut,uint32_t*pc, bool direction);
 extern void (*ref_difftest_exec)(uint64_t n);
 extern void (*ref_difftest_raise_intr)(uint64_t NO);
 
