14c0ddde5ba6ed119e35f1d91ddd239d634b15c3
tracer-ysyx <tracer@ysyx.org>
2024-08-11 16:15:48 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:15:48 up  5:32,  1 user,  load average: 0.39, 0.38, 0.29

--- npc/src/main/top.scala
+++ npc/src/main/top.scala
@@ -5,6 +5,7 @@ import chisel3.util._
 
 class top extends Module {
   val io = IO(new Bundle {
+    val clock = Input(Clock())
     val pc = Output(UInt(32.W))
     val inv_flag = Output(Bool())
   })
@@ -29,6 +30,7 @@ class top extends Module {
 
   io.pc := PC.io.pc
   io.inv_flag := IDU.io.inv_flag 
+  EXU.io.clock := io.clock
 
 }
 
