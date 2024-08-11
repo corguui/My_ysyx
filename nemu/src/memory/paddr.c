f2ad3bc1ff8bcdea1af254e70708d4b6b0548f5a
tracer-ysyx <tracer@ysyx.org>
2024-08-11 21:41:06 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:41:06 up 10:06,  1 user,  load average: 0.17, 0.18, 0.18

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -92,7 +92,7 @@ void paddr_write(paddr_t addr, int len, word_t data) {
 #endif
   return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
-  if(addr==0xa00003f8)
+  if(addr==0xa00003f8 ||addr==0xa00003fc)
   {
     printf("reeturn\n");
     return;
