fcd9e32d7519162fdcb09936b4d09ef2cac750fd
tracer-ysyx <tracer@ysyx.org>
2024-08-11 21:51:43 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:51:43 up 10:16,  1 user,  load average: 0.09, 0.16, 0.17

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -92,6 +92,10 @@ void paddr_write(paddr_t addr, int len, word_t data) {
 #endif
   return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
+  if(addr==0xa00003f8)
+  {
+    return;
+  }
   printf("nemu write\n");
   out_of_bound(addr);
 }
