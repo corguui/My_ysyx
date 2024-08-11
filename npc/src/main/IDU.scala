784a5157c28ec21a99fba8fc0dcf586577694aca
tracer-ysyx <tracer@ysyx.org>
2024-08-11 15:24:32 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:24:32 up  4:41,  1 user,  load average: 0.20, 0.28, 0.27

--- npc/src/main/IDU.scala
+++ npc/src/main/IDU.scala
@@ -330,7 +330,7 @@ class IDU extends Module {
 		is("b0010111".U){
 			io.inv_flag := false.B
 			exu_data.inst_type := 7.U
-			exu_data.alu_op :="b0000".U
+			//exu_data.alu_op :="b0000".U
 			exu_data.imm := Cat(in_data.inst(31,12),0.U(12.W)).asUInt
 			exu_data.reg_wen := true.B
 		}
