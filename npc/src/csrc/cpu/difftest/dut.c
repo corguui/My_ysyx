c3b7e7736c760791f2a41c937931797e87babe31
tracer-ysyx <tracer@ysyx.org>
2024-08-11 21:08:29 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:08:29 up  9:33,  1 user,  load average: 0.17, 0.18, 0.18

--- npc/src/csrc/cpu/difftest/dut.c
+++ npc/src/csrc/cpu/difftest/dut.c
@@ -100,12 +100,10 @@ void difftest_step(uint32_t pc, uint32_t npc) {
     is_skip_ref = false;
     return;
   }
-  else {
   ref_difftest_exec(1);
   ref_difftest_regcpy(ref_r.gpr,&ref_r.pc, DIFFTEST_TO_DUT);
 
   checkregs(&ref_r, npc);
-  }
 }
 
 bool isa_difftest_checkregs(NPC_CPU_state *ref_r, uint32_t pc) {
