4f6db120c913b88032915006a63623d95a141397
tracer-ysyx <tracer@ysyx.org>
2024-08-11 16:18:30 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:18:30 up  5:35,  1 user,  load average: 0.51, 0.42, 0.32

--- npc/src/main/EXU.scala
+++ npc/src/main/EXU.scala
@@ -28,7 +28,6 @@ class EXU extends Module {
 
     class Mem extends BlackBox with HasBlackBoxPath {
     	val io = IO(new Bundle {
-        val clock = Input(Clock())
         val m_waddr = Input(UInt(32.W))
         val m_wdata = Input(UInt(32.W))
         val m_wmask = Input(UInt(32.W))
@@ -50,7 +49,6 @@ class EXU extends Module {
     mem.io.m_raddr :=0.U
     mem.io.m_rmask :=0.U
     mem.io.m_ren :=0.U
-    mem.io.clock := io.clock
     val alu = Module(new ALU)
     alu.io.src1 :=0.U
     alu.io.src2 :=0.U
