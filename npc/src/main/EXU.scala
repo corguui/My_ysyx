aec794c8e3c44fb686bf03018e5e0a85d04a83d1
tracer-ysyx <tracer@ysyx.org>
2024-08-11 10:11:02 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:11:02 up  1:27,  1 user,  load average: 0.15, 0.15, 0.17

--- npc/src/main/EXU.scala
+++ npc/src/main/EXU.scala
@@ -2,39 +2,20 @@ package npc
 
 import chisel3._
 import chisel3.util._
+import chisel3.experimental._
 
-
-class EXUtoALU extends Bundle {
-    val src1 = Output(UInt(32.W))
-    val src2 = Output(UInt(32.W))
-    val alu_op = Output(UInt(4.W))
-}
 
 
 class EXU extends Module {
     val io = IO(new Bundle {
         val idu2in = Flipped(Decoupled(new IDUtoEXU))
-        val out2alu = Decoupled(new EXUtoALU)// 2 means to out to alu
+        val reg_wdata = Output(UInt(32.W))
+        val reg_wen = Output(Bool())
+        val reg_waddr = Output(UInt(5.W))
+        val dnpc = Output(UInt(32.W))
+        val snpc = Input(UInt(32.W))
+        val pc   = Input(UInt(32.W))
     })
-
-    //EXU recive ALU 
-	val alu2s_idle :: alu2s_wait_ready :: Nil = Enum(2)
-	val alu2s_state = RegInit(alu2s_idle)
-	alu2s_state :=MuxLookup(alu2s_state,alu2s_idle)(List(
-		alu2s_idle -> Mux(io.out2alu.valid,alu2s_wait_ready,alu2s_idle),
-		alu2s_wait_ready -> Mux(io.out2alu.ready,alu2s_idle,alu2s_wait_ready)
-	))
-
-    val alu_data = Wire(new EXUtoALU)
-    alu_data.src1 := 0.U
-    alu_data.src2 := 0.U
-    alu_data.alu_op := 0.U
-    val lastsrc1 = RegNext(alu_data.src1,1.U)
-	val lastsrc2 = RegNext(alu_data.src2,1.U)
-	val lastalu_op = RegNext(alu_data.alu_op,1.U)
-
-    io.out2alu.bits := alu_data
-    io.out2alu.valid := (alu_data.src1 =/=lastsrc1 ) | (alu_data.src2 =/=lastsrc2 ) | (alu_data.alu_op =/=lastalu_op )
 
 
     //EXU to IDU
@@ -45,10 +26,136 @@ class EXU extends Module {
 		m2IDUprocess -> Mux(io.idu2in.ready,m2IDUidle,m2IDUprocess)
 	))
 
+    class Mem extends BlackBox with HasBlackBoxPath {
+    	val io = IO(new Bundle {
+        val m_waddr = Input(UInt(32.W))
+        val m_wdata = Input(UInt(32.W))
+        val m_wmask = Input(UInt(32.W))
+        val m_wen = Input(Bool())
+        val m_raddr = Input(UInt(32.W))
+        val m_rdata = Output(UInt(32.W))
+        val m_rmask = Input(UInt(32.W))
+        val m_ren = Input(Bool())
+      })
+
+		addPath("./src/main/Mem.v")
+  	}
+
+    val mem = Module(new Mem)
+    mem.io.m_waddr :=0.U
+    mem.io.m_wdata :=0.U
+    mem.io.m_wmask :=0.U
+    mem.io.m_wen :=0.U
+    mem.io.m_raddr :=0.U
+    mem.io.m_rmask :=0.U
+    mem.io.m_ren :=0.U
+    val alu = Module(new ALU)
+    alu.io.src1 :=0.U
+    alu.io.src2 :=0.U
+    alu.io.alu_op :=15.U
+    io.dnpc := io.snpc 
+    io.reg_wdata := 0.U
+    io.reg_wen := 0.U
+    io.reg_waddr := 0.U
+    /*
+    val data_all = Wire(new IDUtoEXU)
+    data_all.alu_op := 15.U
+    data_all.inst_type := 0.U
+    data_all.imm := 0.U
+    data_all.src1 := 0.U
+    data_all.src2 := 0.U
+    data_all.reg_wen := 0.U
+    data_all.reg_waddr := 0.U
+    data_all.m_wmask := 0.U
+    data_all.m_rmask := 0.U
+    data_all.mem_ren := 0.U
+    data_all.mem_wen := 0.U
+    */
+
+
+
     io.idu2in.ready := ( m2IDUstate===m2IDUidle )
     when(m2IDUstate === m2IDUprocess)
     {
-        io.idu2in.bits <> alu_data  
+        io.idu2in.bits <> data_all  
+        switch(io.idu2in.bits.inst_type)
+        {
+            //R type
+            is(1.U){
+                alu.io.src1 := io.idu2in.bits.src1
+                alu.io.src2 := io.idu2in.bits.src2
+                alu.io.alu_op := io.idu2in.bits.alu_op
+                io.reg_wdata := alu.io.result
+                io.reg_wen := io.idu2in.bits.reg_wen
+                io.reg_waddr := io.idu2in.bits.reg_waddr
+            }
+            //I type
+            is(2.U){
+                alu.io.src1 := io.idu2in.bits.src1 
+                alu.io.src2 := io.idu2in.bits.imm
+                alu.io.alu_op := io.idu2in.bits.alu_op
+                io.reg_wdata := alu.io.result
+                io.reg_wen := io.idu2in.bits.reg_wen
+                io.reg_waddr := io.idu2in.bits.reg_waddr
+            }
+            //IL type
+            is(3.U){
+                alu.io.src1 := io.idu2in.bits.src1
+                alu.io.src2 := io.idu2in.bits.imm
+                alu.io.alu_op := io.idu2in.bits.alu_op
+                mem.io.m_raddr := alu.io.result
+                mem.io.ren := io.idu2in.bits.mem_ren
+                mem.io.m_rmask := io.idu2in.bits.m_rmask
+                when(io.idu2in.bits.m_rmask===1.U)
+                {
+                io.reg_wdata := (Cat(fill(24,m_rdata(7)),mem.io.m_rdata(7,0))).asSInt
+                }.elsewhen(io.idu2in.bits.m_rmask===2.U)
+                {
+                io.reg_wdata := (Cat(fill(16,m_rdata(15)),mem.io.m_rdata(15,0))).asSInt
+                }.otherwise
+                {
+                io.reg_wdata := mem.io.m_rdata.asSInt
+                }
+                io.reg_wen := io.idu2in.bits.reg_wen
+                io.reg_waddr := io.idu2in.bits.reg_waddr
+            }
+            //s type
+            is(4.U){
+                alu.io.src1 := io.idu2in.bits.src1
+                alu.io.src2 := io.idu2in.bits.imm
+                mem.io.m_waddr := alu.io.result
+                mem.io.m_wdata := io.idu2in.bits.src2
+                mem.io.m_wmask := io.idu2in.bits.m_wmask
+                mem.io.wen := io.idu2in.bits.mem_wen
+            }
+            //b type
+            is(5.U){
+                alu.io.src1 := io.idu2in.bits.src1
+                alu.io.src2 := io.idu2in.bits.src2
+                alu.io.alu_op := io.idu2in.bits.alu_op
+                io.dnpc :=  Mux(alu.io.result,io.pc+io.idu2in.bits.imm,io.snpc)
+            }
+            //u type
+            is(6.U){
+                io.reg_wdata := io.idu2in.bits.imm
+                io.reg_wen := io.idu2in.bits.reg_wen
+            }
+            //upc type
+            is(7.U){
+                io.reg_wdata := io.idu2in.bits.imm+io.pc
+                io.reg_wen := io.idu2in.bits.reg_wen
+            }
+            //j type
+            is(8.U){
+                io.reg_wdata := io.snpc
+                io.dnpc := io.idu2in.bits.imm+io.pc
+                io.reg_wen := io.idu2in.bits.reg_wen
+            }
+            is(9.U){
+                io.reg_wdata := io.snpc
+                io.dnpc := io.idu2in.bits.imm+io.idu2in.bits.src1
+                io.reg_wen := io.idu2in.bits.reg_wen
+            }
+        }
     }
-
 }
--- npc/src/main/PC.scala
+++ npc/src/main/PC.scala
@@ -3,36 +3,17 @@ package npc
 import chisel3._
 import chisel3.util._
 
-class PCtoIFU extends Bundle {
-	val pc = Output(UInt(32.W))
-}
+
 
 class PC extends Module {
 	val io = IO(new Bundle{
+		val pc = Outputput(UInt(32.W))
 		val dnpc = Input(UInt(32.W))
 		val snpc = Output(UInt(32.W))
 		val out = Decoupled(new PCtoIFU)
 	})
-
-	//PC recive IFU
-	val ifu2s_idle :: ifu2s_wait_ready :: Nil = Enum(2)
-	val state = RegInit(ifu2s_idle)
-	state :=MuxLookup(state,ifu2s_idle)(List(
-		ifu2s_idle -> Mux(io.out.valid,ifu2s_wait_ready,ifu2s_idle),
-		ifu2s_wait_ready -> Mux(io.out.ready,ifu2s_idle,ifu2s_wait_ready)
-	))
-
-	val data = Wire(new PCtoIFU)
-
-	//记录pc变化前值
-	val lastpc = RegNext(data.pc,0.U)
-
 	//更新pc
-	data.pc := RegNext(io.dnpc.asSInt, 0x80000000.S).asUInt
-	io.out.bits := data
-
-	//判断是否输出数据
-	io.out.valid := (data.pc =/= lastpc)
+	PC.io.pc := RegNext(io.dnpc.asSInt, 0x80000000.S).asUInt
 
 	io.snpc := data.pc + 4.U
 
