f820a19bc8a18333d7c913ea730a267dd91c7e4a
tracer-ysyx <tracer@ysyx.org>
2024-08-11 19:09:50 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:09:50 up  7:34,  1 user,  load average: 0.09, 0.13, 0.18

--- npc/src/main/ALU.scala
+++ npc/src/main/ALU.scala
@@ -6,7 +6,7 @@ import chisel3.util._
 class alu_io extends Bundle{
     val src1 = Input(UInt(32.W))
     val src2 = Input(UInt(32.W))
-    val alu_op = Input(UInt(4.W))
+    val alu_op = Input(UInt(5.W))
     val result = Output(UInt(32.W))
 }
 
@@ -17,22 +17,22 @@ class ALU extends Module{
     io.result := 0.U
     switch(io.alu_op)
     {
-        is("b0000".U){ io.result := io.src1 + io.src2 } //add
-        is("b0001".U){ io.result := io.src1 - io.src2 } //sub
-        is("b0010".U){ io.result := io.src1 & io.src2 } //and
-        is("b0011".U){ io.result := io.src1 | io.src2 } //or
-        is("b0100".U){ io.result := io.src1 ^ io.src2 } //xor
-        is("b0101".U){ io.result := io.src1 << io.src2(4,0) } //sll
-        is("b0110".U){ io.result := io.src1 >> io.src2(4,0) } //srl
-        is("b0111".U){ io.result := (io.src1.asSInt >> io.src2(4,0)).asUInt } //sra maybe error
-        is("b1000".U){ io.result := (io.src1.asSInt < io.src2.asSInt).asUInt } //slt
-        is("b1001".U){ io.result := (io.src1 < io.src2 ).asUInt } //sltu
-        is("b1010".U){ io.result := (io.src1 === io.src2) } //beq
-        is("b1011".U){ io.result := (io.src1 >= io.src2) } //bgeu
-        is("b1100".U){ io.result := (io.src1.asSInt >= io.src2.asSInt).asUInt } //bge
-        is("b1101".U){ io.result := (io.src1 =/= io.src2) } //bne
-        is("b1110".U){ io.result := (io.src1.asSInt < io.src2.asSInt).asUInt } //blt
-        is("b1111".U){ io.result := (io.src1 < io.src2) } //be
+        is("b00000".U){ io.result := io.src1 + io.src2 } //add
+        is("b00001".U){ io.result := io.src1 - io.src2 } //sub
+        is("b00010".U){ io.result := io.src1 & io.src2 } //and
+        is("b00011".U){ io.result := io.src1 | io.src2 } //or
+        is("b00100".U){ io.result := io.src1 ^ io.src2 } //xor
+        is("b00101".U){ io.result := io.src1 << io.src2(4,0) } //sll
+        is("b00110".U){ io.result := io.src1 >> io.src2(4,0) } //srl
+        is("b00111".U){ io.result := (io.src1.asSInt >> io.src2(4,0)).asUInt } //sra maybe error
+        is("b01000".U){ io.result := (io.src1.asSInt < io.src2.asSInt).asUInt } //slt
+        is("b01001".U){ io.result := (io.src1 < io.src2 ).asUInt } //sltu
+        is("b01010".U){ io.result := (io.src1 === io.src2) } //beq
+        is("b01011".U){ io.result := (io.src1 >= io.src2) } //bgeu
+        is("b01100".U){ io.result := (io.src1.asSInt >= io.src2.asSInt).asUInt } //bge
+        is("b01101".U){ io.result := (io.src1 =/= io.src2) } //bne
+        is("b01110".U){ io.result := (io.src1.asSInt < io.src2.asSInt).asUInt } //blt
+        is("b01111".U){ io.result := (io.src1 < io.src2) } //be
     }
 
 }
