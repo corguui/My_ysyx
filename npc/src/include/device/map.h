2d5c5f6c68c883d9eb586754bcf401bfa2c7b4d7
tracer-ysyx <tracer@ysyx.org>
2024-08-12 10:10:18 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:10:18 up 12:25,  1 user,  load average: 0.12, 0.13, 0.16

--- npc/src/include/device/map.h
+++ npc/src/include/device/map.h
@@ -22,6 +22,7 @@ static inline bool map_inside(IOMap *map, paddr_t addr) {
 
 static inline int find_mapid_by_addr(IOMap *maps, int size, paddr_t addr) {
   int i;
+  printf("%x\n", addr);
   for (i = 0; i < size; i ++) {
     if (map_inside(maps + i, addr)) {
       difftest_skip_ref();
