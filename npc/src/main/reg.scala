97229e9acbada9da1528780ea51818260f97e7f8
tracer-ysyx <tracer@ysyx.org>
2024-08-12 20:48:03 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:48:03 up 20:28,  1 user,  load average: 0.59, 0.36, 0.29

--- npc/src/main/reg.scala
+++ npc/src/main/reg.scala
@@ -36,10 +36,10 @@ class Reg extends Module {
     when(io.wen&&io.waddr=/=0.U) {
         reg(io.waddr) := io.wdata
     }
-    when(io.csr_wen_1&&io.csr_waddr_1=/=0.U) {
+    when(io.csr_wen_1) {
         csr(io.csr_waddr_1) := io.csr_wdata_1
     }
-    when(io.csr_wen_2&&io.csr_waddr_2=/=0.U) {
+    when(io.csr_wen_2) {
         csr(io.csr_waddr_2) := io.csr_wdata_2
     } 
 
