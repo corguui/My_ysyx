47be35fa4b1cbecac231c063bb0aba0f0d01c368
tracer-ysyx <tracer@ysyx.org>
2024-08-12 09:31:50 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:31:50 up 11:47,  1 user,  load average: 0.08, 0.15, 0.17

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -43,7 +43,6 @@ __EXPORT void difftest_regcpy(void *dut,uint32_t *pc, bool direction) {
   uint32_t* gpr=(uint32_t*) dut;
   if(direction==DIFFTEST_TO_DUT)
   {
-    printf("nemu pc %x\n",cpu.pc);
     *pc=cpu.pc;
     for(int i = 0;i<32;i++ )
     {
