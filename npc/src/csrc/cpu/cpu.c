8fa34bd9ccd1a32d00444cf904d023c645a85010
tracer-ysyx <tracer@ysyx.org>
2024-08-12 11:14:57 +0800

    > compile NEMU ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:14:57 up 13:30,  1 user,  load average: 0.17, 0.15, 0.17

--- npc/src/csrc/cpu/cpu.c
+++ npc/src/csrc/cpu/cpu.c
@@ -179,9 +179,6 @@ void cpu_exec_once(VerilatedVcdC* tfp,Decode *s)
 {
 
 		top->clock =0; top->eval();
-		s->pc=top->io_pc;
-		s->inst=top->rootp->top__DOT__IFU__DOT___vlg_pc_read_inst;
-    	s->dnpc=top->rootp->top__DOT___EXU_io_dnpc;
 		#ifdef CONFIG_VCD
 		tfp->dump(main_time);
 		#endif
@@ -193,7 +190,9 @@ void cpu_exec_once(VerilatedVcdC* tfp,Decode *s)
 		#endif
 		main_time++;
 		top->eval();
-
+		s->pc=top->io_pc;
+		s->inst=top->rootp->top__DOT__IFU__DOT___vlg_pc_read_inst;
+    	s->dnpc=top->rootp->top__DOT___EXU_io_dnpc;
 
 #ifdef CONFIG_ITRACE
   char *p = s->logbuf;
