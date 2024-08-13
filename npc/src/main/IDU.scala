ee1dcbc714f1602aa9e853d9fb1f6dcf913993cc
tracer-ysyx <tracer@ysyx.org>
2024-08-13 11:19:49 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:19:49 up 1 day, 11:00,  1 user,  load average: 0.28, 0.25, 0.20

--- npc/src/main/IDU.scala
+++ npc/src/main/IDU.scala
@@ -47,6 +47,7 @@ class IDU extends Module {
 	io.out2exu.valid := (exu_data.alu_op =/= lastalu_op )
 	io.out2exu.bits := exu_data
 	
+	/*
     class npc_break extends BlackBox with HasBlackBoxPath {
     	val io = IO(new Bundle {
 			val inst = Input(UInt(32.W))
@@ -54,6 +55,7 @@ class IDU extends Module {
 
 		addPath("./src/main/npc_break.v")
   	}
+	*/
 
 
     //IDU to IFU
@@ -69,8 +71,10 @@ class IDU extends Module {
     //val lastinst = RegNext(in_data.inst,0.U)
     //io.ifu2in.ready := (lastinst =/= in_data.inst)
 
+	/*
 	val npc_break = Module(new npc_break)
 	npc_break.io.inst := in_data.inst
+	*/
 
 	val opcode = in_data.inst(6,0)
 	val rd = in_data.inst(11,7)
