f4bbbd529232c633140ca01fc87fe4f69b81fef7
tracer-ysyx <tracer@ysyx.org>
2024-08-13 11:59:53 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:59:53 up 1 day, 11:40,  1 user,  load average: 0.78, 0.38, 0.25

--- npc/src/main/Elaborate.scala
+++ npc/src/main/Elaborate.scala
@@ -2,7 +2,7 @@ object Elaborate extends App {
   val firtoolOptions = Array("--lowering-options=" + List(
     // make yosys happy
     // see https://github.com/llvm/circt/blob/main/docs/VerilogGeneration.md
-    "emittedLineLength=600",
+    "emittedLineLength=900",
     "disallowLocalVariables",
     "disallowPackedArrays",
     "locationInfoStyle=wrapInAtSquareBracket"
