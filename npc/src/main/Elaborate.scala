32d5dd19f79c988bc9a7deaec9eefaf4e285a1cd
tracer-ysyx <tracer@ysyx.org>
2024-08-13 14:15:40 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:15:40 up 1 day, 13:56,  1 user,  load average: 0.22, 0.16, 0.10

--- npc/src/main/Elaborate.scala
+++ npc/src/main/Elaborate.scala
@@ -2,9 +2,9 @@ object Elaborate extends App {
   val firtoolOptions = Array("--lowering-options=" + List(
     // make yosys happy
     // see https://github.com/llvm/circt/blob/main/docs/VerilogGeneration.md
-    "emittedLineLength=900",
+    //"emittedLineLength=900",
     "disallowLocalVariables",
-    "disallowPackedArrays",
+    "disallowPackedArrays = true",
     "locationInfoStyle=wrapInAtSquareBracket"
   ).reduce(_ + "," + _))
   circt.stage.ChiselStage.emitSystemVerilogFile(new npc.top(), args, firtoolOptions)
