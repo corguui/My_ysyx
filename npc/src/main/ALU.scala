865315ea556b28a561cfae05ec32fea2bc1573fa
tracer-ysyx <tracer@ysyx.org>
2024-08-13 14:49:55 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:49:55 up 1 day, 14:30,  1 user,  load average: 0.17, 0.22, 0.23

--- npc/src/main/ALU.scala
+++ npc/src/main/ALU.scala
@@ -15,7 +15,7 @@ class ALU extends Module{
     val io = IO(new alu_io)
 
     io.result := 0.U
-    /*
+    
     switch(io.alu_op)
     {
         is("b00000".U){ io.result := io.src1 + io.src2 } //add
@@ -33,41 +33,6 @@ class ALU extends Module{
         is("b01100".U){ io.result := (io.src1.asSInt >= io.src2.asSInt).asUInt } //bge
         is("b01101".U){ io.result := (io.src1 =/= io.src2).asUInt } //bne
     }
-    */
-    
-  io.result := 0.U
-
-  when(io.alu_op === "b00000".U) {
-    io.result := io.src1 + io.src2 // add
-  } .elsewhen(io.alu_op === "b00001".U) {
-    io.result := io.src1 - io.src2 // sub
-  } .elsewhen(io.alu_op === "b00010".U) {
-    io.result := io.src1 & io.src2 // and
-  } .elsewhen(io.alu_op === "b00011".U) {
-    io.result := io.src1 | io.src2 // or
-  } .elsewhen(io.alu_op === "b00100".U) {
-    io.result := io.src1 ^ io.src2 // xor
-  } .elsewhen(io.alu_op === "b00101".U) {
-    io.result := io.src1 << io.src2(4,0) // sll
-  } .elsewhen(io.alu_op === "b00110".U) {
-    io.result := io.src1 >> io.src2(4,0) // srl
-  } .elsewhen(io.alu_op === "b00111".U) {
-    io.result := (io.src1.asSInt >> io.src2(4,0)).asUInt // sra
-  } .elsewhen(io.alu_op === "b01000".U) {
-    io.result := Mux(io.src1.asSInt < io.src2.asSInt, 1.U, 0.U) // slt
-  } .elsewhen(io.alu_op === "b01001".U) {
-    io.result := Mux(io.src1 < io.src2, 1.U, 0.U) // sltu
-  } .elsewhen(io.alu_op === "b01010".U) {
-    io.result := Mux(io.src1 === io.src2, 1.U, 0.U) // beq
-  } .elsewhen(io.alu_op === "b01011".U) {
-    io.result := Mux(io.src1 >= io.src2, 1.U, 0.U) // bgeu
-  } .elsewhen(io.alu_op === "b01100".U) {
-    io.result := Mux(io.src1.asSInt >= io.src2.asSInt, 1.U, 0.U) // bge
-  } .elsewhen(io.alu_op === "b01101".U) {
-    io.result := Mux(io.src1 =/= io.src2, 1.U, 0.U) // bne
-  } .otherwise {
-    io.result := 0.U // 默认情况
-  }
 
 
 }
--- npc/src/main/Elaborate.scala
+++ npc/src/main/Elaborate.scala
@@ -1,3 +1,4 @@
+/*
 object Elaborate extends App {
   val firtoolOptions = Array("--lowering-options=" + List(
     // make yosys happy
@@ -9,13 +10,11 @@ object Elaborate extends App {
   ).reduce(_ + "," + _))
   circt.stage.ChiselStage.emitSystemVerilogFile(new npc.top(), args, firtoolOptions)
 }
-// object Elaborate extends App{
-//     val firtoolOptions = Array("--lowering-options=" + List(
-//     // make yosys happy
-//     // see https://github.com/llvm/circt/blob/main/docs/VerilogGeneration.md
-//     "disallowLocalVariables",
-//     "disallowPackedArrays",
-//     "locationInfoStyle=wrapInAtSquareBracket"
-//   ).reduce(_ + "," + _))
-//   circt.stage.ChiselStage.emitSystemVerilogFile(new nanhu.MEFreeList(32), args, firtoolOptions)
-// }
+*/
+
+object Elaborate extends App {
+  val firtoolOptions = Array("--lowering-options=" + List(
+    "locationInfoStyle=wrapInAtSquareBracket"
+  ).reduce(_ + "," + _))
+  circt.stage.ChiselStage.emitSystemVerilogFile(new npc.top(), args, firtoolOptions)
+}
