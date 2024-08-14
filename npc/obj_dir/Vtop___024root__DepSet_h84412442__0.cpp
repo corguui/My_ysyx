af09c5275da0e114208a58b7d4bd6af83fc592f8
tracer-ysyx <tracer@ysyx.org>
2024-08-14 20:28:45 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:28:45 up 2 days, 19:04,  1 user,  load average: 0.13, 0.20, 0.26

--- npc/obj_dir/Vtop___024root__DepSet_h84412442__0.cpp
+++ npc/obj_dir/Vtop___024root__DepSet_h84412442__0.cpp
@@ -100,18 +100,8 @@ VL_INLINE_OPT void Vtop___024root___nba_sequent__TOP__0(Vtop___024root* vlSelf) 
     } else {
         vlSelf->top__DOT__IFU__DOT___vlg_pc_read_inst = 0U;
     }
-    __Vdly__top__DOT__EXU__DOT__m2IDUstate = ((~ (IData)(vlSelf->reset)) 
-                                              & ((~ (IData)(vlSelf->top__DOT__EXU__DOT__m2IDUstate)) 
-                                                 & ((vlSelf->top__DOT__IDU__DOT__exu_data_imm 
-                                                     != vlSelf->top__DOT__IDU__DOT__lastimm) 
-                                                    | ((IData)(vlSelf->top__DOT__IDU__DOT__exu_data_alu_op) 
-                                                       != (IData)(vlSelf->top__DOT__IDU__DOT__lastaluop)))));
-    vlSelf->top__DOT__IDU__DOT__state = ((~ (IData)(vlSelf->reset)) 
-                                         & ((~ (IData)(vlSelf->top__DOT__IDU__DOT__state)) 
-                                            & (IData)(vlSelf->top__DOT__IFU__DOT__lasten)));
-    vlSelf->top__DOT__IDU__DOT__lastreg_wen = ((~ (IData)(vlSelf->reset)) 
-                                               & (IData)(vlSelf->top__DOT__IDU__DOT__exu_data_reg_wen));
     if (vlSelf->reset) {
+        __Vdly__top__DOT__EXU__DOT__m2IDUstate = 0U;
         vlSelf->top__DOT__Reg__DOT__csr_1 = 0U;
         vlSelf->top__DOT__Reg__DOT__csr_3 = 0U;
         vlSelf->top__DOT__Reg__DOT__csr_2 = 0U;
@@ -151,6 +141,13 @@ VL_INLINE_OPT void Vtop___024root___nba_sequent__TOP__0(Vtop___024root* vlSelf) 
         vlSelf->top__DOT__IDU__DOT__lastaluop = 0x10U;
         vlSelf->top__DOT__IDU__DOT__lastimm = 0U;
     } else {
+        if ((1U & (~ (IData)(vlSelf->top__DOT__EXU__DOT__m2IDUstate)))) {
+            __Vdly__top__DOT__EXU__DOT__m2IDUstate 
+                = ((vlSelf->top__DOT__IDU__DOT__exu_data_imm 
+                    != vlSelf->top__DOT__IDU__DOT__lastimm) 
+                   | ((IData)(vlSelf->top__DOT__IDU__DOT__exu_data_alu_op) 
+                      != (IData)(vlSelf->top__DOT__IDU__DOT__lastaluop)));
+        }
         if (((IData)(vlSelf->top__DOT___EXU_io_csr_wen_1) 
              & (1U == (IData)(vlSelf->top__DOT___EXU_io_csr_waddr_1)))) {
             vlSelf->top__DOT__Reg__DOT__csr_1 = vlSelf->top__DOT___EXU_io_csr_wdata_1;
@@ -308,6 +305,11 @@ VL_INLINE_OPT void Vtop___024root___nba_sequent__TOP__0(Vtop___024root* vlSelf) 
         vlSelf->top__DOT__IDU__DOT__lastaluop = vlSelf->top__DOT__IDU__DOT__exu_data_alu_op;
         vlSelf->top__DOT__IDU__DOT__lastimm = vlSelf->top__DOT__IDU__DOT__exu_data_imm;
     }
+    vlSelf->top__DOT__IDU__DOT__state = ((~ (IData)(vlSelf->reset)) 
+                                         & ((~ (IData)(vlSelf->top__DOT__IDU__DOT__state)) 
+                                            & (IData)(vlSelf->top__DOT__IFU__DOT__lasten)));
+    vlSelf->top__DOT__IDU__DOT__lastreg_wen = ((~ (IData)(vlSelf->reset)) 
+                                               & (IData)(vlSelf->top__DOT__IDU__DOT__exu_data_reg_wen));
     vlSelf->top__DOT__EXU__DOT__m2IDUstate = __Vdly__top__DOT__EXU__DOT__m2IDUstate;
 }
 
