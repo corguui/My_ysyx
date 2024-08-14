b65b0789b037f10f464f58547e8f7b4b093378ef
tracer-ysyx <tracer@ysyx.org>
2024-08-14 10:08:52 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:08:52 up 2 days,  9:49,  1 user,  load average: 0.76, 0.44, 0.32

--- npc/src/main/IFU.scala
+++ npc/src/main/IFU.scala
@@ -74,7 +74,7 @@ class IFU extends Module {
 	
 
 	//取指令和生成ready,valid信号
-	val lastinst = RegNext(vlg_pc_read,0.U)
+	val lastinst = RegNext(vlg_pc_read.io.inst,0.U)
 	//io.out.valid := (lastinst =/= out_data.inst)
 	val lastpc = RegNext(out_data.pc,0.U)
 	val lastsnpc = RegNext(out_data.snpc,0.U)
