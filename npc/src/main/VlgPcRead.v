521d910d8f34741a873ea490cfa68ea7b36773cb
tracer-ysyx <tracer@ysyx.org>
2024-08-14 08:40:19 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:40:19 up 2 days,  8:21,  1 user,  load average: 0.35, 0.31, 0.18

--- npc/src/main/VlgPcRead.v
+++ npc/src/main/VlgPcRead.v
@@ -2,10 +2,16 @@ import "DPI-C" function int vlg_pc_read(input int pc);
 module VlgPcRead(
     input clk,
     input [31:0] pc,
+    input pc_en,
     output reg [31:0] inst
 );
     always@(posedge clk) begin
+        if(pc_en)
+        begin
             inst=vlg_pc_read(pc);
+        end
+        else
+            inst=0;
     end
 
 
