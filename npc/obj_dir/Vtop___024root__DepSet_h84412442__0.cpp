7f12a288f84c5e1541a8664c7a07f0bb913ece06
tracer-ysyx <tracer@ysyx.org>
2024-08-14 11:13:21 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:13:21 up 2 days, 10:54,  1 user,  load average: 0.65, 0.31, 0.23

--- npc/obj_dir/Vtop___024root__DepSet_h84412442__0.cpp
+++ npc/obj_dir/Vtop___024root__DepSet_h84412442__0.cpp
@@ -45,8 +45,6 @@ VL_INLINE_OPT void Vtop___024root___nba_sequent__TOP__0(Vtop___024root* vlSelf) 
     // Init
     IData/*31:0*/ __Vfunc_vlg_pc_read__0__Vfuncout;
     __Vfunc_vlg_pc_read__0__Vfuncout = 0;
-    CData/*0:0*/ __Vdly__top__DOT__IDU__DOT__state;
-    __Vdly__top__DOT__IDU__DOT__state = 0;
     CData/*0:0*/ __Vdly__top__DOT__EXU__DOT__m2IDUstate;
     __Vdly__top__DOT__EXU__DOT__m2IDUstate = 0;
     // Body
@@ -80,7 +78,6 @@ VL_INLINE_OPT void Vtop___024root___nba_sequent__TOP__0(Vtop___024root* vlSelf) 
     }
     vlSelf->__Vdly__top__DOT__IFU__DOT__m2EXUstate 
         = vlSelf->top__DOT__IFU__DOT__m2EXUstate;
-    __Vdly__top__DOT__IDU__DOT__state = vlSelf->top__DOT__IDU__DOT__state;
     if (vlSelf->top__DOT__IFU__DOT__m2EXUstate) {
         Vtop___024unit____Vdpiimwrap_vlg_pc_read_TOP____024unit(vlSelf->io_pc, __Vfunc_vlg_pc_read__0__Vfuncout);
         vlSelf->top__DOT__IFU__DOT___vlg_pc_read_inst 
@@ -88,9 +85,16 @@ VL_INLINE_OPT void Vtop___024root___nba_sequent__TOP__0(Vtop___024root* vlSelf) 
     } else {
         vlSelf->top__DOT__IFU__DOT___vlg_pc_read_inst = 0U;
     }
+    __Vdly__top__DOT__EXU__DOT__m2IDUstate = ((~ (IData)(vlSelf->reset)) 
+                                              & ((~ (IData)(vlSelf->top__DOT__EXU__DOT__m2IDUstate)) 
+                                                 & ((vlSelf->top__DOT__IDU__DOT__exu_data_imm 
+                                                     != vlSelf->top__DOT__IDU__DOT__lastimm) 
+                                                    | ((IData)(vlSelf->top__DOT__IDU__DOT__exu_data_alu_op) 
+                                                       != (IData)(vlSelf->top__DOT__IDU__DOT__lastaluop)))));
+    vlSelf->top__DOT__IDU__DOT__state = ((~ (IData)(vlSelf->reset)) 
+                                         & ((~ (IData)(vlSelf->top__DOT__IDU__DOT__state)) 
+                                            & (IData)(vlSelf->top__DOT__IFU__DOT__lasten)));
     if (vlSelf->reset) {
-        __Vdly__top__DOT__EXU__DOT__m2IDUstate = 0U;
-        __Vdly__top__DOT__IDU__DOT__state = 0U;
         vlSelf->top__DOT__Reg__DOT__csr_1 = 0U;
         vlSelf->top__DOT__Reg__DOT__csr_3 = 0U;
         vlSelf->top__DOT__Reg__DOT__csr_2 = 0U;
@@ -130,16 +134,6 @@ VL_INLINE_OPT void Vtop___024root___nba_sequent__TOP__0(Vtop___024root* vlSelf) 
         vlSelf->top__DOT__IDU__DOT__lastaluop = 0x10U;
         vlSelf->top__DOT__IDU__DOT__lastimm = 0U;
     } else {
-        if ((1U & (~ (IData)(vlSelf->top__DOT__EXU__DOT__m2IDUstate)))) {
-            __Vdly__top__DOT__EXU__DOT__m2IDUstate 
-                = ((vlSelf->top__DOT__IDU__DOT__exu_data_imm 
-                    != vlSelf->top__DOT__IDU__DOT__lastimm) 
-                   | ((IData)(vlSelf->top__DOT__IDU__DOT__exu_data_alu_op) 
-                      != (IData)(vlSelf->top__DOT__IDU__DOT__lastaluop)));
-        }
-        if ((1U & (~ (IData)(vlSelf->top__DOT__IDU__DOT__state)))) {
-            __Vdly__top__DOT__IDU__DOT__state = vlSelf->top__DOT__IFU__DOT__lasten;
-        }
         if (((IData)(vlSelf->top__DOT___EXU_io_csr_wen_1) 
              & (1U == (IData)(vlSelf->top__DOT___EXU_io_csr_waddr_1)))) {
             vlSelf->top__DOT__Reg__DOT__csr_1 = vlSelf->top__DOT___EXU_io_csr_wdata_1;
@@ -297,7 +291,6 @@ VL_INLINE_OPT void Vtop___024root___nba_sequent__TOP__0(Vtop___024root* vlSelf) 
         vlSelf->top__DOT__IDU__DOT__lastaluop = vlSelf->top__DOT__IDU__DOT__exu_data_alu_op;
         vlSelf->top__DOT__IDU__DOT__lastimm = vlSelf->top__DOT__IDU__DOT__exu_data_imm;
     }
-    vlSelf->top__DOT__IDU__DOT__state = __Vdly__top__DOT__IDU__DOT__state;
     vlSelf->top__DOT__EXU__DOT__m2IDUstate = __Vdly__top__DOT__EXU__DOT__m2IDUstate;
 }
 
