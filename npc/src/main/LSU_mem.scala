ed5d6a3968fe9d7089252851a8917a17eb70bf5a
tracer-ysyx <tracer@ysyx.org>
2024-08-15 19:13:34 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:13:33 up 3 days, 17:48,  1 user,  load average: 0.22, 0.23, 0.15

--- npc/src/main/LSU_mem.scala
+++ npc/src/main/LSU_mem.scala
@@ -4,15 +4,19 @@ import chisel3._
 import chisel3.util._
 import chisel3.experimental._
 
+class MemtoEXU extends Bundle {
+    val rdata = Output(UInt(32.W))
+    val rvalid = Output(Bool())
+    val rready = Input(Bool())
+}
+
 class IO_mem extends Bundle {
+    val r_exu_mem = Flipped(new EXUtoMem) 
+    val r_mem_exu = (new MemtoEXU)
     val m_waddr = Input(UInt(32.W))
     val m_wdata = Input(UInt(32.W))
     val m_wmask = Input(UInt(32.W))
     val m_wen = Input(Bool())
-    val m_raddr = Input(UInt(32.W))
-    val m_rdata = Output(UInt(32.W))
-    val m_rmask = Input(UInt(32.W))
-    val m_ren = Input(Bool())
 }
 
 class LSU_mem extends Module {
@@ -42,10 +46,22 @@ class LSU_mem extends Module {
     m.io.m_wdata := io.mem.m_wdata
     m.io.m_wmask := io.mem.m_wmask
     m.io.m_wen := io.mem.m_wen
-    m.io.m_raddr := io.mem.m_raddr
-    io.mem.m_rdata := m.io.m_rdata
-    m.io.m_rmask := io.mem.m_rmask
-    m.io.m_ren := io.mem.m_ren
+
+    io.r_exu_mem.arready := false.B
+    io.r_mem_exu.rdata := 0.U
+    io.r_mem_exu.rvalid := false.B
+
+    val lastraddr = RegNext(io.r_exu_mem.raddr,0.U)
+    when(io.r_exu_mem.arvalid &(io.r_exu_mem.raddr =/= lastraddr)){ 
+        io.r_exu_mem.arready := true.B
+        m.io.m_raddr := io.r_exu_mem.raddr
+        m.io.m_rmask := io.r_exu_mem.rmask
+        m.io.m_ren := io.r_exu_mem.arvalid
+        io.r_mem_exu.rdata := m.io.m_rdata
+        io.r_mem_exu.rvalid := true.B
+    }.elsewhen(io.r_mem_exu.rready){
+        io.r_mem_exu.rvalid := false.B
+    }
 
 
 }
