1aaa57e2a59b5b1e89b994082c2c3f2a6bf9af83
tracer-ysyx <tracer@ysyx.org>
2024-08-15 22:10:45 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:10:45 up 3 days, 20:46,  1 user,  load average: 0.22, 0.22, 0.26

--- npc/src/main/EXU.scala
+++ npc/src/main/EXU.scala
@@ -257,12 +257,7 @@ class EXU extends Module {
                 io.w_exu_mem.wdata := io.idu2in.bits.src2
                 io.w_exu_mem.wmask := io.idu2in.bits.m_wmask
                 io.w_exu_mem.wvalid := Mux(io.idu2in.bits.mem_wen === 1.U,io.idu2in.bits.mem_wen,memwen_reg_en)
-                when(io.w_exu_mem.wready === 1.U)
-                {
-                    io.w_exu_mem.wvalid := 0.U
-                }.otherwise{
-                    memwen_reg_en := true.B
-                }
+                memwen_reg_en := true.B
 
             }
             //b type
