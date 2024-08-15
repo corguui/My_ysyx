669ac4fe8026c60d0d88b9c5a2e9cca9477b8195
tracer-ysyx <tracer@ysyx.org>
2024-08-15 11:06:14 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:06:14 up 3 days,  9:41,  1 user,  load average: 0.25, 0.24, 0.23

--- npc/src/csrc/cpu/difftest/dut.c
+++ npc/src/csrc/cpu/difftest/dut.c
@@ -17,7 +17,7 @@ enum { DIFFTEST_TO_DUT, DIFFTEST_TO_REF };
 
 static bool is_skip_ref = false;
 static int skip_dut_nr_inst = 0;
-int wait=0;
+int ref_skip_wait=0;
 
 void difftest_skip_ref() {
   is_skip_ref = true;
@@ -101,12 +101,13 @@ void difftest_step(uint32_t pc, uint32_t npc) {
     //传入的pc 会加4 在nemu diff ref.c
     ref_difftest_regcpy(cpu.gpr,&cpu.pc, DIFFTEST_TO_REF);
     is_skip_ref = false;
-    wait=1;
+    ref_skip_wait=1;
+    printf("skip_ref\n");
     return;
   }
-  printf("%d\n",wait);
+  printf("%d\n",ref_skip_wait);
   //等待npc多执行一次
-  if(wait=1)
+  if(ref_skip_wait=1)
   {
     return;
   }
