e8fb4ec092e9a2cb344700085357905931011119
tracer-ysyx <tracer@ysyx.org>
2024-08-15 19:37:16 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:37:16 up 3 days, 18:12,  1 user,  load average: 0.25, 0.41, 0.39

--- npc/src/main/EXU.scala
+++ npc/src/main/EXU.scala
@@ -192,13 +192,11 @@ class EXU extends Module {
                 io.reg_wen := io.idu2in.bits.reg_wen
                 io.reg_waddr := io.idu2in.bits.reg_waddr
 
-                when(io.r_exu_mem.arready === 0.U)
-                {
                 //只有一次,如果发送后接收不到再次发送应该是0.U了
                 io.r_exu_mem.rmask := io.idu2in.bits.m_rmask
                 io.r_exu_mem.raddr := alu.io.result 
                 io.r_exu_mem.arvalid := io.idu2in.bits.mem_ren               
-                }.elsewhen(io.r_exu_mem.arready === 1.U)
+                when(io.r_exu_mem.arready === 1.U)
                 {
                     when((io.r_mem_exu.rvalid === 1.U)&(io.r_mem_exu.rdata =/= lastrdata))
                     {
