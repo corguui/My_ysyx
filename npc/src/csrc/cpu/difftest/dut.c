ed4de2615c82fa123f552d567f3c6b05f667fc0c
tracer-ysyx <tracer@ysyx.org>
2024-08-15 20:31:52 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:31:52 up 3 days, 19:07,  1 user,  load average: 0.19, 0.20, 0.20

--- npc/src/csrc/cpu/difftest/dut.c
+++ npc/src/csrc/cpu/difftest/dut.c
@@ -113,6 +113,7 @@ void difftest_step(uint32_t pc, uint32_t npc) {
   ref_difftest_regcpy(ref_r.gpr,&ref_r.pc, DIFFTEST_TO_DUT);
 
   checkregs(&ref_r, pc);
+  //checkregs(&ref_r, npc);
 }
 
 bool isa_difftest_checkregs(NPC_CPU_state *ref_r, uint32_t pc) {
--- npc/src/main/EXU.scala
+++ npc/src/main/EXU.scala
@@ -135,8 +135,8 @@ class EXU extends Module {
     
 
     //当为IL类型指令时可以保存数据
-    //val  reg_wen_reg = RegEbable(io.idu2in.bits.reg_wen,0.U,io.idu2in.valid)
-    //val  reg_waddr_reg = RegEnable(io.idu2in.bits.reg_waddr,0.U,io.idu2in.valid)
+    val  reg_wen_reg = RegEbable(io.idu2in.bits.reg_wen,0.U,io.idu2in.valid)
+    val  reg_waddr_reg = RegEnable(io.idu2in.bits.reg_waddr,0.U,io.idu2in.valid)
 
     /*
     val data_all = Wire(new IDUtoEXU)
@@ -186,10 +186,10 @@ class EXU extends Module {
                 alu.io.src1 := io.idu2in.bits.src1
                 alu.io.src2 := io.idu2in.bits.imm
                 alu.io.alu_op := io.idu2in.bits.alu_op
-                //io.reg_wen := reg_wen_reg 
-                //io.reg_waddr := reg_waddr_reg 
-                io.reg_wen := io.idu2in.bits.reg_wen
-                io.reg_waddr := io.idu2in.bits.reg_waddr
+                io.reg_wen := reg_wen_reg 
+                io.reg_waddr := reg_waddr_reg 
+                //io.reg_wen := Mux(io.idu2in.bits.reg_wen === 1,io.idu2in.bits.reg_wen,reg_wen_reg)
+                //io.reg_waddr := Mux(io.idu2in.bits.reg_waddr === 0,reg_waddr_reg,io.idu2in.bits.reg_waddr)
 
                 //只有一次,如果发送后接收不到再次发送应该是0.U了
                 io.r_exu_mem.rmask := io.idu2in.bits.m_rmask
