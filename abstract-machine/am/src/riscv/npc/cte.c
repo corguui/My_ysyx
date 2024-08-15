3e01d293354a42326becf69b17a18f1c5bc33a51
tracer-ysyx <tracer@ysyx.org>
2024-08-15 10:04:30 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:04:30 up 3 days,  8:39,  1 user,  load average: 0.33, 0.26, 0.24

--- abstract-machine/am/src/riscv/npc/cte.c
+++ abstract-machine/am/src/riscv/npc/cte.c
@@ -7,7 +7,7 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
-    printf("macause %d\n", ev.event);
+    printf("macause %d\n", c->mcause);
     switch (c->mcause) {
       case  0: ev.event = EVENT_YIELD; break;
       case  11:ev.event = EVENT_YIELD; break;
