7a4d645f0e33527c0821ce80b717e7b48ed31f39
tracer-ysyx <tracer@ysyx.org>
2024-08-15 11:32:23 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:32:23 up 3 days, 10:07,  1 user,  load average: 0.28, 0.26, 0.21

--- npc/obj_dir/Vtop___024root__DepSet_heccd7ead__0__Slow.cpp
+++ npc/obj_dir/Vtop___024root__DepSet_heccd7ead__0__Slow.cpp
@@ -53,7 +53,7 @@ VL_ATTR_COLD void Vtop___024root___eval_settle(Vtop___024root* vlSelf) {
 #ifdef VL_DEBUG
                 Vtop___024root___dump_triggers__stl(vlSelf);
 #endif
-                VL_FATAL_MT("build/top.sv", 1114, "", "Settle region did not converge.");
+                VL_FATAL_MT("build/top.sv", 1116, "", "Settle region did not converge.");
             }
             vlSelf->__VstlIterCount = ((IData)(1U) 
                                        + vlSelf->__VstlIterCount);
@@ -203,7 +203,6 @@ VL_ATTR_COLD void Vtop___024root___ctor_var_reset(Vtop___024root* vlSelf) {
     vlSelf->top__DOT__EXU__DOT____Vcellinp__alu__io_src1 = VL_RAND_RESET_I(32);
     vlSelf->top__DOT__EXU__DOT____VdfgTmp_h6f9fceee__0 = 0;
     vlSelf->top__DOT__EXU__DOT____VdfgTmp_h61894a5a__0 = 0;
-    vlSelf->top__DOT__EXU__DOT____VdfgTmp_h14fbfe57__0 = 0;
     vlSelf->top__DOT__EXU__DOT__alu__DOT____VdfgTmp_hdb870149__0 = 0;
     vlSelf->top__DOT__Reg__DOT__csr_0 = VL_RAND_RESET_I(32);
     vlSelf->top__DOT__Reg__DOT__csr_1 = VL_RAND_RESET_I(32);
@@ -245,7 +244,7 @@ VL_ATTR_COLD void Vtop___024root___ctor_var_reset(Vtop___024root* vlSelf) {
     vlSelf->top__DOT__Reg__DOT__casez_tmp_0 = VL_RAND_RESET_I(32);
     vlSelf->top__DOT__Reg__DOT___GEN = VL_RAND_RESET_I(1);
     vlSelf->__VdfgTmp_h075e1d2a__0 = 0;
-    vlSelf->__VdfgTmp_h3308c7df__0 = 0;
+    vlSelf->__VdfgTmp_h570e6332__0 = 0;
     vlSelf->__Vfunc_vlg_pmem_read__2__Vfuncout = 0;
     vlSelf->__Vdly__top__DOT__IFU__DOT__m2EXUstate = VL_RAND_RESET_I(1);
     vlSelf->__Vtrigrprev__TOP__clock = VL_RAND_RESET_I(1);
