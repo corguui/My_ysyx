09488b56c8e8d8fddbe6add8f07143b2cd30f6c3
tracer-ysyx <tracer@ysyx.org>
2024-08-16 21:51:06 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:51:06 up 4 days, 20:26,  1 user,  load average: 0.20, 0.21, 0.26

--- npc/src/main/EXU.scala
+++ npc/src/main/EXU.scala
@@ -157,10 +157,10 @@ class EXU extends Module {
     val reg_wen_reg = RegEnable(io.idu2in.bits.reg_wen,0.U,io.idu2in.valid)
     val reg_waddr_reg = RegEnable(io.idu2in.bits.reg_waddr,0.U,io.idu2in.valid)
 
-    val mem_ren_reg_en = Wire(Bool())
     val mem_raddr_reg = RegEnable(alu.io.result,0.U,io.idu2in.valid)
     val mem_rmask_reg = RegEnable(io.idu2in.bits.m_rmask,0.U,io.idu2in.valid)
-    val mem_ren_reg = RegEnable(io.idu2in.bits.mem_ren,0.U,mem_ren_reg_en)
+    //val mem_ren_reg = RegEnable(io.idu2in.bits.mem_ren,0.U,io.idu2in.valid)
+    val mem_ren_reg = RegNext(mem_ren_reg_en,0.U)
     val rready_reg = RegInit(0.U)
 
     //rready_reg := 0.U
@@ -206,7 +206,7 @@ class EXU extends Module {
                 alu.io.src1 := io.idu2in.bits.src1
                 alu.io.src2 := io.idu2in.bits.imm
                 alu.io.alu_op := io.idu2in.bits.alu_op
-                mem_ren_reg_en := true.B
+                mem_ren_reg_en := io.idu2in.bits.mem_ren 
                 when((io.ar_exu_mem.arready)&(io.ar_exu_mem.arvalid))
                 {                    
                     io.ar_exu_mem.rmask := mem_rmask_reg
