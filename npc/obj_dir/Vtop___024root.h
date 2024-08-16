3761aab810ebfa88ffc6138397a746cdfff3b5a4
tracer-ysyx <tracer@ysyx.org>
2024-08-16 21:53:31 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:53:31 up 4 days, 20:28,  1 user,  load average: 0.44, 0.37, 0.32

--- npc/obj_dir/Vtop___024root.h
+++ npc/obj_dir/Vtop___024root.h
@@ -56,20 +56,20 @@ class Vtop___024root final : public VerilatedModule {
         CData/*0:0*/ top__DOT__EXU__DOT___GEN_3;
         CData/*0:0*/ top__DOT__EXU__DOT___GEN_4;
         CData/*0:0*/ top__DOT__EXU__DOT___GEN_5;
-        CData/*0:0*/ top__DOT__EXU__DOT___GEN_7;
+        CData/*0:0*/ top__DOT__EXU__DOT___GEN_6;
         CData/*0:0*/ top__DOT__EXU__DOT___GEN_8;
-        CData/*0:0*/ top__DOT__EXU__DOT___GEN_16;
+        CData/*0:0*/ top__DOT__EXU__DOT___GEN_9;
         CData/*0:0*/ top__DOT__EXU__DOT___GEN_17;
         CData/*0:0*/ top__DOT__EXU__DOT___GEN_18;
         CData/*0:0*/ top__DOT__EXU__DOT___GEN_19;
         CData/*0:0*/ top__DOT__EXU__DOT___GEN_20;
         CData/*0:0*/ top__DOT__EXU__DOT___GEN_21;
         CData/*0:0*/ top__DOT__EXU__DOT___GEN_22;
-        CData/*0:0*/ top__DOT__EXU__DOT___GEN_24;
-        CData/*0:0*/ top__DOT__EXU__DOT___GEN_26;
+        CData/*0:0*/ top__DOT__EXU__DOT___GEN_23;
+        CData/*0:0*/ top__DOT__EXU__DOT___GEN_25;
         CData/*0:0*/ top__DOT__EXU__DOT___GEN_27;
+        CData/*0:0*/ top__DOT__EXU__DOT___GEN_28;
         CData/*4:0*/ top__DOT__EXU__DOT____Vcellinp__alu__io_alu_op;
-        CData/*0:0*/ top__DOT__EXU__DOT____VdfgTmp_h3a32f024__0;
         CData/*0:0*/ top__DOT__EXU__DOT____VdfgTmp_h6f9fceee__0;
         CData/*0:0*/ top__DOT__EXU__DOT____VdfgTmp_h61894a5a__0;
         CData/*4:0*/ top__DOT__EXU__DOT__alu__DOT____VdfgTmp_hdb870149__0;
@@ -82,9 +82,10 @@ class Vtop___024root final : public VerilatedModule {
         CData/*4:0*/ __VdfgTmp_hd76c6d79__0;
         CData/*0:0*/ __Vdly__top__DOT__IFU__DOT__m2EXUstate;
         CData/*3:0*/ __Vdly__top__DOT__IDU__DOT__exu_data_inst_type;
-        CData/*0:0*/ __Vdly__top__DOT__EXU__DOT__mem_ren_reg;
+        CData/*0:0*/ __Vdly__top__DOT__IDU__DOT__exu_data_mem_ren;
     };
     struct {
+        CData/*0:0*/ __Vdly__top__DOT__EXU__DOT__m2IDUstate;
         CData/*0:0*/ __Vtrigrprev__TOP__clock;
         CData/*0:0*/ __VactDidInit;
         CData/*0:0*/ __VactContinue;
@@ -148,9 +149,9 @@ class Vtop___024root final : public VerilatedModule {
         IData/*31:0*/ top__DOT__Reg__DOT__reg_24;
         IData/*31:0*/ top__DOT__Reg__DOT__reg_25;
         IData/*31:0*/ top__DOT__Reg__DOT__reg_26;
-        IData/*31:0*/ top__DOT__Reg__DOT__reg_27;
     };
     struct {
+        IData/*31:0*/ top__DOT__Reg__DOT__reg_27;
         IData/*31:0*/ top__DOT__Reg__DOT__reg_28;
         IData/*31:0*/ top__DOT__Reg__DOT__reg_29;
         IData/*31:0*/ top__DOT__Reg__DOT__reg_30;
