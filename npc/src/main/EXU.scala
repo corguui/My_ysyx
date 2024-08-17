b0f697b728f2a96774125c14f5a62a6bba4c8ebf
tracer-ysyx <tracer@ysyx.org>
2024-08-17 10:41:45 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:41:45 up 5 days,  9:17,  1 user,  load average: 0.43, 0.28, 0.19

--- npc/src/main/EXU.scala
+++ npc/src/main/EXU.scala
@@ -157,11 +157,11 @@ class EXU extends Module {
     val reg_wen_reg = RegEnable(io.idu2in.bits.reg_wen,0.U,io.idu2in.valid)
     val reg_waddr_reg = RegEnable(io.idu2in.bits.reg_waddr,0.U,io.idu2in.valid)
 
+    val rready_reg = RegInit(0.U)
     val mem_raddr_reg = RegEnable(alu.io.result,0.U,io.idu2in.valid)
     val mem_rmask_reg = RegEnable(io.idu2in.bits.m_rmask,0.U,io.idu2in.valid)
     val mem_ren_reg = RegEnable(io.idu2in.bits.mem_ren,0.U,(io.idu2in.valid | rready_reg))
     //val mem_ren_reg = RegInit(0.U)
-    val rready_reg = RegInit(0.U)
 
     //rready_reg := 0.U
 
