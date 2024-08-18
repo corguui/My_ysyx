f44ba5820720db986b5eb39793ca32fe3afcb5d9
tracer-ysyx <tracer@ysyx.org>
2024-08-18 15:47:27 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:47:27 up 6 days, 14:22,  1 user,  load average: 0.32, 0.28, 0.21

--- npc/src/main/EXU.scala
+++ npc/src/main/EXU.scala
@@ -101,21 +101,21 @@ class EXU extends Module {
 
     val ifu_outdata = Wire(new EXUtoIFU)
     val lastdnpc = RegNext(ifu_outdata.dnpc,0.U)
-    dnpc_en := 0.U
+    io.out2ifu.dnpc_en := 0.U
     when(io.idu2in.bits.inst_type === 3.U)
     {
     io.out2ifu.valid :=  io.r_mem_exu.rready
-    dnpc_en := 1.U
+    io.out2ifu.dnpc_en := 1.U
     }
     .elsewhen(io.idu2in.bits.inst_type === 4.U)
     {
     io.out2ifu.valid :=  io.b_mem_exu.bready
-    dnpc_en := 1.U
+    io.out2ifu.dnpc_en := 1.U
     }
     .otherwise
     {
     io.out2ifu.valid := (lastdnpc =/= ifu_outdata.dnpc)
-    dnpc_en := 1.U
+    io.out2ifu.dnpc_en := 1.U
     }
     io.out2ifu.bits := ifu_outdata
 
