185e472a592a49781827dbf495fbb86303c93141
tracer-ysyx <tracer@ysyx.org>
2024-08-19 10:39:23 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:39:23 up 7 days,  9:14,  1 user,  load average: 0.11, 0.22, 0.25

--- npc/src/main/LSU_mem.scala
+++ npc/src/main/LSU_mem.scala
@@ -65,7 +65,7 @@ class LSU_mem extends Module {
 
     io.ar_exu_mem.arready := true.B
     io.r_mem_exu.rdata := 0.U 
-    //io.r_mem_exu.rresp := 3.U
+    io.r_mem_exu.rresp := 0.U
     io.r_mem_exu.rvalid := rvalid_reg 
 
     //AXI-lite write member
@@ -77,7 +77,7 @@ class LSU_mem extends Module {
     val bresp_reg = RegEnable(bresp,0.U,(io.w_exu_mem.wvalid | io.aw_exu_mem.awvalid))
     io.aw_exu_mem.awready := true.B
     io.w_exu_mem.wready := true.B
-    //io.b_mem_exu.bresp := 3.U 
+    io.b_mem_exu.bresp := 0.U 
     io.b_mem_exu.bvalid := bvalid_reg
 
     //AXI-lite read part
