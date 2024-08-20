ecec29356626b17b6e961a557cd7021bd3c66d0a
tracer-ysyx <tracer@ysyx.org>
2024-08-20 16:11:50 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:11:50 up  5:36,  1 user,  load average: 0.24, 0.16, 0.15

--- npc/src/main/LSU_mem.scala
+++ npc/src/main/LSU_mem.scala
@@ -65,8 +65,8 @@ class LSU_mem extends Module {
     //test delay 
     /*
     val delaycycles = 5 
-    val m_rdata_delay = ShiftRegister(m.io.m_rdata,delaycycles,1.U,io.ar_exu_mem.arvalid)
-    val rdata_reg = RegEnable(m_rdata_delay,1.U,io.ar_exu_mem.arvalid)
+    val m_rdata_delay = ShiftRegister(m.io.m_rdata,delaycycles,0.U,io.ar_exu_mem.arvalid)
+    val rdata_reg = RegEnable(m_rdata_delay,0.U,io.ar_exu_mem.arvalid)
     */
     //tes delay 
 
