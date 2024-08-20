8c8ab19153849d2c74ff8bc18def28066724d3f7
tracer-ysyx <tracer@ysyx.org>
2024-08-20 20:54:28 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:54:28 up 10:18,  1 user,  load average: 0.54, 0.36, 0.32

--- npc/obj_dir/Vtop__Dpi.h
+++ npc/obj_dir/Vtop__Dpi.h
@@ -16,13 +16,13 @@ extern "C" {
 
 
     // DPI IMPORTS
-    // DPI import at build/top.sv:1572:30
+    // DPI import at build/top.sv:1574:30
     extern void ebreak(int inst);
-    // DPI import at build/top.sv:1625:29
+    // DPI import at build/top.sv:1627:29
     extern int vlg_pc_read(int pc);
-    // DPI import at build/top.sv:1586:29
+    // DPI import at build/top.sv:1588:29
     extern int vlg_pmem_read(int m_raddr, int rmask);
-    // DPI import at build/top.sv:1585:30
+    // DPI import at build/top.sv:1587:30
     extern void vlg_pmem_write(int waddr, int wdata, int wmask);
 
 #ifdef __cplusplus
