2596918e55b7cdff65041672c971e7fc26a3847c
tracer-ysyx <tracer@ysyx.org>
2024-08-20 21:27:36 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:27:36 up 10:51,  1 user,  load average: 0.02, 0.35, 0.35

--- npc/obj_dir/Vtop__Dpi.h
+++ npc/obj_dir/Vtop__Dpi.h
@@ -16,13 +16,13 @@ extern "C" {
 
 
     // DPI IMPORTS
-    // DPI import at build/top.sv:1594:30
+    // DPI import at build/top.sv:1595:30
     extern void ebreak(int inst);
-    // DPI import at build/top.sv:1647:29
+    // DPI import at build/top.sv:1648:29
     extern int vlg_pc_read(int pc);
-    // DPI import at build/top.sv:1608:29
+    // DPI import at build/top.sv:1609:29
     extern int vlg_pmem_read(int m_raddr, int rmask);
-    // DPI import at build/top.sv:1607:30
+    // DPI import at build/top.sv:1608:30
     extern void vlg_pmem_write(int waddr, int wdata, int wmask);
 
 #ifdef __cplusplus
--- npc/src/main/LSU_mem.scala
+++ npc/src/main/LSU_mem.scala
@@ -98,12 +98,12 @@ class LSU_mem extends Module {
         m.io.m_rmask := io.ar_exu_mem.rmask
         m.io.m_ren := io.ar_exu_mem.arvalid
         //或的最后一个条件是为了防止第一个rdata为0
-        //rvalid_en := Mux((m.io.m_rdata =/= rdata_reg & rdata_reg =/= 11.U ) | (m.io.m_rdata =/=0.U) | (m.io.m_rdata === 0.U & rdata_reg === 11.U)  , true.B, false.B)
+        //rvalid_en := Mux((m.io.m_rdata =/= rdata_reg  ) | (m.io.m_rdata =/=0.U) | (m.io.m_rdata === 0.U & rdata_reg === 11.U)  , true.B, false.B)
 
 
         //delay
         //rvalid_en := Mux((((m_rdata_delay =/= rdata_reg) & (rdata_reg>=1.U)) | ((m_rdata_delay === 0.U) & (rdata_reg === 0.U))) , true.B, false.B)
-        rvalid_en := Mux((m_rdata_delay =/= rdata_reg & rdata_reg =/= 11.U) | (m_rdata_delay =/= 0.U ) , true.B, false.B)
+        rvalid_en := Mux((m_rdata_delay =/= rdata_reg & rdata_reg =/= 11.U) | (m_rdata_delay =/= 0.U & m_rdata_delay =/= rdata_reg ) , true.B, false.B)
         //delay
 
         when(((m.io.m_raddr >= 0x80000000.S.asUInt)&(m.io.m_raddr < 0x8fffffff.S.asUInt)) | ((m.io.m_raddr >= 0xa00003f8.S.asUInt)&(m.io.m_raddr <= 0xa00003ff.S.asUInt)) | ((m.io.m_raddr >= 0xa0000048.S.asUInt)&(m.io.m_raddr <= 0xa000004f.S.asUInt))){
