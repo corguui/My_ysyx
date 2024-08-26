3c7e532bacf07c0caa3104c5e52c1909b970ae2b
tracer-ysyx <tracer@ysyx.org>
2024-08-26 15:55:46 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:55:46 up  3:41,  1 user,  load average: 0.09, 0.13, 0.11

--- npc/src/main/AXI_arbitration.scala
+++ npc/src/main/AXI_arbitration.scala
@@ -79,7 +79,6 @@ class AXI_arbiter extends Module {
         io.ifu_axi_r <> io.axi_r
         io.ifu_axi_b <> io.axi_b
     }.elsewhen(io.lsu_sta&&(!io.ifu_sta)){
-        /*
         //uart
         when((io.lsu_axi_aw.awaddr >= 0xa00003f8.S.asUInt) & (io.lsu_axi_aw.awaddr <= 0xa00003ff.S.asUInt)) {
             io.uart_axi_ar <> io.lsu_axi_ar
@@ -90,13 +89,11 @@ class AXI_arbiter extends Module {
         }
         //SRAM
         .elsewhen(((io.lsu_axi_ar.raddr >= 0x80000000.S.asUInt) & (io.lsu_axi_ar.raddr <= 0x8fffffff.S.asUInt)) | ((io.lsu_axi_aw.awaddr >= 0x80000000.S.asUInt) & (io.lsu_axi_aw.awaddr <= 0x8fffffff.S.asUInt))) {
-        */
             io.axi_ar <> io.lsu_axi_ar
             io.axi_aw <> io.lsu_axi_aw
             io.axi_w <> io.lsu_axi_w
             io.lsu_axi_r <> io.axi_r
             io.lsu_axi_b <> io.axi_b
-            /*
         }.otherwise{
             io.lsu_axi_ar.arready := true.B
             io.lsu_axi_r.rdata := 0.U
@@ -106,7 +103,6 @@ class AXI_arbiter extends Module {
             io.lsu_axi_b.bvalid := true.B
             io.lsu_axi_b.bresp := 0.U
         }
-        */
     }
 }
 
