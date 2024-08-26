03b79e83737220b688a99133b37e9815fd05a5cd
tracer-ysyx <tracer@ysyx.org>
2024-08-26 10:11:05 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:11:05 up 33 min,  1 user,  load average: 0.25, 0.10, 0.10

--- npc/src/csrc/device/serial.c
+++ npc/src/csrc/device/serial.c
@@ -16,7 +16,7 @@ static void serial_io_handler(uint32_t offset, int len, bool is_write) {
   switch (offset) {
     /* We bind the serial port with the host stderr in NEMU. */
     case CH_OFFSET:
-      printf(" %x %c",serial_base[0],serail_base[0]);
+      printf(" %x %c",serial_base[0],serial_base[0]);
       if (is_write) serial_putc(serial_base[0]);
       else{ printf("do not support read"); assert(0);}
       break;
