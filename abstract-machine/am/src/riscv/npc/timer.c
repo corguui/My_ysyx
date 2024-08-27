e06c61c025f5b8e7f68bad34aca2dd69d9aa9788
tracer-ysyx <tracer@ysyx.org>
2024-08-27 11:31:22 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:31:22 up  1:26,  1 user,  load average: 0.02, 0.10, 0.08

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -19,7 +19,8 @@ void __am_timer_init()
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime)
 {
-    uptime->us = get_time() - boot_time;
+    //uptime->us = get_time() - boot_time;
+    uptime->us = (get_time() - boot_time)/9;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
