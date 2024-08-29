fc162d0c1cc064f69aa4b0065c032f89bb69ef69
tracer-ysyx <tracer@ysyx.org>
2024-08-29 15:36:55 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:36:55 up  4:21,  1 user,  load average: 0.33, 0.22, 0.09

--- npc/src/main/top.scala
+++ npc/src/main/top.scala
@@ -144,6 +144,6 @@ class top extends Module {
   AXI_arbiter.io.axi_ar.arready := io.master.arready  
   */
 
-  (io.master:Data).waiveAll :<>= (axi_ar:Data).waiveAll
+  (io.master:Data).waiveAll :<>= (axi_ar,axi_aw,aix_w,axi_b,axi_r:Data).waiveAll
 }
 
