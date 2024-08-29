806659222a7116be7f3293f87c2869d2dbbb4c68
tracer-ysyx <tracer@ysyx.org>
2024-08-29 10:23:25 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:23:25 up  1:11,  1 user,  load average: 0.25, 0.13, 0.06

--- npc/src/include/config.h
+++ npc/src/include/config.h
@@ -7,9 +7,9 @@
 //#define CONFIG_CC_WATCHPOINT 1
 #define CONFIG_ITRACE 1
 #define CONFIG_IQUEUE 1
-//#define CONFIG_TARGET_NATIVE_ELF 1
+#define CONFIG_TARGET_NATIVE_ELF 1
 #define __GUEST_ISA__ "riscv32" 
-//#define CONFIG_FTRACE 1
+#define CONFIG_FTRACE 1
 //#define CONFIG_DTRACE 1
 //#define CONFIG_DIFFTEST 1
 //#define CONFIG_MTRACE 1
