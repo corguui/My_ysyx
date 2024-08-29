51610e48cb4a2fa980e8d6d6d9e8011d3f49bd8e
tracer-ysyx <tracer@ysyx.org>
2024-08-29 19:12:53 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:12:53 up  6:46,  1 user,  load average: 0.00, 0.01, 0.00

--- npc/src/csrc/cpu/cpu.c
+++ npc/src/csrc/cpu/cpu.c
@@ -185,7 +185,7 @@ void cpu_exec_once(VerilatedVcdC* tfp,Decode *s)
 
 		top->clock =0; top->eval();
 		valid_flag=0;
-		if(top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__IFU__DOT__io_out_bits_pc_0!=0&&top->rootp->top__DOT__WBU__DOT__ifu_outdata_dnpc!=0x80000000&&cmp_dnpc!=top->rootp->top__DOT__WBU__DOT__ifu_outdata_dnpc)
+		if(top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__IFU__DOT__io_out_bits_pc_0!=0&&top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__WBU__DOT__ifu_outdata_dnpc!=0x80000000&&cmp_dnpc!=top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__WBU__DOT__ifu_outdata_dnpc)
 		{
 		valid_flag =1;
 		pc=top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__IFU__DOT__io_out_bits_pc_0;
@@ -210,7 +210,7 @@ void cpu_exec_once(VerilatedVcdC* tfp,Decode *s)
 		top->clock =0; top->eval();
 		//cpu_read_reg(); 
 		}
-		cmp_dnpc=top->rootp->top__DOT__WBU__DOT__ifu_outdata_dnpc;
+		cmp_dnpc=top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__WBU__DOT__ifu_outdata_dnpc;
 		#ifdef CONFIG_VCD
 		tfp->dump(main_time);
 		#endif
