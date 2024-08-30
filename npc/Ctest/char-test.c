b495683c163b837bc606ca0770a0cd3a4860fe23
tracer-ysyx <tracer@ysyx.org>
2024-08-30 15:13:15 +0800

    > sim RTL ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:13:15 up  6:17,  1 user,  load average: 0.02, 0.02, 0.00

--- npc/Ctest/char-test.c
+++ npc/Ctest/char-test.c
@@ -1,7 +1,13 @@
+#include <stdio.h>
 #define UART_BASE 0x10000000L
 #define UART_TX   0
 void _start() {
   *(volatile char *)(UART_BASE + UART_TX) = 'A';
   *(volatile char *)(UART_BASE + UART_TX) = '\n';
   while (1);
+}
+int main()
+{
+  _start();
+  return 0;
 }
