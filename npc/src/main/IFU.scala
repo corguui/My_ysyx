66f151c64bd2f30eccd1f4e16b2d6ce310bc1314
tracer-ysyx <tracer@ysyx.org>
2024-08-30 16:20:30 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:20:30 up  7:24,  1 user,  load average: 0.24, 0.14, 0.11

--- npc/src/main/IFU.scala
+++ npc/src/main/IFU.scala
@@ -72,6 +72,7 @@ class IFU extends Module {
 
   	//val vlg_pc_read = Module(new pcreadmem)   yosys 使用
 	val inst = Wire(UInt(32.W))
+	inst := 0.U
 	val exu2in_reg = RegNext(io.exu2in.valid)
 	val indata 	= Reg(new EXUtoIFU)
 	when(io.exu2in.valid & (io.exu2in.valid =/= exu2in_reg )){	
