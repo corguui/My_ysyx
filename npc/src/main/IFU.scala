cad659fa89481e5042aa19e8537970af79574be0
tracer-ysyx <tracer@ysyx.org>
2024-09-01 11:20:45 +0800

    > generate verilog ysyx_23060111 陈柯帆 Linux corgi-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:20:45 up  2:15,  1 user,  load average: 0.02, 0.03, 0.03

--- npc/src/main/IFU.scala
+++ npc/src/main/IFU.scala
@@ -80,7 +80,7 @@ class IFU extends Module {
 	}
 	
 	val ardata_reg = RegEnable(indata.dnpc,0x20000000.S.asUInt,exu2in_reg)
-	val inst_reg 	= RegEnable(inst,0.U,io.ifu_axi_r.rready )
+	//val inst_reg 	= RegEnable(inst,0.U,io.ifu_axi_r.rready )
 	//def delay(x:Bool)={RegNext(x)}
 	val arvalid_reg = RegEnable(exu2in_reg,false.B,(io.ifu_axi_ar.arready| exu2in_reg ))
 	val sta_reg = RegEnable(exu2in_reg,false.B,(io.ifu_axi_r.rready| exu2in_reg ))
@@ -95,7 +95,7 @@ class IFU extends Module {
 
 	io.out.bits.pc := 0.U
 	io.out.bits.snpc := io.out.bits.pc + 4.U
-	io.out.bits.inst := inst_reg 
+	io.out.bits.inst := inst 
 
 
 	when(m2EXUstate === m2EXUprocess){
